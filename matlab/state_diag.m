55c356b6727bdfa152d44208f2150ca082fafd92
Jeff Anderson <jla@ucar.edu>
2002-04-25 16:57:50 -0600

    Matlab and users guide draft
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@87 dfa8782c-da17-4c45-ba5c-5625b50a00d6

---
+++ matlab/state_diag.m
@@ -0,0 +1,18 @@
+file_name = input('What is file for true state');
+
+read_state;
+
+true_state = state;
+
+file_name = input('What is file for prior state');
+
+read_state;
+
+prior_state = state;
+
+file_name = input('What is file for posterior state')
+
+read_state;
+
+posterior_state = state;
+
