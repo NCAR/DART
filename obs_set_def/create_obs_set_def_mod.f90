116d7ba543871e48f1a01bdea1e768e030ad0a72
Tim Hoar <thoar@ucar.edu>
2002-07-24 15:10:08 -0600

    renamed obs_set_def.f90 to obs_set_def_mod.f90
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@130 dfa8782c-da17-4c45-ba5c-5625b50a00d6

---
+++ obs_set_def/create_obs_set_def_mod.f90
@@ -0,0 +1,77 @@
+program create_obs_defs
+
+!
+! <next four lines automatically updated by CVS, do not edit>
+! $Source$
+! $Revision$
+! $Date$
+! $Author$
+
+! Main program to interactively create a set_def_list description file for a 
+! particular spatial domain and obs_kind set. This is a prototype for more
+! user friendly GUI driven methods of creating set_def_lists. For now, there
+! is no support for nested observation definition subsets, but that will be
+! needed in the long run.
+
+use obs_def_mod, only : obs_def_type, init_obs_def, interactive_obs_def
+use obs_set_def_mod, only : obs_set_def_type, init_obs_set_def, add_obs
+use set_def_list_mod, only : set_def_list_type, init_set_def_list, &
+   add_to_list, write_set_def_list, read_set_def_list
+use utilities_mod, only : open_file
+
+implicit none
+
+type(obs_def_type) :: obs_def
+type(set_def_list_type) :: set_def_list
+type(obs_set_def_type) :: obs_set_def
+
+integer :: max_sets, num_obs
+integer :: i, j, obs_set_def_index, unit
+character(len = 129) :: file_name
+
+! Get output filename
+write(*, *) 'Input the filename for output of observation set_def_list'
+read(*, *) file_name
+
+! Initialize a set_def_list; for now need to specify max_sets
+write(*, *) 'Input the number of unique observation sets you might define'
+read(*, *) max_sets
+set_def_list = init_set_def_list(max_sets)
+
+! Loop through to get definitions for each set
+do i = 1, max_sets
+   write(*, *) 'How many observations in set ', i
+   read(*, *) num_obs
+
+! Initialize the obs_set_def
+   obs_set_def = init_obs_set_def(num_obs)
+
+! Build a set of obs_defs and put them into an obs_set_def
+   do j = 1, num_obs
+! Make calls to obs_def to define observations
+      write(*, *) 'Defining observation ', j
+      obs_def = interactive_obs_def()
+! Insert this obs_def into an obs_set
+      call add_obs(obs_set_def, obs_def)
+   end do
+
+! Insert this obs_set_def into the list
+   obs_set_def_index = add_to_list(set_def_list, obs_set_def)
+
+end do
+
+! Output the set_def_list
+unit = open_file(file_name)
+call write_set_def_list(unit, set_def_list)
+close(unit)
+
+! Read and rewrite as test
+unit = open_file(file_name)
+set_def_list = read_set_def_list(unit)
+close(unit)
+
+unit = open_file(file_name)
+call write_set_def_list(unit, set_def_list)
+close(unit)
+
+end program create_obs_defs
