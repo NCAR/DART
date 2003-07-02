18758cbc02b91a2bad79d9050a006d65078a3715
Tim Hoar <thoar@ucar.edu>
2003-07-01 19:49:59 -0600

    much faster vectorized version
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@248 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- matlab/get_ens_series.m
+++ matlab/get_ens_series.m
@@ -1,5 +1,13 @@
 function ens = get_ens_series(fname, state_var_index)
-%GET_ENS_SERIES: Returns array of time series for all members of ensemble for varible
+%GET_ENS_SERIES: Returns matrix of time series for all members of ensemble for a variable
+%
+% the rows of the matrix correspond to time,
+% the columns of the matrix correspond to ensemble members
+%
+% fname = 'Prior_Diag.nc';
+% state_var_index = 3;
+% ens = get_ens_series(fname,state_var_index);
+%
 
 f = netcdf(fname);
 model      = f.model(:);
@@ -14,22 +22,43 @@ close(f);
 % disp(sprintf('get_ens_series: num_copies  is %d',num_copies))
 % disp(sprintf('get_ens_series: num_vars    is %d',num_vars))
 
+metadata    = getnc(fname,'CopyMetaData');           % get all the metadata
+copyindices = strmatch('ensemble member',metadata);  % find all 'member's
+if ( isempty(copyindices) )
+   disp(sprintf('%s has no valid ensemble members',fname))
+   disp('To be a valid ensemble member, the CopyMetaData for the member')
+   disp('must start with the character string ''ensemble member''')
+   disp('None of them in do in your file.')
+   disp(sprintf('%s claims to have %d copies',fname, num_copies))
+   error('netcdf file has no ensemble members.')
+end
+ens_num     = length(copyindices);
+
+% Get the whole thing and then return the ones we want.
+% This is usually not too bad, as there are usually many more
+% ensemble members than "mean" and "spread" (the two members
+% we are NOT interested in for this function).
+
+state_vec = getnc(fname,'state', [-1, -1, state_var_index], ...
+                                 [-1, -1, state_var_index]);
+% getnc always squeezes out the singleton last dimension.
+ens       = state_vec(:,copyindices);
+
+%----------------------------------------------------------------------
+% This was way too slow.
+%----------------------------------------------------------------------
 % Try to loop through each possible ensemble member.
 % As long as we keep generating valid copy_indexes, we keep going.
+%
+%ens_num = 1;
+%copy_index = get_copy_index(fname, 'ensemble member1');
+%while copy_index > 0,
+%   ens(:, ens_num) = get_var_series(fname, copy_index, state_var_index);
+%   ens_num = ens_num + 1;    % advance to next potential member
+%   copy_string = ['ensemble member', num2str(ens_num)];
+%   copy_index = get_copy_index(fname, copy_string);
+%end
+%ens_num = ens_num - 1;
 
-% It would be more efficient to search though the metadata 
-% to collect the useful copy indices and then preallocate
-% storage ...
-
-ens_num = 1;
-copy_index = get_copy_index(fname, 'ensemble member1');
-while copy_index > 0,
-   ens(:, ens_num) = get_var_series(fname, copy_index, state_var_index);
-   ens_num = ens_num + 1;    % advance to next potential member
-   copy_string = ['ensemble member', num2str(ens_num)];
-   copy_index = get_copy_index(fname, copy_string);
-end
-
-% disp(sprintf('Total number of ensemble members is %d', ens_num - 1));
-disp(sprintf('Read %d ensemble members for state variable %d', ...
-             ens_num-1, state_var_index));
+disp(sprintf('Read %d ensemble members for variable %d in %s', ...
+             ens_num, state_var_index,fname));
