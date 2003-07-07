243b5a2606be8af406e00a44d6a34b0a3b1d3b72
Tim Hoar <thoar@ucar.edu>
2003-07-07 17:08:06 -0600

    doh!
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@255 dfa8782c-da17-4c45-ba5c-5625b50a00d6

---
+++ matlab/SetVariableID.m
@@ -0,0 +1,28 @@
+function varid = SetVariableID(vars);
+% SetVariableID   queries the 
+%
+%
+
+switch lower(vars.model)
+
+   case 'lorenz_96'
+
+      % query to see if these are OK, if not ...
+
+      disp(sprintf('\nUsing state variable IDs %s ', ...
+                      num2str(vars.def_state_vars)))
+      disp('If these are OK, <cr>;')
+      disp(sprintf('If not, please enter array of state variable ID''s (between %d and %d)',vars.min_state_var, vars.max_state_var))
+      IDstring = input('(no syntax required)\n','s');
+      if isempty(IDstring)
+         varid = vars.def_state_vars;
+      else
+         varid = str2num(IDstring);
+      end 
+
+   otherwise
+
+      % low-order models have no choice.
+      varid = vars.def_state_vars;
+
+end
