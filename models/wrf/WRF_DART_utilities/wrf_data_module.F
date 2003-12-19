b18844503a20a145256b2896a276a99cd475318a
Alain Caya <Alain>
2003-12-19 14:53:53 -0700

    Program to convert DART vertor file into wrfinput format, and vice versa.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@372 dfa8782c-da17-4c45-ba5c-5625b50a00d6

---
+++ models/wrf/WRF_DART_utilities/wrf_data_module.F
@@ -0,0 +1,33 @@
+MODULE wrf_data_module
+TYPE wrf_data
+
+   integer :: ncid  ! netcdf id for file
+   integer :: bt_id, bt, sn_id, sn, we_id, we
+   integer :: u_id, v_id, w_id, ph_id, phb_id, t_id,   &
+                        mu_id, mub_id,                           &
+                        qv_id, qc_id, qr_id, qi_id, qs_id, qg_id 
+   integer :: ptop_id
+   logical :: ice_micro
+
+
+!---
+!  arrays for data
+
+   real, pointer :: u(:,:,:)
+   real, pointer :: v(:,:,:)
+   real, pointer :: w(:,:,:)
+   real, pointer :: ph(:,:,:)
+   real, pointer :: phb(:,:,:)
+   real, pointer :: t(:,:,:)
+   real, pointer :: qv(:,:,:)
+   real, pointer :: qc(:,:,:)
+   real, pointer :: qr(:,:,:)
+   real, pointer :: qi(:,:,:)
+   real, pointer :: qs(:,:,:)
+   real, pointer :: qg(:,:,:)
+   real, pointer :: mu(:,:)
+   real, pointer :: mub(:,:)
+
+end type
+END MODULE wrf_data_module
+
