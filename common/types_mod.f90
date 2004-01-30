5989e32db8ebb719ecfe35bde2e2388b91f1691f
Alain Caya <Alain>
2004-01-30 11:16:12 -0700

    we add a few decimal to pi and two new parameters.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@375 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- common/types_mod.f90
+++ common/types_mod.f90
@@ -12,7 +12,7 @@ SAVE
 
 !----------------------------------------------------------------------------
 ! Attributes for variable kinds -- no need to rely on -r8 switch in compiler
-! all real variables are 64bit, ditto for all integer variables.  
+! all real variables are 64bit, ditto for all integer variables.
 !----------------------------------------------------------------------------
 ! These are TJH's favorites
 ! integer, parameter :: i4 = SELECTED_INT_KIND(8)
@@ -35,10 +35,12 @@ SAVE
 ! Constants ... 
 !----------------------------------------------------------------------------
 
-real(kind=r8), parameter :: pi = 3.141592654_r8
-! real(kind=r8), parameter :: conv_to_rad = pi / 180.0_r8
+!real(kind=r8), parameter :: pi = 3.141592654_r8
+real(kind=r8), parameter :: pi = 3.1415926535897932346_r8
 real(kind=r8), parameter :: deg2rad = pi / 180.0_r8
 real(kind=r8), parameter :: rad2deg = 180.0_r8 / pi
 
+INTEGER, PARAMETER ::  missing       = -888888
+REAL   , PARAMETER ::  missing_r     = -888888.
 
 END MODULE types_mod
