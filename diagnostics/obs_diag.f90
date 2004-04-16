51f204146fe2ebf64c8cceadd9b173770a167daa
Alain Caya <Alain>
2004-04-16 16:53:44 -0600

    Variable async is now of type integer.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@448 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- diagnostics/obs_diag.f90
+++ diagnostics/obs_diag.f90
@@ -47,10 +47,10 @@ integer               :: lon, lat
 !----------------------------------------------------------------
 ! Namelist input with default values
 !
-integer  :: ens_size    = 20
+integer  :: async = 0, ens_size    = 20
 real(r8) :: cutoff      = 200.0_r8
 real(r8) :: cov_inflate = 1.0_r8
-logical  :: async = .false., start_from_restart = .false., output_restart = .false.
+logical  :: start_from_restart = .false., output_restart = .false.
 integer  :: init_time_days    = -1
 integer  :: init_time_seconds = -1
 ! Control diagnostic output for state variables
