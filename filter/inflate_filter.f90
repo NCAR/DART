706d27122fb5c55268c0c1e4beaa88a6e3835d57
Alain Caya <Alain>
2004-04-19 10:13:29 -0600

    Variable async is now of integer type.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@453 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- filter/inflate_filter.f90
+++ filter/inflate_filter.f90
@@ -103,9 +103,9 @@ real(r8) :: mean_correl
 !----------------------------------------------------------------
 ! Namelist input with default values
 !
-integer :: ens_size = 20
-real(r8) :: cutoff = 200.0, cov_inflate = 1.0_r8
-logical :: async = .false., start_from_restart = .false., output_restart = .false.
+integer :: async = 0, ens_size = 20
+real(r8) :: cutoff = 200.0_r8, cov_inflate = 1.0_r8
+logical :: start_from_restart = .false., output_restart = .false.
 ! if init_time_days and seconds are negative initial time is 0, 0
 ! for no restart or comes from restart if restart exists
 integer :: init_time_days = -1, init_time_seconds = -1
