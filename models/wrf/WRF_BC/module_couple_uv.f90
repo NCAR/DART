71b8df4de30e7ca7d65f38257eb0d8c84495ee9a
Tim Hoar <thoar@ucar.edu>
2004-04-28 17:16:31 -0600

    reverting back to non-r8 non-f90-netcdf interface -- previous version(s?) did not work correctly
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@569 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- models/wrf/WRF_BC/module_couple_uv.f90
+++ models/wrf/WRF_BC/module_couple_uv.f90
@@ -4,125 +4,130 @@
  
 MODULE module_couple_uv
 
-  use        types_mod, only : r8
+! <next four lines automatically updated by CVS, do not edit>
+! $Source$
+! $Revision$
+! $Date$
+! $Author$
 
 CONTAINS
 
 !------------------------------------------------------------------------
 
-  SUBROUTINE couple_uv ( u,  v,  mu, mub,  msfu, msfv, &
+SUBROUTINE couple_uv ( u,  v,  mu, mub,  msfu, msfv, &
                        ids, ide, jds, jde, kds, kde )
 
-    IMPLICIT NONE
+   IMPLICIT NONE
 
-    !--Input data.
+!--Input data.
 
-    INTEGER, INTENT(IN) :: ids, ide, jds, jde, kds, kde
+   INTEGER, INTENT(IN) :: ids, ide, jds, jde, kds, kde
 
-    REAL(r8), DIMENSION(ids:ide+1, jds:jde  , kds:kde), INTENT(INOUT) :: u
-    REAL(r8), DIMENSION(ids:ide  , jds:jde+1, kds:kde), INTENT(INOUT) :: v
+   REAL, DIMENSION(ids:ide+1, jds:jde  , kds:kde), INTENT(INOUT) :: u
+   REAL, DIMENSION(ids:ide  , jds:jde+1, kds:kde), INTENT(INOUT) :: v
 
-    REAL(r8), DIMENSION(ids:ide+1, jds:jde  ),          INTENT(IN   ) :: msfu
-    REAL(r8), DIMENSION(ids:ide  , jds:jde+1),          INTENT(IN   ) :: msfv
+   REAL, DIMENSION(ids:ide+1, jds:jde  ),          INTENT(IN   ) :: msfu
+   REAL, DIMENSION(ids:ide  , jds:jde+1),          INTENT(IN   ) :: msfv
 
-    REAL(r8), DIMENSION(ids:ide  , jds:jde  ),          INTENT(IN   ) :: mu, mub
+   REAL, DIMENSION(ids:ide  , jds:jde  ),          INTENT(IN   ) :: mu, mub
 
-    REAL(r8), ALLOCATABLE, DIMENSION(:, :) :: muu, muv
+   REAL, ALLOCATABLE, DIMENSION(:, :) :: muu, muv
 
-    allocate(muu(ids:ide+1, jds:jde  ))
-    allocate(muv(ids:ide  , jds:jde+1))
+   allocate(muu(ids:ide+1, jds:jde  ))
+   allocate(muv(ids:ide  , jds:jde+1))
 
-    !--couple variables u, v
+!--couple variables u, v
 
-    CALL calc_mu_uv ( mu, mub, muu, muv, &
-         ids, ide, jds, jde )
+   CALL calc_mu_uv ( mu, mub, muu, muv, &
+                     ids, ide, jds, jde )
 
-    CALL couple ( muu, u, msfu, &
-         ids, ide+1, jds, jde, kds, kde )
+   CALL couple ( muu, u, msfu, &
+                 ids, ide+1, jds, jde, kds, kde )
 
-    CALL couple ( muv, v, msfv, &
-         ids, ide, jds, jde+1, kds, kde )
+   CALL couple ( muv, v, msfv, &
+                 ids, ide, jds, jde+1, kds, kde )
 
-    deallocate(muu)
-    deallocate(muv)
+   deallocate(muu)
+   deallocate(muv)
 
-  END SUBROUTINE couple_uv
+END SUBROUTINE couple_uv
 
 !-------------------------------------------------------------------------------
 
-  SUBROUTINE calc_mu_uv ( mu, mub, muu, muv, &
+SUBROUTINE calc_mu_uv ( mu, mub, muu, muv, &
                         ids, ide, jds, jde )
 
-    IMPLICIT NONE
+   IMPLICIT NONE
 
-    !--Input data
+!--Input data
 
-    INTEGER, INTENT(IN) :: ids, ide, jds, jde
+   INTEGER, INTENT(IN) :: ids, ide, jds, jde
 
-    REAL(r8), DIMENSION(ids:ide, jds:jde),     INTENT(IN   ) :: mu,  mub
+   REAL, DIMENSION(ids:ide, jds:jde),     INTENT(IN   ) :: mu,  mub
 
-    REAL(r8), DIMENSION(ids:ide+1, jds:jde  ), INTENT(  OUT) :: muu
-    REAL(r8), DIMENSION(ids:ide  , jds:jde+1), INTENT(  OUT) :: muv
+   REAL, DIMENSION(ids:ide+1, jds:jde  ), INTENT(  OUT) :: muu
+   REAL, DIMENSION(ids:ide  , jds:jde+1), INTENT(  OUT) :: muv
 
-    REAL(r8), DIMENSION(ids-1:ide+1, jds-1:jde+1) :: mut
+   REAL, DIMENSION(ids-1:ide+1, jds-1:jde+1) :: mut
 
-    INTEGER :: i, j
+   INTEGER :: i, j
 
-    DO j=jds,jde
-       DO i=ids,ide
-          mut(i,j) = mu(i,j)+mub(i,j)
-       ENDDO
+   DO j=jds,jde
+      DO i=ids,ide
+         mut(i,j) = mu(i,j)+mub(i,j)
+      ENDDO
 
-       mut(ids-1,j) = mut(ids,j)
-       mut(ide+1,j) = mut(ide,j)
-    ENDDO
+      mut(ids-1,j) = mut(ids,j)
+      mut(ide+1,j) = mut(ide,j)
+   ENDDO
 
-    DO i=ids-1,ide+1
-       mut(i,jds-1)=mut(i,jds)
-       mut(i,jde+1)=mut(i,jde)
-    ENDDO
+   DO i=ids-1,ide+1
+      mut(i,jds-1)=mut(i,jds)
+      mut(i,jde+1)=mut(i,jde)
+   ENDDO
 
-    DO j=jds,jde
-       DO i=ids,ide+1
-          muu(i,j) = 0.5*(mut(i,j)+mut(i-1,j))
-       ENDDO
-    ENDDO
+   DO j=jds,jde
+   DO i=ids,ide+1
+      muu(i,j) = 0.5*(mut(i,j)+mut(i-1,j))
+   ENDDO
+   ENDDO
 
-    DO j=jds,jde+1
-       DO i=ids,ide
-          muv(i,j) = 0.5*(mut(i,j)+mut(i,j-1))
-       ENDDO
-    ENDDO
+   DO j=jds,jde+1
+   DO i=ids,ide
+      muv(i,j) = 0.5*(mut(i,j)+mut(i,j-1))
+   ENDDO
+   ENDDO
 
-  END SUBROUTINE calc_mu_uv
+END SUBROUTINE calc_mu_uv
 
 !-------------------------------------------------------------------------------
 
-  SUBROUTINE couple ( mut, field, msf, &
-       ids, ide, jds, jde, kds, kde )
+SUBROUTINE couple ( mut, field, msf, &
+                    ids, ide, jds, jde, kds, kde )
 
-    IMPLICIT NONE
+   IMPLICIT NONE
 
-    !--Input data
+!--Input data
 
-    INTEGER, INTENT(IN) :: ids, ide, jds, jde, kds, kde
+   INTEGER, INTENT(IN) :: ids, ide, jds, jde, kds, kde
 
-    REAL(r8), DIMENSION(ids:ide, jds:jde),          INTENT(IN   ) :: mut, msf
+   REAL, DIMENSION(ids:ide, jds:jde),          INTENT(IN   ) :: mut, msf
   
-    REAL(r8), DIMENSION(ids:ide, jds:jde, kds:kde), INTENT(INOUT) :: field
+   REAL, DIMENSION(ids:ide, jds:jde, kds:kde), INTENT(INOUT) :: field
   
-    !--Local data
+!--Local data
   
-    INTEGER :: i, j, k
+   INTEGER :: i, j, k
+  
+   DO j=jds,jde
+   DO k=kds,kde
+   DO i=ids,ide
+      field(i,j,k)=field(i,j,k)*mut(i,j)/msf(i,j)
+   ENDDO
+   ENDDO
+   ENDDO
 
-    DO j=jds,jde
-       DO k=kds,kde
-          DO i=ids,ide
-             field(i,j,k)=field(i,j,k)*mut(i,j)/msf(i,j)
-          ENDDO
-       ENDDO
-    ENDDO
-
-  END SUBROUTINE couple
+END SUBROUTINE couple
 
 END MODULE module_couple_uv
+
