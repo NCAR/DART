f9f174949cc4191fc7e29dd359cb7039aa6ed589
Jeff Anderson <jla@ucar.edu>
2004-05-06 10:37:37 -0600

    Version 2 obs_model_mod which includes get_close and get_expected_obs.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@613 dfa8782c-da17-4c45-ba5c-5625b50a00d6

---
+++ obs_model/obs_model_mod.f90
@@ -0,0 +1,166 @@
+! Data Assimilation Research Testbed -- DART
+! Copyright 2004, Data Assimilation Initiative, University Corporation for Atmospheric Research
+! Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+
+module obs_model_mod
+
+! <next four lines automatically updated by CVS, do not edit>
+! $Source$
+! $Revision$
+! $Date$
+! $Author$
+
+use       types_mod, only : r8
+use   utilities_mod, only : register_module
+use    location_mod, only : location_type, interactive_location
+use assim_model_mod, only : interpolate, &
+                            am_get_close_states => get_close_states
+use    obs_kind_mod, only : obs_kind_type, interactive_kind, get_obs_kind
+use obs_sequence_mod, only : obs_sequence_type, obs_type, get_obs_from_key, &
+   get_obs_def, init_obs, destroy_obs
+use obs_def_mod, only : obs_def_type, get_obs_def_location, get_obs_def_kind
+
+
+implicit none
+private
+
+public take_obs, interactive_def, get_expected_obs, get_close_states
+
+! CVS Generated file description for error handling, do not edit
+character(len=128) :: &
+source   = "$Source$", &
+revision = "$Revision$", &
+revdate  = "$Date$"
+
+
+logical, save :: module_initialized = .false.
+
+contains
+
+!======================================================================
+
+subroutine initialize_module
+
+   call register_module(source, revision, revdate)
+   module_initialized = .true.
+
+end subroutine initialize_module
+
+
+
+
+
+function take_obs(state_vector, location, obs_kind)
+!--------------------------------------------------------------------
+!
+
+implicit none
+
+real(r8) :: take_obs
+real(r8), intent(in) :: state_vector(:)
+type(location_type), intent(in) :: location
+type(obs_kind_type), intent(in) :: obs_kind
+
+if ( .not. module_initialized ) call initialize_module
+
+! Initially, have only raw state observations implemented so obs_kind is
+! irrelevant. Just do interpolation and return.
+
+take_obs = interpolate(state_vector, location, get_obs_kind(obs_kind))
+
+end function take_obs
+
+
+
+subroutine interactive_def(location, obs_kind)
+!---------------------------------------------------------------------
+!
+! Used for interactive creation of an obs_def. For now, an observation
+! is uniquely defined by a location and a kind. The obs_model level
+! knows how kinds and locations need to be combined if there are any
+! intricacies (although for models to date there are none, 17 April, 2002).
+
+implicit none
+
+type(location_type), intent(out) :: location
+type(obs_kind_type), intent(out) :: obs_kind
+
+if ( .not. module_initialized ) call initialize_module
+
+call interactive_location(location)
+call interactive_kind(obs_kind)
+
+end subroutine interactive_def
+
+
+subroutine get_expected_obs(seq, keys, state, obs_vals)
+!---------------------------------------------------------------------------
+! Compute forward operator for set of obs in sequence
+
+type(obs_sequence_type), intent(in) :: seq
+integer, intent(in) :: keys(:)
+real(r8), intent(in) :: state(:)
+real(r8), intent(out) :: obs_vals(:)
+
+integer :: num_obs, i
+type(location_type) :: location
+type(obs_kind_type) :: obs_kind
+type(obs_type) :: obs
+type(obs_def_type) :: obs_def
+
+if ( .not. module_initialized ) call initialize_module
+
+num_obs = size(keys)
+
+! Initialize the observation type
+!!! Can actually init with the correct size here if wanted
+call init_obs(obs, 0, 0)
+
+do i = 1, num_obs
+   call get_obs_from_key(seq, keys(i), obs)
+   call get_obs_def(obs, obs_def)
+   location = get_obs_def_location(obs_def)
+   obs_kind = get_obs_def_kind(obs_def)
+   obs_vals(i) = take_obs(state, location, obs_kind)
+end do
+
+! Need to destroy this obs to free up allocated space
+call destroy_obs(obs)
+
+end subroutine get_expected_obs
+
+
+
+subroutine get_close_states(seq, key, radius, numinds, indices, dist)
+!------------------------------------------------------------------------
+!
+
+type(obs_sequence_type), intent(in) :: seq
+integer, intent(in) :: key
+real(r8), intent(in) :: radius
+integer, intent(out) :: numinds, indices(:)
+real(r8), intent(out) :: dist(:)
+
+type(obs_type) :: obs
+type(obs_def_type) :: obs_def
+type(location_type) :: location
+
+if ( .not. module_initialized ) call initialize_module
+
+! Initialize the observation type
+!!! Can actually init with the correct size here if wanted
+call init_obs(obs, 0, 0)
+
+! Get the location of the obs and pass through to assim_model interface
+call get_obs_from_key(seq, key, obs)
+call get_obs_def(obs, obs_def)
+location = get_obs_def_location(obs_def)
+
+call am_get_close_states(location, radius, numinds, indices, dist)
+
+!Need to destroy this obs to free up allocated space
+call destroy_obs(obs)
+
+end subroutine get_close_states
+
+end module obs_model_mod
