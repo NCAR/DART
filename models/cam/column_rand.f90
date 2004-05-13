99f490139e4f5bf5afb8885bc2beae0ac99d47ac
Jeff Anderson <jla@ucar.edu>
2004-05-13 13:44:26 -0600

    update for obs sequence version 2
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@659 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- models/cam/column_rand.f90
+++ models/cam/column_rand.f90
@@ -25,22 +25,16 @@ revision = "$Revision$", &
 revdate  = "$Date$"
 
 integer, allocatable :: levels(:)
-integer  :: num_sets, level, num_cols, num_levs, i, j, iunit
+integer  :: level, num_cols, num_levs, i, j, iunit
 real(r8) :: lat, lon, t_err_var, uv_err_var, ps_err_var, q_err_var
 type(random_seq_type) :: r
 
 ! Initialize the random sequence
 call init_random_seq(r)
 
-! Set up constants
-write(*, *) 'Input number of sets'
-read(*, *) num_sets
-
 ! Open an output file and write header info
 iunit = get_unit()
 open(unit = iunit, file = 'cam_column_rand.out')
-write(iunit, *) 'set_def.out'
-write(iunit, *) num_sets
 
 write(*, *) 'input the number of columns per set'
 read(*, *) num_cols
@@ -54,6 +48,14 @@ do i = 1, num_levs
    read(*, *) levels(i)
 end do
 
+! Output the total number of obs in set; Q is being observed, too
+write(*, *) 'total num is ', num_cols * (num_levs * 4 + 1)
+write(iunit, *) num_cols * (num_levs * 4 + 1)
+
+! No copies or qc
+write(iunit, *) 0
+write(iunit, *) 0
+
 ! First get error variance for surface pressure
 write(*, *) 'Input error VARIANCE for surface pressure obs'
 read(*, *) ps_err_var
@@ -66,82 +68,85 @@ read(*, *) uv_err_var
 write(*, *) 'Input error VARIANCE for Q obs'
 read(*, *) q_err_var
 
-! Loop through each set
-do j = 1, num_sets
 
-   ! Output the total number of obs in set; Q is being observed, too
-   write(*, *) 'total num is ', num_cols * (num_levs * 4 + 1)
-   write(iunit, *) num_cols * (num_levs * 4 + 1)
+! Loop through each column
+do i = 1, num_cols
 
-   ! Loop through each column
-   do i = 1, num_cols
+   ! Get a random lon lat location for this column
+   ! Longitude is random from 0 to 360
+   lon = random_uniform(r) * 360.0_r8
 
-      ! Get a random lon lat location for this column
-      ! Longitude is random from 0 to 360
-      lon = random_uniform(r) * 360.0_r8
+   ! Latitude must be area weighted
+   lat = asin(random_uniform(r) * 2.0_r8 - 1.0_r8)
 
-      ! Latitude must be area weighted
-      lat = asin(random_uniform(r) * 2.0_r8 - 1.0_r8)
+   ! Now convert from radians to degrees latitude
+   lat = lat * 360.0_r8 / (2.0_r8 * PI)
 
-      ! Now convert from radians to degrees latitude
-      lat = lat * 360.0_r8 / (2.0_r8 * PI)
+   ! Do ps ob
+   write(iunit, *) 0
+   ! Kind for surface pressure is 3
+   write(iunit, *) 3
+   write(iunit, *) 1
+   ! Level is -1 for ps
+   write(iunit, *) -1
+   write(iunit, *) lon
+   write(iunit, *) lat
+   write(iunit, *) 0, 0
+   write(iunit, *) ps_err_var
 
-      ! Do ps ob
-      write(iunit, *) ps_err_var
-      write(iunit, *) -1
+   ! Loop through each observation in the column
+   do level = 1, num_levs
 
-      ! Level is -1 for ps
-      write(iunit, *) -1
+      ! Write out the t observation
+      write(iunit, *) 0
+      ! Kind for t is 4
+      write(iunit, *) 4
+      write(iunit, *) 1
+      write(iunit, *) levels(level)
       write(iunit, *) lon
       write(iunit, *) lat
+      write(iunit, *) 0, 0
+      write(iunit, *) t_err_var
 
-      ! Kind for surface pressure is 3
-      write(iunit, *) 3
 
-      ! Loop through each observation in the column
-      do level = 1, num_levs
+      ! Write out the u observation
+      write(iunit, *) 0
+      ! Kind for u is 1
+      write(iunit, *) 1
+      write(iunit, *) 1
+      write(iunit, *) levels(level)
+      write(iunit, *) lon
+      write(iunit, *) lat
+      write(iunit, *) 0, 0
+      write(iunit, *) uv_err_var
 
-         ! Write out the t observation
-         write(iunit, *) t_err_var
-         write(iunit, *) -1
-         write(iunit, *) levels(level)
-         write(iunit, *) lon
-         write(iunit, *) lat
 
-         ! Kind for t is 4
-         write(iunit, *) 4
+      ! Write out the v observation
+      write(iunit, *) 0
+      ! Kind for v is 2
+      write(iunit, *) 2
+      write(iunit, *) 1
+      write(iunit, *) levels(level)
+      write(iunit, *) lon
+      write(iunit, *) lat
+      write(iunit, *) 0, 0
+      write(iunit, *) uv_err_var
 
-         ! Write out the u observation
-         write(iunit, *) uv_err_var
-         write(iunit, *) -1
-         write(iunit, *) levels(level)
-         write(iunit, *) lon
-         write(iunit, *) lat
 
-         ! Kind for u is 1
-         write(iunit, *) 1
+      ! Write out the q observation
+      write(iunit, *) 0
+      ! Kind for q is 5
+      write(iunit, *) 5
+      write(iunit, *) 1
+      write(iunit, *) levels(level)
+      write(iunit, *) lon
+      write(iunit, *) lat
+      write(iunit, *) 0, 0
+      write(iunit, *) q_err_var
 
-         ! Write out the v observation
-         write(iunit, *) uv_err_var
-         write(iunit, *) -1
-         write(iunit, *) levels(level)
-         write(iunit, *) lon
-         write(iunit, *) lat
-
-         ! Kind for v is 2
-         write(iunit, *) 2
-
-         ! Write out the q observation
-         write(iunit, *) q_err_var
-         write(iunit, *) -1
-         write(iunit, *) levels(level)
-         write(iunit, *) lon
-         write(iunit, *) lat
-
-         ! Kind for q is 5???
-         write(iunit, *) 5
-      end do
    end do
 end do
+
+write(iunit, *) 'set_def.out'
 
 end program column_rand
