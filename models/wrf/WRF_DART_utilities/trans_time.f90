6033006e6878cc9caec9af9287e0f215abb305ff
Alain Caya <Alain>
2004-05-14 11:34:50 -0600

    Cosmetic.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@666 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- models/wrf/WRF_DART_utilities/trans_time.f90
+++ models/wrf/WRF_DART_utilities/trans_time.f90
@@ -10,7 +10,7 @@ program trans_time
 ! Note: This needs to be done BEFORE update_wrf_bc
 !
 ! author: Kevin Raeder 8/1/03
-! mods for WRF: Josh Hacker 4/16/03
+! mods for WRF: Josh Hacker 4/16/04
 !
 !----------------------------------------------------------------------
 
@@ -20,21 +20,20 @@ use time_manager_mod, only : time_type, read_time, write_time, &
                              print_time, set_date, operator (+), &
                              set_calendar_type, NO_CALENDAR, NOLEAP, &
                              GREGORIAN
-use assim_model_mod, only : static_init_assim_model, binary_restart_files
+use assim_model_mod,  only : static_init_assim_model, binary_restart_files
 
-use model_mod, only: get_wrf_date, set_wrf_date, output_wrf_time
+use model_mod,        only : get_wrf_date, set_wrf_date, output_wrf_time
 
 
-use utilities_mod, only : get_unit
+use utilities_mod,    only : get_unit
 
 implicit none
 
-integer               :: ntimes = 2, n, nhtfrq, &
-                         calendar_type = GREGORIAN
-integer               :: file_unit, seconds, days, &
-                         year, month, day, hour, minute, second, &
-                         cam_date, cam_tod
-type(time_type)       :: dart_time(2), forecast_length, wrf_time
+integer               :: calendar_type = GREGORIAN
+integer               :: file_unit, &
+                         year, month, day, hour, minute, second
+
+type(time_type)       :: dart_time(2), wrf_time
 character (len = 128) :: file_name = 'dart_wrf_vector'
 character (len = 16)  :: file_form
 
@@ -52,9 +51,6 @@ endif
 file_unit = get_unit()
 
 open(unit = file_unit, file = file_name, form=file_form)
-! end time is first, then beginning time
-!  -namelist "&camexp START_YMD=$times[3] START_TOD=$times[4] \
-!                     STOP_YMD=$times[1] STOP_TOD=$times[2] NHTFRQ=$times[5] /" \
 
 ! this should be the init time
 call get_wrf_date(year, month, day, hour, minute, second)
