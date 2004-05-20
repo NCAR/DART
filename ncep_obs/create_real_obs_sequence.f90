956910eb956f70636706cf72747012e37e1450c0
Hui Liu <Hui>
2004-05-20 15:58:48 -0600

    BUFR converter for new (post-fiji) observation mechanism
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@696 dfa8782c-da17-4c45-ba5c-5625b50a00d6

---
+++ ncep_obs/create_real_obs_sequence.f90
@@ -0,0 +1,21 @@
+program create_real_obs_sequence
+
+use obs_sequence_mod, only : obs_sequence_type, write_obs_seq, &
+                             real_obs_sequence, static_init_obs_sequence
+
+type(obs_sequence_type) :: seq
+character(len = 129) :: file_name = 'obs_seq.out'
+
+! Initialize the obs_sequence module
+!
+  call static_init_obs_sequence()
+
+! Creation of an real observation sequence
+!
+  seq = real_obs_sequence()
+
+! Write the sequence to a file
+!
+  call write_obs_seq(seq, file_name)
+
+end program create_real_obs_sequence
