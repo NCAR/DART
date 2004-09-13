5b04ab1f98423e5a67517ef084bb91a8c1876105
Kevin Raeder <raeder@ucar.edu>
2004-09-13 13:25:18 -0600

    Move restart file format to assim_model_mod.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@900 dfa8782c-da17-4c45-ba5c-5625b50a00d6

--- models/cam/trans_sv_pv.f90
+++ models/cam/trans_sv_pv.f90
@@ -12,7 +12,7 @@ program trans_sv_pv
 !----------------------------------------------------------------------
 ! purpose: interface between CAM and DART
 !
-! method: Read DART state vector ("proprietary" format)
+! method: Read DART state vector ("proprietary" format), but not time(s).
 !         Reform state vector back into CAM fields.
 !         Replace those fields on the CAM initial file with the new values,
 !         preserving all other information on the file.
@@ -25,11 +25,10 @@ program trans_sv_pv
 use       types_mod, only : r8
 use   utilities_mod, only : get_unit
 use       model_mod, only : model_type, init_model_instance, write_cam_init, &
-   vector_to_prog_var
+   vector_to_prog_var 
 use assim_model_mod, only : assim_model_type, static_init_assim_model, &
    init_assim_model, get_model_size, get_model_state_vector, read_state_restart, &
    open_restart_read, close_restart
-! Guam clean out advance_model
 use time_manager_mod, only : time_type, read_time
 
 implicit none
@@ -47,7 +46,6 @@ type(time_type)        :: adv_to_time
 real(r8), allocatable  :: x_state(:)
 integer                :: file_unit, x_size
 character (len = 128)  :: file_name = 'caminput.nc', file_in = 'temp_ic'
-character (len = 16)   :: file_form
 
 ! Static init assim model calls static_init_model
 PRINT*,'static_init_assim_model in trans_sv_pv'
@@ -58,15 +56,10 @@ call init_assim_model(x)
 call init_model_instance(var)
 
 ! Get file for DART vector input
-! debug file_unit = 17
 file_unit = open_restart_read(file_in)
-PRINT*,'In trans_sv_pv file_in unit  = ',file_unit
-PRINT*,' '
 
-! Guam clean out advance_model
-! Read in time to which CAM must advance.  
-! Neither this, nor time in x is used in this program
-! read in state vector from DART
+! read in state vector from DART and throwing away the time(s)
+! since those are handled by trans_time.f90
 call read_state_restart(x, file_unit, adv_to_time)
 call close_restart(file_unit)
 
