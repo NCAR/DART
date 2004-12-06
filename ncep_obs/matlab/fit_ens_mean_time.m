cb0ae681839acb3e2d481c7299b2911fe27340ae
Tim Hoar <thoar@ucar.edu>
2004-12-06 15:51:04 -0700

    Added version control pieces, cosmetic changes to internal plotting routine.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1031 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: d6172c5bb617bda861aa91397083588e9669657f

--- ncep_obs/matlab/fit_ens_mean_time.m
+++ ncep_obs/matlab/fit_ens_mean_time.m
@@ -1,87 +1,99 @@
-function fit_ens_mean_time
-
-p=load('Tanl_times_level.dat');
-level=p(1);
-
-   figure(1); clf
-%  orient landscape
-   orient portrait
-   wysiwyg
-
-   ges   = sprintf('Tges_times_%04dmb.dat',level);
-   anl   = sprintf('Tanl_times_%04dmb.dat',level);
-
-   region = 'NH';
-   top   = sprintf('T fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,1)
-   
-   region = 'SH';
-   top   = sprintf('T fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,2)
-   
-   region = 'TR';
-   top   = sprintf('T fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,3)
-   
-   region = 'NA';
-   top   = sprintf('T fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,4)
-   
-   % Now for the Winds
-   figure(2); clf
-%  orient landscape
-   orient portrait
-   wysiwyg
-   
-   ges   = sprintf('Wges_times_%04dmb.dat',level);
-   anl   = sprintf('Wanl_times_%04dmb.dat',level);
-   region = 'NH';
-   top   = sprintf('Wind fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,1)
-   
-   region = 'SH';
-   top   = sprintf('Wind fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,2)
-   
-   region = 'TR';
-   top   = sprintf('Wind fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,3)
-   
-   region = 'NA';
-   top   = sprintf('Wind fit to RAobs ens mean %04dmb %s',level,region);
-   Myplot(ges,anl,top,4)
-
-   str = '-dpsc ';
-
-   print(1,str,'t_ens_mean_time.ps');
-   print(2,str,'w_ens_mean_time.ps');
-
-% end
-
-
-
-function Myplot(file1,file2,top,region)
-
-p=load(file1);
-a=load(file2);
-xp=p(:,1);
-xa=a(:,1);
+function fit_ens_mean_time(ddir)
+% fit_ens_mean_time(ddir)
 %
-count=2+(region-1)*3;
-yp=p(:,count);
-ya=a(:,count);
+% ddir     is an optional argument specifying the directory containing
+%               the data files as preprocessed by the support routines.
 %
-subplot(2,2,region)
-plot(xp,yp,'c+-',xa,ya,'ro:')
-grid
-%if region > 2 
-xlabel('Time interval', 'fontsize', 10) ;
-%end
-ylabel('RMSE ', 'fontsize', 10)
-%title({'Fit to RAOBS, ensemble mean',file1,file2}, ...
-%      'fontsize', 10,'Interpreter','none')
-title({top}, ...
-      'fontsize', 10,'Interpreter','none')
-legend('guess', 'analysis')
-h=legend;
-legend(h,'boxoff')
+% USAGE: if the preprocessed data files are in a directory called 'plot'
+%
+% ddir = 'plot';
+% fit_ens_mean_time(ddir)
+
+% Data Assimilation Research Testbed -- DART
+% Copyright 2004, Data Assimilation Initiative, University Corporation for Atmospheric Research
+% Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+
+% $Source$
+% $Revision$
+% $Date$
+% $Id$
+
+% This ensures the directory with the datafiles 
+% is in Matlab's search path.
+
+if ( nargin > 0 )
+   datafile = fullfile(ddir,'Tanl_times_level.dat');
+else
+   datafile = 'Tanl_times_level.dat';
+   ddir = [];
+end	
+
+if ( exist(datafile,'file') == 2 )
+   p     = load(datafile);
+   level = p(1);
+else
+   error(sprintf('%s cannot be found.', datafile))
+end
+
+%----------------------------------------------------------------------
+figure(1); clf; orient landscape;   % Temperature
+%----------------------------------------------------------------------
+
+   % Set up a structure with all the plotting components
+   plotdat.Regions = {'Northern Hemisphere', ...
+                      'Southern Hemisphere', ...
+                      'Tropics', 'North America'};
+   plotdat.level   = level;
+   plotdat.flavor  = 'ens mean';
+   plotdat.xlabel  = 'Time interval';
+   plotdat.ylabel  = 'RMSE';
+   plotdat.ges     = fullfile(ddir,sprintf('Tges_times_%04dmb.dat',level));
+   plotdat.anl     = fullfile(ddir,sprintf('Tanl_times_%04dmb.dat',level));
+   plotdat.varname = 'T';
+
+   plotdat.region = 1; myplot(plotdat);
+   plotdat.region = 2; myplot(plotdat);
+   plotdat.region = 3; myplot(plotdat);
+   plotdat.region = 4; myplot(plotdat);
+
+%----------------------------------------------------------------------
+figure(2); clf; orient landscape;  % Winds
+%----------------------------------------------------------------------
+
+   plotdat.ges     = fullfile(ddir,sprintf('Wges_times_%04dmb.dat',level));
+   plotdat.anl     = fullfile(ddir,sprintf('Wanl_times_%04dmb.dat',level));
+   plotdat.varname = 'Wind';
+
+   plotdat.region = 1; myplot(plotdat);
+   plotdat.region = 2; myplot(plotdat);
+   plotdat.region = 3; myplot(plotdat);
+   plotdat.region = 4; myplot(plotdat);
+
+%----------------------------------------------------------------------
+% common
+%----------------------------------------------------------------------
+
+print(1,'-dpsc','t_ens_mean_time.ps');
+print(2,'-dpsc','w_ens_mean_time.ps');
+
+
+
+function myplot(plotdat)
+p  = load(plotdat.ges); xp = p(:,1);
+a  = load(plotdat.anl); xa = a(:,1);
+
+count = 2+(plotdat.region-1)*3;
+yp    = p(:,count);
+ya    = a(:,count);
+
+subplot(2,2,plotdat.region)
+   plot(xp,yp,'c+-',xa,ya,'ro-','LineWidth',1.5)
+   grid
+   xlabel(plotdat.xlabel, 'fontsize', 10);
+   ylabel(plotdat.ylabel, 'fontsize', 10);
+   string0 = sprintf('%s',plotdat.Regions{plotdat.region});
+   string1 = sprintf('%s fit to RAobs', plotdat.varname);
+   string2 = sprintf('%s %d hPa',plotdat.flavor,plotdat.level);
+   title({string0,string1,string2}, 'fontsize', 12,'FontWeight','bold')
+   h = legend('guess', 'analysis');
+   legend(h,'boxoff')
