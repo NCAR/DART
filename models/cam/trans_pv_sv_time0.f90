20ebbac284197aed81d62feecead836958054c8a
Kevin Raeder <raeder@ucar.edu>
2004-12-08 11:22:30 -0700

    Get time of state vector from namelist (perfect_model_obs_nml) instead of temp_ic. (This removes need to have a filter_ic file in order to create a filter_ic file!) The temp_ud file(s) created can be used directly as filter_ic.# files if single_restart_file = .false.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1037 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 718249b063a1474aff05a61f20d218d72e80ed4a

--- models/cam/trans_pv_sv_time0.f90
+++ models/cam/trans_pv_sv_time0.f90
@@ -22,7 +22,7 @@ program trans_pv_sv_time0
 !----------------------------------------------------------------------
 
 use        types_mod, only : r8
-use    utilities_mod, only : get_unit
+use    utilities_mod, only : get_unit, file_exist, open_file, close_file
 use        model_mod, only : model_type, init_model_instance, read_cam_init, &
                              prog_var_to_vector
 use  assim_model_mod, only : assim_model_type, static_init_assim_model, &
@@ -30,7 +30,7 @@ use  assim_model_mod, only : assim_model_type, static_init_assim_model, &
    set_model_time, open_restart_read, open_restart_write, close_restart, &
    aread_state_restart
 ! Guam; move time stripping from advance_model to here
-use time_manager_mod, only : time_type, read_time
+use time_manager_mod, only : time_type, read_time, set_time
 
 implicit none
 
@@ -40,17 +40,38 @@ source   = "$Source$", &
 revision = "$Revision$", &
 revdate  = "$Date$"
 
-! character (len = 128) :: file_name = 'caminput.nc', file_out = 'temp_ic'
-! Guam; move time stripping from script into here
-character (len = 128) :: file_name = 'caminput.nc', file_out = 'temp_ud', &
-                         file_time = 'temp_ic'
+character (len = 128) :: file_name = 'caminput.nc', file_out = 'temp_ud'
+! Hawaii;                          file_time = 'temp_ic'
+! trans_pv_sv_time0 should get it's time from the namelist, 
+! not from temp_ic, which came from filter_ics, which will not exist for a new
+! set of fields comprising the state vector.
 
 ! Temporary allocatable storage to read in a native format for cam state
 type(assim_model_type) :: x
 type(model_type)       :: var
 type(time_type)        :: model_time, adv_to_time
 real(r8), allocatable  :: x_state(:), x_temp(:)
-integer                :: file_unit, x_size
+integer                :: file_unit, x_size, iunit, io
+
+!-------------------------
+! define junk for reading in whole namelist.
+logical :: start_from_restart = .false., output_restart = .false.
+integer :: async = 0
+integer :: init_time_days = 0, init_time_seconds = 0, output_interval = 1
+character(len = 129) :: restart_in_file_name  = 'perfect_ics',     &
+                        restart_out_file_name = 'perfect_restart', &
+                        obs_seq_in_file_name  = 'obs_seq.in',      &
+                        obs_seq_out_file_name = 'obs_seq.out',     &
+                        adv_ens_command       = './advance_ens.csh'
+
+
+! namelist /filter_nml/init_time_days, init_time_seconds
+namelist /perfect_model_obs_nml/ async, adv_ens_command, obs_seq_in_file_name, &
+   obs_seq_out_file_name, start_from_restart, output_restart, &
+   restart_in_file_name, restart_out_file_name, init_time_days, init_time_seconds, &
+   output_interval
+
+!-------------------------
 
 ! Static init assim model calls static_init_model
 PRINT*,'static_init_assim_model in trans_pv_sv'
@@ -75,11 +96,26 @@ call prog_var_to_vector(var, x_state)
 ! Put this in the structure
 call set_model_state_vector(x, x_state)
 
-! Guam; move time stripping from advance_model into here
-file_unit = open_restart_read(file_time)
-call aread_state_restart(model_time, x_temp, file_unit, adv_to_time)
-! for these time 0 _ud files we want the model time (0) to be written,
-! not the target time
+! Set model_time from the namelist, rather than temp_ic as is done in trans_pv_sv
+if(file_exist('input.nml')) then
+   iunit = open_file('input.nml', action = 'read')
+!   ierr = 1
+!   do while(ierr /= 0)
+!!Either an i/o list or a namelist may be specified but not both
+!!      read(iunit, nml = filter_nml, iostat = io, end = 11) init_time_seconds
+!      read(iunit, iostat = io, end = 11) init_time_days
+!      read(iunit, iostat = io, end = 11) init_time_seconds
+      read(iunit, nml = perfect_model_obs_nml, iostat = io, end = 11) 
+
+      PRINT*,'init_time_days and _seconds = ',init_time_days, init_time_seconds 
+!      ierr = check_nml_error(io, 'filter_nml')
+!   enddo
+ 11 continue
+   call close_file(iunit)
+endif
+
+call filter_set_initial_time
+
 call set_model_time (x, model_time)
 call close_restart(file_unit)
 
@@ -92,4 +128,22 @@ PRINT*,' '
 call write_state_restart(x, file_unit)
 call close_restart(file_unit)
 
+contains
+
+! WARNING: THERE IS SOME DANGER IN USING THESE SCOPED SUBROUTINES
+!==========================================================================
+
+!-------------------------------------------------------------------------
+subroutine filter_set_initial_time
+
+if(init_time_days >= 0) then
+   model_time = set_time(init_time_seconds, init_time_days)
+else
+   model_time = set_time(0, 0)
+endif
+
+end subroutine filter_set_initial_time
+
+!-------------------------------------------------------------------------
 end program trans_pv_sv_time0
+
