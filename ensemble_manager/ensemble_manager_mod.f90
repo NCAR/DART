054fba984f7e99c5eeab247e7ebb8038777755b1
Jeff Anderson <jla@ucar.edu>
2004-12-09 10:48:54 -0700

    Sequential file access to ensembles, used by KDR with CAM for previous 2 months
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1045 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 0935eb5f3bdfa0f2a631a3951f0e81ad3498e511

--- ensemble_manager/ensemble_manager_mod.f90
+++ ensemble_manager/ensemble_manager_mod.f90
@@ -11,7 +11,7 @@ use    utilities_mod, only :  get_unit, open_file, close_file, register_module, 
                               E_ERR, E_WARN, E_MSG, E_DBG, initialize_utilities, &
                               logfileunit, timestamp
 use assim_model_mod, only : aread_state_restart, awrite_state_restart, open_restart_read, &
-   open_restart_write, close_restart, assim_model_type, get_model_time_step, adv_1step
+   open_restart_write, close_restart, get_model_time_step, adv_1step
 use time_manager_mod, only : time_type, get_time, read_time, write_time, get_calendar_type, &
                              THIRTY_DAY_MONTHS, JULIAN, GREGORIAN, NOLEAP, NO_CALENDAR, &
                              operator(<), operator(>), operator(+), operator(-), &
@@ -43,21 +43,27 @@ logical :: mean_allocated = .false., spread_allocated = .false.
 ! Global in core storage for the ensemble
 real(r8), allocatable :: ens(:, :), ens_mean(:), ens_spread(:)
 type(time_type), allocatable :: ens_time(:)
-integer :: ens_size, model_size, req_rec_length
+integer :: ens_size, model_size
 
-integer :: direct_unit
+integer :: seq_unit
 character(len = 129) :: errstring
+! File name for the temporary files; has extensions added
+character(len = 21) :: seq_file_name = 'ens_manager_temp_file'
 
 !-----------------------------------------------------------------
 !
 ! namelist with default values
 
-logical :: in_core = .true.
 ! If true, then ensemble is held in core storage; if false, only
 ! a single ensemble member worth of storage is used and ensemble
 ! is kept in a direct access temporary file.
+logical :: in_core = .true.
+! If true a single restart file containing all ensemble states is read/written
+! If false, one restart file is used for each ensemble member
+logical :: single_restart_file_in = .true.
+logical :: single_restart_file_out = .true.
 
-namelist / ensemble_manager_nml / in_core
+namelist / ensemble_manager_nml / in_core, single_restart_file_in, single_restart_file_out
 
 !-----------------------------------------------------------------
 
@@ -73,9 +79,9 @@ integer, intent(in)                         :: ens_size_in, model_size_in
 character(len = 129), intent(out), optional :: file_name
 type(time_type), intent(in), optional       :: init_time
 
-real(r8) :: x
-integer :: iunit, i, j, ilength, rlength, rnum, ierr, io
-character(len = 129) :: msgstring
+integer :: iunit, i, ierr, io
+character(len = 129) :: msgstring, this_file_name
+character(len = 4) :: extension
 
 ! Initialize the module with utilities 
 call register_module(source, revision, revdate)
@@ -101,48 +107,53 @@ endif
 call error_handler(E_MSG,'init_ensemble_manager','ensemble_manager_nml values are',' ',' ',' ')
 write(logfileunit,nml=ensemble_manager_nml)
 write(     *     ,nml=ensemble_manager_nml)
-
-if(.not. in_core) then
-   ! Find out the required record length for real(r8)
-   inquire(iolength = req_rec_length) x
-
-   ! Open the direct access file for on disk ensemble
-   direct_unit = get_unit()
-   open(unit = direct_unit, file = "temp_ens_file", access = 'direct', &
-      status = 'replace', form = 'unformatted', recl = req_rec_length)
-endif
-
 ! Initialize the storage and read in from restart file if needed
 if(in_core) then 
    ! Don't allocate space for mean and spread until needed to conserve space
    allocate(ens(ens_size, model_size), ens_time(-1:ens_size))
 else
    ! For out of core will only have storage for one ensemble member
-   ! Could make it so this was only used in this routine if really desparate for space
    allocate(ens(1, model_size), ens_time(-1:ens_size))
 endif
 
+! If a file_name is present, read in ensemble from (a) restart file(s)
 if(present(file_name)) then
-   iunit = open_restart_read(file_name)
+   if(single_restart_file_in) iunit = open_restart_read(file_name)
 
    do i = 1, ens_size
+      ! If multiple restart files, open one for each ensemble member
+      if(.not. single_restart_file_in) then
+         write(extension, 99) i
+         99 format(i4.4)
+         this_file_name = trim(file_name) // '.' // extension
+         iunit = open_restart_read(this_file_name)
+      endif
+
       write(msgstring, *) 'trying to read restart ', i
       call error_handler(E_DBG,'init_ensemble_manager',msgstring,source,revision,revdate)
+
+      ! If in core, can just read the ensemble members straight into storage
       if(in_core) then 
          call aread_state_restart(ens_time(i), ens(i, :), iunit)
       else
+         ! If out of core, read in from restart file and write out to temp file
          call aread_state_restart(ens_time(i), ens(1, :), iunit)
-         ! Write out the ensemble to direct access file
-         do j = 1, model_size
-            rnum = (i - 1) * model_size + j
-            write(direct_unit, rec = rnum) ens(1, j) 
-         end do
+         ! Open the file for this ensemble member
+         call get_seq_file_name(this_file_name, i)
+         seq_unit = get_unit()
+         open(unit = seq_unit, file = this_file_name, access = 'sequential', &
+            status = 'replace', form = 'unformatted')
+         write(seq_unit) ens(1, :)
+         close(seq_unit)
       endif
 
       ! If the inital file time is being overridden, do it
       if(present(init_time)) ens_time(i) = init_time
+
+      if(.not. single_restart_file_in) call close_restart(iunit)
    end do
-   call close_restart(iunit)
+   
+   if(single_restart_file_in) call close_restart(iunit)
 endif
 
 end subroutine init_ensemble_manager
@@ -156,34 +167,33 @@ integer, intent(in) :: index
 real(r8), intent(out) :: member(:)
 type(time_type), intent(out) :: mtime
 
-integer :: i, rnum
-
+! Return one member of an ensemble by index
 ! Index 0 gets ensemble mean, index -1 gets ensemble spread
+
+character(len = 129) :: this_file_name
 
 if(index < -1 .or. index > ens_size) then
    write(errstring, *) 'index out of range'
    call error_handler(E_ERR,'get_ensemble_member', errstring, source, revision, revdate)
 endif
 
+! For in memory, just copy and return
 if(in_core) then
    if(index > 0) then
       member = ens(index, :)
    else if(index == 0) then
       member = ens_mean
-   else 
+   else if(index == -1) then 
       member = ens_spread
    endif
 else
-   do i = 1, model_size
-      if(index > 0) then
-         rnum = (index - 1) * model_size + i
-      else if(index == 0) then
-         rnum = (ens_size) * model_size + i
-      else if(index == -1) then
-         rnum = (ens_size + 1) * model_size + i
-      endif
-      read(direct_unit, rec = rnum) member(i)
-   end do
+   ! Out of core, need to read in from file
+   call get_seq_file_name(this_file_name, index)
+   ! Open the file and read in the field      
+   seq_unit = get_unit()
+   open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+   read(seq_unit) member
+   close(unit = seq_unit)
 endif
 
 mtime = ens_time(index)
@@ -194,18 +204,22 @@ end subroutine get_ensemble_member
 
 subroutine put_ensemble_member(ens_handle, index, member, mtime)
 
+! Set one member of an ensemble by index
+! Index 0 gets ensemble mean, index -1 gets ensemble spread
+
 type(ensemble_type), intent(in) :: ens_handle
 integer, intent(in) :: index
 real(r8), intent(in) :: member(:)
 type(time_type), intent(in) :: mtime
 
-integer :: i, rnum
+character(len = 129) :: this_file_name
 
 if(index < -1 .or. index > ens_size) then
    write(errstring, *) 'index out of range ', index
    call error_handler(E_ERR,'put_ensemble_member', errstring, source, revision, revdate)
 endif
 
+! For in core, just copy
 if(in_core) then
    if(index > 0) then
       ens(index, :) = member
@@ -215,16 +229,13 @@ if(in_core) then
       ens_spread = member
    endif
 else
-   do i = 1, model_size
-      if(index > 0) then
-         rnum = (index - 1) * model_size + i
-      else if(index == 0) then
-         rnum = ens_size * model_size + i
-      else if(index == -1) then
-         rnum = (ens_size + 1) * model_size + i
-      endif
-      write(direct_unit, rec = rnum) member(i)
-   end do
+   ! For out of core, open file and write out new values
+   call get_seq_file_name(this_file_name, index)
+   ! Open the file and write the field
+   seq_unit = get_unit()
+   open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+   write(seq_unit) member
+   close(unit = seq_unit)
 end if
 
 ens_time(index) = mtime
@@ -234,6 +245,8 @@ end subroutine put_ensemble_member
 !-----------------------------------------------------------------
 
 subroutine get_ensemble_time(ens_handle, index, mtime)
+
+! Returns the time of an ensemble member; 0 for ensemble mean, -1 for spread
 
 type(ensemble_type), intent(in) :: ens_handle
 integer, intent(in) :: index
@@ -252,9 +265,14 @@ end subroutine get_ensemble_time
 
 subroutine update_ens_mean(ens_handle)
 
+! Update the value of the ensemble mean from the current ensemble members
+
 type(ensemble_type), intent(in) :: ens_handle
-integer :: i, j, rnum
-real(r8) :: val
+
+integer :: i, j
+character(len = 129) :: this_file_name
+! Sequential version read efficiency requires having second model_size storage
+real(r8) :: ens_member(model_size)
 
 ! Update the ensemble mean (copy 0) from ensemble members
 if(in_core) then
@@ -267,20 +285,25 @@ if(in_core) then
       ens_mean(i) = sum(ens(1:ens_size, i)) / ens_size
    end do
 else
+   ! Stored on disk, read in each member and sum
    ens = 0.0
    do j = 1, ens_size
-      do i = 1, model_size
-         rnum = (j - 1) * model_size + i
-         read(direct_unit, rec = rnum) val
-         ens(1, i) = ens(1, i) + val
-      end do
+      call get_seq_file_name(this_file_name, j)
+      ! Open the file and read in the field      
+      seq_unit = get_unit()
+      open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+      read(seq_unit) ens_member
+      close(unit = seq_unit)
+      ! Add this member to sum
+      ens(1, :) = ens(1, :) + ens_member
    end do
    ens = ens / ens_size
-   ! Put the ensemble mean into the file
-   do i = 1, model_size
-      rnum = ens_size * model_size + i
-      write(direct_unit, rec = rnum) ens(1, i)
-   end do
+   ! Put the ensemble mean into the file; ensemble mean is 0 member
+   call get_seq_file_name(this_file_name, 0)
+   seq_unit = get_unit()
+   open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+   write(seq_unit) ens
+   close(unit = seq_unit)
 endif
 
 ! Update the ensemble mean time, assume everything else stays in synch
@@ -292,9 +315,14 @@ end subroutine update_ens_mean
 
 subroutine update_ens_mean_spread(ens_handle)
 
+! Update the ensemble spread from the current ensemble members,
+! Also does an update of the ensemble mean
 type(ensemble_type), intent(in) :: ens_handle
-integer :: i, rnum, j
-real(r8) :: temp, temp_spread, temp_mean, ens_element
+
+integer :: i, j
+character(len = 129) this_file_name
+! For efficient disk use now need three copies, if this is too much can alter
+real(r8) :: tens_mean(model_size), tens_spread(model_size)
 
 ! Update both the mean and spread (copy -1) from ensemble members
 call update_ens_mean(ens_handle)
@@ -309,20 +337,31 @@ if(in_core) then
       ens_spread(i) = sqrt(sum((ens(1:, i) - ens_mean(i))**2) / (ens_size - 1))
    end do
 else
+   ! Get the ensemble mean from disk file; mean is copy 0
+   call get_seq_file_name(this_file_name, 0)
+   seq_unit = get_unit()
+   open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+   read(seq_unit) tens_mean
+   close(unit = seq_unit)
+
+   tens_spread = 0.0
    ! Loop through and add squared difference from mean
-   ! This is going to wreak havoc on disks potentially
-   do i = 1, model_size
-      temp_spread = 0.0
-      rnum = ens_size * model_size + i
-      read(direct_unit, rec = rnum) temp_mean
       do j = 1, ens_size
-         rnum = (j - 1) * model_size + i
-         read(direct_unit, rec = rnum) ens_element
-         temp_spread = temp_spread + (ens_element - temp_mean)**2
-      end do
-      rnum = (ens_size + 1) * model_size + i
-      write(direct_unit, rec = rnum) sqrt(temp_spread / (ens_size - 1))
+      call get_seq_file_name(this_file_name, j)
+      seq_unit = get_unit()
+      open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+      read(seq_unit) ens
+      close(unit = seq_unit)
+      tens_spread = tens_spread + (ens(1, :) - tens_mean)**2
    end do
+   
+   tens_spread = sqrt(tens_spread / (ens_size - 1))
+   ! Write to the ensemble_spread file, index is -1
+   call get_seq_file_name(this_file_name, -1)
+   seq_unit = get_unit()
+   open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+   write(seq_unit) tens_spread
+   close(unit = seq_unit)
 endif
 
 ! Update the spread time
@@ -339,9 +378,13 @@ real(r8), intent(out) :: region(:, :)
 type(time_type), intent(out) :: rtime(:)
 integer, intent(in), optional :: state_vars_in(:), ens_members_in(:)
 
-integer :: state_vars(model_size), ens_members(-1:ens_size), i, j, rnum
+integer :: state_vars(model_size), ens_members(ens_size + 2), i, j
 integer :: num_state_vars, num_ens_members
+character(len = 129) :: this_file_name
 ! Returns a subset of the state variables and ensemble members
+! The ensemble members to be returned are specified by number in ens_members_in
+! The ensemble mean is number 0 and ensemble spread is -1
+! The state variables to be returned are specified by number in state_var_in
 
 ! NOTE: Leaving out ens_members_in results in return of all base
 ! ensemble members but NOT the ensemble mean or spread
@@ -370,6 +413,16 @@ endif
 
 do i = 1, num_ens_members
    rtime(i) = ens_time(ens_members(i))
+
+   ! If not in core, open the file for this ensemble member
+   if(.not. in_core) then
+      call get_seq_file_name(this_file_name, ens_members(i))
+      seq_unit = get_unit()
+      open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+      read(seq_unit) ens
+      close(seq_unit)
+   endif
+
    do j = 1, num_state_vars
       if(in_core) then
          if(ens_members(i) == 0) then
@@ -380,14 +433,8 @@ do i = 1, num_ens_members
             region(i, j) = ens(ens_members(i), state_vars(j))
          endif
       else
-         if(ens_members(i) == 0) then
-            rnum = ens_size * model_size + state_vars(j)
-         else if(ens_members(i) == -1) then
-            rnum = (ens_size + 1) * model_size + state_vars(j)
-         else
-            rnum = (ens_members(i) - 1) * model_size + state_vars(j)
-         endif
-         read(direct_unit, rec = rnum) region(i, j)
+         ! Already have proper ensemble member read in for out of core
+         region(i, j) = ens(1, state_vars(j))
       endif
    end do
 end do
@@ -403,8 +450,9 @@ real(r8), intent(in) :: region(:, :)
 type(time_type), intent(in) :: rtime(:)
 integer, intent(in), optional :: state_vars_in(:), ens_members_in(:)
 
-integer :: state_vars(model_size), ens_members(-1:ens_size), i, j, rnum
+integer :: state_vars(model_size), ens_members(ens_size + 2), i, j
 integer :: num_state_vars, num_ens_members
+character(len = 129) :: this_file_name
 ! Puts a subset of state variables and ensemble members
 
 ! Determine size and elements of state variable subset
@@ -430,6 +478,17 @@ else
 endif
 
 do i = 1, num_ens_members
+   ens_time(ens_members(i)) = rtime(i)
+
+   ! If not in core, open the file for this ensemble member
+   if(.not. in_core) then
+      call get_seq_file_name(this_file_name, ens_members(i))
+      seq_unit = get_unit()
+      open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+      read(seq_unit) ens
+      close(seq_unit)
+   endif
+
    do j = 1, num_state_vars
       if(in_core) then 
          if(ens_members(i) == 0) then
@@ -440,17 +499,18 @@ do i = 1, num_ens_members
             ens(ens_members(i), state_vars(j)) = region(i, j)
          endif
       else
-         if(ens_members(i) == 0) then
-            rnum = ens_size * model_size + state_vars(j)
-         else if(ens_members(i) == -1) then
-            rnum = (ens_size + 1) * model_size + state_vars(j)
-         else
-            rnum = (ens_members(i) - 1) * model_size + state_vars(j)
+         ens(1, state_vars(j)) = region(i, j)
          endif
-         write(direct_unit, rec = rnum) region(i, j)
-      endif
-      ens_time(ens_members(i)) = rtime(i)
    end do
+
+   ! If not in core, open the file for this ensemble member
+   if(.not. in_core) then
+      call get_seq_file_name(this_file_name, ens_members(i))
+      seq_unit = get_unit()
+      open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+      write(seq_unit) ens
+      close(seq_unit)
+   endif
 end do
 
 end subroutine put_ensemble_region
@@ -462,28 +522,47 @@ subroutine end_ensemble_manager(ens_handle, file_name)
 type(ensemble_type), intent(in) :: ens_handle
 character(len = 129), intent(in), optional :: file_name
 
-integer :: iunit, i, j, rnum
+integer :: iunit, i, seq_unit
+character(len = 129) :: this_file_name, command_string
+character(len = 4) :: extension
 
-iunit = open_restart_write(file_name)
+if(single_restart_file_out) iunit = open_restart_write(file_name)
 
 do i = 1, ens_size
+   
+   if(.not. single_restart_file_out) then
+      write(extension, 99) i
+      99 format(i4.4)
+      this_file_name = trim(file_name) // '.' // extension
+      iunit = open_restart_write(this_file_name)
+   endif
+
    if(in_core) then
       call awrite_state_restart(ens_time(i), ens(i, :), iunit)
    else
-      do j = 1, model_size
-         rnum = (i + 1) * model_size + j
-         read(direct_unit, rec = rnum) ens(1, j)
-      end do
+      call get_seq_file_name(this_file_name, i)
+      seq_unit = get_unit()
+      open(unit = seq_unit, file = this_file_name, access = 'sequential', form = 'unformatted')
+      read(seq_unit) ens
+      close(seq_unit)
       call awrite_state_restart(ens_time(i), ens(1, :), iunit)
    endif
+
+   if(.not. single_restart_file_out) call close_restart(iunit)
+
 end do
-call close_restart(iunit)
-close(direct_unit)
+
+if(single_restart_file_out) call close_restart(iunit)
 
 ! Free up the allocated storage
 deallocate(ens, ens_time)
 if(mean_allocated) deallocate(ens_mean)
 if(spread_allocated) deallocate(ens_spread)
+
+! LETS GET RID OF ALL THE TEMP FILES, TOO!
+command_string = 'rm -f ' // trim(seq_file_name) // '.*'
+!write(*, *) 'command string is ', command_string
+call system(command_string)
 
 end subroutine end_ensemble_manager
 
@@ -693,6 +772,29 @@ if(asynch /= 0) then
 end if
 
 end subroutine Aadvance_state
+
+
+!-----------------------------------------------------------------
+
+subroutine get_seq_file_name(file_name, index)
+
+character(len = *), intent(out) :: file_name
+integer, intent(in) :: index
+
+character(len = 4) extension
+
+if(index > 0) then
+   ! Open the direct access file for this ensemble member
+   write(extension, 99) index
+   99 format(i4.4)
+   file_name = trim(seq_file_name) // '.' // extension
+else if(index == 0) then
+   file_name = trim(seq_file_name) // '.mean'
+else if(index == -1) then
+   file_name = trim(seq_file_name) // '.spread'
+endif
+
+end subroutine get_seq_file_name
 
 
 end module ensemble_manager_mod
