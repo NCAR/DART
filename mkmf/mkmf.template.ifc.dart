cc7bb6c30e9a54faa1225cc6a1d14f9e03674b17
Tim Hoar <thoar@ucar.edu>
2005-02-24 14:56:21 -0700

    mkmf.template.anchorage
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1145 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 7bd74db054bd1af58ae88c33b60f0828bba8eef9

--- mkmf/mkmf.template.ifc.dart
+++ mkmf/mkmf.template.ifc.dart
@@ -1,10 +1,46 @@
-# template for the Intel Fortran Compiler Version 7.1 on machine "dart"
-# typical use with mkmf
-# mkmf -t template.ifc -c"-Duse_libMPI -Duse_netCDF" path_names /usr/local/include
+# template for Intel 7.1 Fortran Compiler on a RedHat 7.3 architecture 
 #
-# LIBS needs to be customized for your site
+# Data Assimilation Research Testbed -- DART
+# Copyright 2005, Data Assimilation Initiative, University Corporation for Atmospheric Research
+# Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+#
+# <next two lines are maintained by CVS, please do not edit>
+# $Id$
+# $Name$
+#
+# typical use with mkmf
+# mkmf -t mkmf.template.xxxx -c"-Duse_netCDF" ...
+#
+# NETCDF and LIBS needs to be customized for your site
+#       We had the netcdf libraries built right in: /opt/Intel/[lib,include]
+#
+# MPICH libraries are only needed for Bgrid model and only when you turn on
+#       the -Duse_libMPI preprocessor flag.
+#
+# FFLAGS   used all the time
+#    -fpp
+#
+# FFLAGS   for benchmarking
+#    -O0
+#    -pc64
+#
+# FFLAGS   for debugging
+#    -g
+#
+# FFLAGS   for production
+#    -O2
+#
+# FFLAGS   no longer needed as of Hawaii release
+#    -i4
+#    -r8
+#
+# LDFLAGS
+#     -static -KPIC    needed for relocatable code
+#
 FC = ifc
 LD = ifc
-FFLAGS = -i4 -r8 -fpp -O2 -I/opt/Intel/include
-LIBS = -L/usr/local/lib -lPEPCF90 -L/opt/Intel/lib -ludunits -lnetcdf -L/usr/mpi/lib
+NETCDF = /opt/Intel
+INCS = -I$(NETCDF)/include
+FFLAGS = -i4 -r8 -fpp -O2 $(INCS)
+LIBS = -L/usr/local/lib -lPEPCF90 -L$(NETCDF)/lib -ludunits -lnetcdf -L/usr/mpi/lib 
 LDFLAGS = $(LIBS)
