36caee5feec17a8ab32458729930b6c08e001385
Jeff Anderson <jla@ucar.edu>
2005-03-03 13:40:24 -0700

    Modified to reduce storage and compute sd of sd also.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1179 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 95c025cdf6806f308622ec19e72dff253f8f42ee

--- system_simulation/sys_sim502.f90
+++ system_simulation/sys_sim502.f90
@@ -20,7 +20,7 @@ program sys_sim502
 ! info than group filter because group filter draws from a much more 
 ! meaningful prior (i.e. from what the model and assim have generated).
 
-
+use types_mod, only : r8
 use random_seq_mod, only : random_seq_type, init_random_seq, random_gaussian, &
    twod_gaussians, random_uniform
 
@@ -32,12 +32,16 @@ character(len=128) :: &
    revision = "$Revision$", &
    revdate  = "$Date$"
 
+integer, parameter :: num_bins = 50
+real(r8) :: sum(num_bins) = 0.0, sum2(num_bins)
+integer  :: num(num_bins) = 0.0
 type (random_seq_type) :: r
-double precision :: correl, cov(2, 2), zero_2(2) = 0.0, correl_mean, correl_var
-double precision :: q, alpha, alpha2, mean_abs_correl
-double precision, allocatable :: pairs(:, :), sample_correl(:), real_correl(:)
-double precision :: bottom_cor, top_cor, sum_sample2, sum_sample_real, sum_real
-integer :: ens_size, n_samples, i, j, k, sample_size
+real(r8) :: correl, cov(2, 2), zero_2(2) = 0.0, correl_mean, correl_var, sd
+real(r8) :: q, alpha, alpha2, mean_abs_correl, sample_correl, real_correl
+real(r8), allocatable :: pairs(:, :)
+real(r8) :: bottom_cor, top_cor, sum_sample2, sum_sample_real, sum_real
+real(r8) :: q_factor
+integer :: ens_size, n_samples, i, j, k, sample_size, bin
 
 ! Generate a bazillion cases where the background correlation is assumed to 
 ! be U(-1, 1)
@@ -55,15 +59,14 @@ write(*, *) 'Input number of samples for statistics'
 read(*, *) n_samples
 
 ! Allocate storage for sample from this correlation
-allocate(pairs(2, ens_size), sample_correl(n_samples), real_correl(n_samples))
+allocate(pairs(2, ens_size))
 
 ! Loop through a set of sample draws from uniform (-1, 1)
 do k = 1, n_samples
-   111 real_correl(k) = -1.0 + 2.0 * random_uniform(r)
-   correl = real_correl(k)
+   111 real_correl = -1.0 + 2.0 * random_uniform(r)
    ! Generate the covariance matrix for this correlation
    cov(1, 1) = 1.0;    cov(2, 2) = 1.0
-   cov(1, 2) = correl; cov(2, 1) = correl
+   cov(1, 2) = real_correl; cov(2, 1) = real_correl
 
    ! Loop to generate an ensemble size sample from this correl
    ! Generate a random sample of size ens_size from something with this correlation
@@ -72,36 +75,37 @@ do k = 1, n_samples
    end do
 
    ! Compute the sample correlation
-   call comp_correl(pairs, ens_size, sample_correl(k)) 
+   call comp_correl(pairs, ens_size, sample_correl) 
 
-   if(sample_correl(k) > 0.1 .or. sample_correl(k) < -0.1) goto 111
+   !!!   if(sample_correl > 0.1 .or. sample_correl < -0.1) goto 111
    
-   !!!write(*, *) k, real_correl(k), sample_correl(k)
+   !!!write(*, *) k, real_correl, sample_correl
+
+   ! Now adjust the information for the appropriate sample_correl bin
+   bin = (sample_correl + 1.0) / 2.0 * (num_bins) + 1
+
+   num(bin) = num(bin) + 1
+   sum(bin) = sum(bin) + real_correl
+   sum2(bin) = sum2(bin) + real_correl*real_correl
 
 end do
 
-! Now, gather all the sample_correls from a given range
-do j = 1, 200
-   bottom_cor = -1.0 + (j - 1) * 0.01 
-   top_cor = bottom_cor + 0.01
-   write(*, *) '------', j, bottom_cor, top_cor, '--------'
-   ! Gather up all the SAMPLE correls in this range
-   sum_sample2 = 0.0
-   sum_sample_real = 0.0
-   sum_real = 0.0
-   sample_size = 0
-   do k = 1, n_samples
-      if(sample_correl(k) >= bottom_cor .and. sample_correl(k) < top_cor) then
-         !!!write(*, *) sample_correl(k), real_correl(k)
-         sample_size = sample_size + 1
-         sum_sample2 = sum_sample2 + sample_correl(k)**2
-         sum_sample_real = sum_sample_real + sample_correl(k) * real_correl(k)
-         sum_real = sum_real + real_correl(k)
-      endif
-   end do
-   write(*, *) 'optimal alpha is ', sum_sample_real / sum_sample2, sample_size
-   write(*, *) 'mean real_correl is ', sum_real / sample_size
+do i = 1, num_bins
+   ! Output sample and mean real for this bin
+   sample_correl = ((i - 0.5) / num_bins) * 2.0 - 1.0
+   real_correl = sum(i) / num(i)
+   sd = sqrt((sum2(i) - num(i) * real_correl**2) / (num(i) - 1.0))
+   q = sd / abs(real_correl)
+   q_factor = (num(i) - q**2) / ((num(i) - 1) * q + num(i))
+   write(*, 11) i, num(i), sample_correl, real_correl, &
+      real_correl / sample_correl, sd, q_factor
+   11 format(i4, 1x, i8, 1x, 5(e10.4, 1x))
 end do
+
+if(1 == 1) stop
+
+!!!   write(*, *) 'optimal alpha is ', sum_sample_real / sum_sample2, sample_size
+!!!   write(*, *) 'mean real_correl is ', sum_real / sample_size
 
 end program sys_sim502
 
