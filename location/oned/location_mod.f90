5e394420c03144e8b022c3c617fc5ed5c0981736
Jeff Anderson <jla@ucar.edu>
2005-05-03 15:24:02 -0600

    Added addtional interfaces for get_close_obs
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1199 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 65529a17628bf65a1709d861058191ddec0edaf3

--- location/oned/location_mod.f90
+++ location/oned/location_mod.f90
@@ -24,7 +24,8 @@ private
 
 public location_type, get_dist, get_location, set_location, set_location_missing, &
        write_location, read_location, interactive_location, &
-       LocationDims, LocationName, LocationLName, query_location
+       LocationDims, LocationName, LocationLName, query_location, &
+       alloc_get_close_obs, get_close_obs
 
 ! CVS Generated file description for error handling, do not edit
 character(len=128) :: &
@@ -288,6 +289,58 @@ else
 end if
 
 end subroutine interactive_location
+
+
+
+!----------------------------------------------------------------------------
+
+subroutine alloc_get_close_obs(num, obs, cutoff, obs_box)
+
+implicit none
+
+integer, intent(in) :: num
+type(location_type), intent(in) :: obs(num)
+real(r8), intent(in) :: cutoff
+integer, intent(out) :: obs_box(num)
+
+! This does pre-computing for close obs; no function needed in one dimension
+
+return
+
+end subroutine alloc_get_close_obs
+
+
+!----------------------------------------------------------------------------
+
+subroutine get_close_obs(base_ob, num, obs, cutoff, obs_box, num_close, close_ind, dist)
+
+! Default version with no smarts; no need to be smart in 1D
+
+implicit none
+
+integer, intent(in) :: base_ob, num
+type(location_type), intent(in) :: obs(num)
+real(r8), intent(in) :: cutoff
+integer, intent(in) :: obs_box(num)
+integer, intent(out) :: num_close, close_ind(num)
+real(r8), intent(out) :: dist(num)
+
+integer :: i
+real(r8) :: this_dist
+
+! Return list of obs that are within cutoff and their distances
+num_close = 0
+do i = 1, num
+   this_dist = get_dist(obs(base_ob), obs(i))
+   if(this_dist <= cutoff) then
+      ! Add this ob to the list
+      num_close = num_close + 1
+      close_ind(num_close) = i
+      dist(num_close) = this_dist 
+   endif
+end do
+
+end subroutine get_close_obs
 
 !----------------------------------------------------------------------------
 ! end of location/oned/location_mod.f90
