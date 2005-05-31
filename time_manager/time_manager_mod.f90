6764bbbc156da33b77d46b1062e0c2d35b56fd64
Tim Hoar <thoar@ucar.edu>
2005-05-31 16:18:16 -0600

    changed write_time format to explicitly NOT write any intervening delimiter ... works with intel, absoft, pg ... needed for pathscale compatibility
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1242 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 63e15198e3e46dc604b85e7e5834f02d8af117a1

--- time_manager/time_manager_mod.f90
+++ time_manager/time_manager_mod.f90
@@ -2560,7 +2560,7 @@ SELECT CASE (fileformat)
    CASE ("unf","UNF","unformatted","UNFORMATTED")
       write(file_unit) secs, days
    CASE DEFAULT
-      write(file_unit, *) secs, days
+      write(file_unit,'(i,1x,i)') secs, days
 END SELECT
 
 end subroutine write_time
