24572ec804235a9a4ca0b60ee96e654918c992e9
Jeff Anderson <jla@ucar.edu>
2005-06-11 15:07:07 -0600

    Patches to allow bgrid forward operators to work.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1400 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 9f28ec9625e3d6ce5f022029193114dbb067ea5a

--- obs_def/obs_def_mod.F90
+++ obs_def/obs_def_mod.F90
@@ -473,15 +473,27 @@ if(obs_kind_info(obs_kind_ind)%assimilate .or. obs_kind_info(obs_kind_ind)%evalu
       #ENDIF
       #IFDEF radiosonde_u_wind_component
          case(RADIOSONDE_U_WIND_COMPONENT)
-         call interpolate(state, location, TYPE_U, obs_val, istatus)
+         !!!call interpolate(state, location, TYPE_U, obs_val, istatus)
+         call interpolate(state, location, 1, obs_val, istatus)
+write(*, *) 'getting expected u wind'
       #ENDIF
       #IFDEF radiosonde_v_wind_component
          case(RADIOSONDE_V_WIND_COMPONENT)
-         call interpolate(state, location, TYPE_V, obs_val, istatus)
+         !!!call interpolate(state, location, TYPE_V, obs_val, istatus)
+         call interpolate(state, location, 2, obs_val, istatus)
+write(*, *) 'getting expected v wind'
+      #ENDIF
+      #IFDEF radiosonde_temperature
+         case(SURFACE_PRESSURE)
+         !!!call interpolate(state, location, TYPE_PS, obs_val, istatus)
+         call interpolate(state, location, 3, obs_val, istatus)
+write(*, *) 'getting expected ps'
       #ENDIF
       #IFDEF radiosonde_temperature
          case(RADIOSONDE_TEMPERATURE)
-         call interpolate(state, location, TYPE_T, obs_val, istatus)
+         !!!call interpolate(state, location, TYPE_T, obs_val, istatus)
+         call interpolate(state, location, 4, obs_val, istatus)
+write(*, *) 'getting expected t'
       #ENDIF
    end select
 else
@@ -547,6 +559,7 @@ select case(obs_def%kind)
    case(RADIOSONDE_U_WIND_COMPONENT)
    case(RADIOSONDE_V_WIND_COMPONENT)
    case(RADIOSONDE_TEMPERATURE)
+   case(SURFACE_PRESSURE)
    #IFDEF raw_state_1d_integral
       case(RAW_STATE_1D_INTEGRAL)
          call read_1d_integral(obs_def%key, ifile, fileformat)
@@ -610,6 +623,7 @@ select case(obs_def%kind)
    case(RADIOSONDE_U_WIND_COMPONENT)
    case(RADIOSONDE_V_WIND_COMPONENT)
    case(RADIOSONDE_TEMPERATURE)
+   case(SURFACE_PRESSURE)
    #IFDEF raw_state_1d_integral
       case(RAW_STATE_1D_INTEGRAL)
          call write_1d_integral(obs_def%key, ifile, fileformat)
