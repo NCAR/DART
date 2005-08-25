f36a83283d9d30869505f67d83ad37848c9859dd
Tim Hoar <thoar@ucar.edu>
2005-08-24 20:20:10 -0600

    Incorporating better logging and namelist changes as per Alains request.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1465 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 46e6eb1b4fd340fe25be383c1b82899f01592efd

--- models/wrf/WRF_DART_utilities/ensemble_init.f90
+++ models/wrf/WRF_DART_utilities/ensemble_init.f90
@@ -43,8 +43,8 @@ use  wrf_data_module, only : wrf_data, wrf_bdy_data, &
                              wrf_io, wrfbdy_io, &
                              set_wrf_date
 use    utilities_mod, only : get_unit, file_exist, open_file, check_nml_error, &
-                             close_file, error_handler, E_ERR, initialize_utilities, &
-                             finalize_utilities, register_module, logfileunit
+                             close_file, error_handler, E_ERR, E_MSG, initialize_utilities, &
+                             finalize_utilities, register_module, logfileunit, timestamp
 
 use                         netcdf
 
@@ -65,10 +65,11 @@ integer :: num_moist_vars       = 0
 integer :: num_domains          = 1
 integer :: calendar_type        = GREGORIAN
 logical :: surf_obs             = .false.
+logical :: h_diab               = .false.
 character(len = 72) :: adv_mod_command = 'wrf.exe'
 
 namelist /model_nml/ output_state_vector, num_moist_vars, &
-                     num_domains, calendar_type, surf_obs, &
+                     num_domains, calendar_type, surf_obs, h_diab, &
                      adv_mod_command
 !-----------------------------------------------------------------------
 
@@ -81,7 +82,7 @@ real(r8)           :: scale       ! each deviation scaled by this amt
 
 character (len=6)  :: imem
 character (len=1)  :: idom
-logical            :: debug
+logical            :: debug = .false.
 integer            :: Ne,                 & ! Ensemble size
                       i, id
 
@@ -93,9 +94,7 @@ character(len=19) :: timestring
 read(5,*) Ne       ! Read ensemble size from stdin.
 read(5,*) scale    ! Read scaling       from stdin.
 
-debug = .false.
-
-call initialize_utilities
+call initialize_utilities('ensemble_init')
 call register_module(source, revision, revdate)
 write(logfileunit,*)'STARTING ensemble_init ...'
 
@@ -107,10 +106,15 @@ if(file_exist('input.nml')) then
    ierr = check_nml_error(io, 'model_nml')
    call close_file(iunit)
 
-   if ( debug ) then
-      write(*,'(''num_moist_vars = '',i3)')num_moist_vars
-   endif
+else
+
+   write(*,*) 'input.nml does not exist here. Using default values.'
+
 endif
+
+call error_handler(E_MSG,'dart_tf_wrf','model_nml values are',' ',' ',' ')
+write(logfileunit, nml=model_nml)
+write(     *     , nml=model_nml)
 
 call set_calendar_type(calendar_type)
 
@@ -130,6 +134,10 @@ wrf_bdy_tmp%n_moist = num_moist_vars
 wrf%surf_obs = surf_obs
 wrf_mean%surf_obs = surf_obs
 wrf_tmp%surf_obs = surf_obs
+
+wrf%h_diab = h_diab
+wrf_mean%h_diab = h_diab
+wrf_tmp%h_diab = h_diab
 
 !-- First, do BC's.
 
@@ -323,6 +331,7 @@ enddo
 write(logfileunit,*)'FINISHED ensemble_init.'
 write(logfileunit,*)
 
+call timestamp(source,revision,revdate,'none')
 call finalize_utilities ! closes the log file.
  
 contains
@@ -339,7 +348,7 @@ contains
 
 !---------------------------------------------------------------
 
- subroutine wrf_add( wrf_a, a, wrf_b, b  )
+  subroutine wrf_add( wrf_a, a, wrf_b, b  )
 !
 ! Does wrf_a = a*wrf_a + b*wrf_b, for each real array in type wrf.
 ! Other components of wrf_a (integers, logical) are unchanged.
@@ -352,7 +361,7 @@ contains
  real(r8),       intent(in)    :: a,b
 
  !-- Add: wrf_a = a*wrf_a + b*wrf_b, for components 
- !       u,v,w,ph,phb,t,qv,qc,qr, qi,qs,qg, mu,mub of a and b
+ !       u,v,w,ph,phb,t, qv,qc,qr, qi,qs,qg,qnice, mu,mub, tslb,tsk, of a and b
  wrf_a%u = a * wrf_a%u + b * wrf_b%u
  wrf_a%v = a * wrf_a%v + b * wrf_b%v
  wrf_a%w = a * wrf_a%w + b * wrf_b%w
@@ -381,137 +390,150 @@ contains
  if(wrf%n_moist > 5) then
     wrf_a%qg = a * wrf_a%qg + b * wrf_b%qg
  endif
+ if(wrf%n_moist > 6) then
+    wrf_a%qnice = a * wrf_a%qnice + b * wrf_b%qnice
+ endif
  
 end subroutine wrf_add
 !---------------------------------------------------------------
 
- subroutine wrfbdy_add( wrfbdy_a, a, wrfbdy_b, b  )
+subroutine wrfbdy_add( wrfbdy_a, a, wrfbdy_b, b  )
 !
 ! Does wrfbdy_a = a*wrfbdy_a + b*wrfbdy_b, for each real array in type wrfbdy.
 ! Other components of wrf_a (integers, logical) are unchanged.
 !
 
- implicit none
+  implicit none
 
- type(wrf_bdy_data), intent(inout) :: wrfbdy_a
- type(wrf_bdy_data), intent(in   ) :: wrfbdy_b
- real(r8),           intent(in)    :: a,b
+  type(wrf_bdy_data), intent(inout) :: wrfbdy_a
+  type(wrf_bdy_data), intent(in   ) :: wrfbdy_b
+  real(r8),           intent(in)    :: a,b
 
- !-- Add: wrfbdy_a = a*wrfbdy_a + b*wrfbdy_b, for components 
- !       u,v,w,ph,t,qv on bndries + their tendencies
+  !-- Add: wrfbdy_a = a*wrfbdy_a + b*wrfbdy_b, for components 
+  !       u,v,w,ph,t,qv on bndries + their tendencies
 
- wrfbdy_a%uxs = a * wrfbdy_a%uxs + b * wrfbdy_b%uxs
- wrfbdy_a%uxe = a * wrfbdy_a%uxe + b * wrfbdy_b%uxe
- wrfbdy_a%uys = a * wrfbdy_a%uys + b * wrfbdy_b%uys
- wrfbdy_a%uye = a * wrfbdy_a%uye + b * wrfbdy_b%uye
- wrfbdy_a%utxs = a * wrfbdy_a%utxs + b * wrfbdy_b%utxs
- wrfbdy_a%utxe = a * wrfbdy_a%utxe + b * wrfbdy_b%utxe
- wrfbdy_a%utys = a * wrfbdy_a%utys + b * wrfbdy_b%utys
- wrfbdy_a%utye = a * wrfbdy_a%utye + b * wrfbdy_b%utye
+  wrfbdy_a%uxs = a * wrfbdy_a%uxs + b * wrfbdy_b%uxs
+  wrfbdy_a%uxe = a * wrfbdy_a%uxe + b * wrfbdy_b%uxe
+  wrfbdy_a%uys = a * wrfbdy_a%uys + b * wrfbdy_b%uys
+  wrfbdy_a%uye = a * wrfbdy_a%uye + b * wrfbdy_b%uye
+  wrfbdy_a%utxs = a * wrfbdy_a%utxs + b * wrfbdy_b%utxs
+  wrfbdy_a%utxe = a * wrfbdy_a%utxe + b * wrfbdy_b%utxe
+  wrfbdy_a%utys = a * wrfbdy_a%utys + b * wrfbdy_b%utys
+  wrfbdy_a%utye = a * wrfbdy_a%utye + b * wrfbdy_b%utye
 
- wrfbdy_a%vxs = a * wrfbdy_a%vxs + b * wrfbdy_b%vxs
- wrfbdy_a%vxe = a * wrfbdy_a%vxe + b * wrfbdy_b%vxe
- wrfbdy_a%vys = a * wrfbdy_a%vys + b * wrfbdy_b%vys
- wrfbdy_a%vye = a * wrfbdy_a%vye + b * wrfbdy_b%vye
- wrfbdy_a%vtxs = a * wrfbdy_a%vtxs + b * wrfbdy_b%vtxs
- wrfbdy_a%vtxe = a * wrfbdy_a%vtxe + b * wrfbdy_b%vtxe
- wrfbdy_a%vtys = a * wrfbdy_a%vtys + b * wrfbdy_b%vtys
- wrfbdy_a%vtye = a * wrfbdy_a%vtye + b * wrfbdy_b%vtye
+  wrfbdy_a%vxs = a * wrfbdy_a%vxs + b * wrfbdy_b%vxs
+  wrfbdy_a%vxe = a * wrfbdy_a%vxe + b * wrfbdy_b%vxe
+  wrfbdy_a%vys = a * wrfbdy_a%vys + b * wrfbdy_b%vys
+  wrfbdy_a%vye = a * wrfbdy_a%vye + b * wrfbdy_b%vye
+  wrfbdy_a%vtxs = a * wrfbdy_a%vtxs + b * wrfbdy_b%vtxs
+  wrfbdy_a%vtxe = a * wrfbdy_a%vtxe + b * wrfbdy_b%vtxe
+  wrfbdy_a%vtys = a * wrfbdy_a%vtys + b * wrfbdy_b%vtys
+  wrfbdy_a%vtye = a * wrfbdy_a%vtye + b * wrfbdy_b%vtye
 
- wrfbdy_a%phxs = a * wrfbdy_a%phxs + b * wrfbdy_b%phxs
- wrfbdy_a%phxe = a * wrfbdy_a%phxe + b * wrfbdy_b%phxe
- wrfbdy_a%phys = a * wrfbdy_a%phys + b * wrfbdy_b%phys
- wrfbdy_a%phye = a * wrfbdy_a%phye + b * wrfbdy_b%phye
- wrfbdy_a%phtxs = a * wrfbdy_a%phtxs + b * wrfbdy_b%phtxs
- wrfbdy_a%phtxe = a * wrfbdy_a%phtxe + b * wrfbdy_b%phtxe
- wrfbdy_a%phtys = a * wrfbdy_a%phtys + b * wrfbdy_b%phtys
- wrfbdy_a%phtye = a * wrfbdy_a%phtye + b * wrfbdy_b%phtye
+  wrfbdy_a%phxs = a * wrfbdy_a%phxs + b * wrfbdy_b%phxs
+  wrfbdy_a%phxe = a * wrfbdy_a%phxe + b * wrfbdy_b%phxe
+  wrfbdy_a%phys = a * wrfbdy_a%phys + b * wrfbdy_b%phys
+  wrfbdy_a%phye = a * wrfbdy_a%phye + b * wrfbdy_b%phye
+  wrfbdy_a%phtxs = a * wrfbdy_a%phtxs + b * wrfbdy_b%phtxs
+  wrfbdy_a%phtxe = a * wrfbdy_a%phtxe + b * wrfbdy_b%phtxe
+  wrfbdy_a%phtys = a * wrfbdy_a%phtys + b * wrfbdy_b%phtys
+  wrfbdy_a%phtye = a * wrfbdy_a%phtye + b * wrfbdy_b%phtye
 
- wrfbdy_a%txs = a * wrfbdy_a%txs + b * wrfbdy_b%txs
- wrfbdy_a%txe = a * wrfbdy_a%txe + b * wrfbdy_b%txe
- wrfbdy_a%tys = a * wrfbdy_a%tys + b * wrfbdy_b%tys
- wrfbdy_a%tye = a * wrfbdy_a%tye + b * wrfbdy_b%tye
- wrfbdy_a%ttxs = a * wrfbdy_a%ttxs + b * wrfbdy_b%ttxs
- wrfbdy_a%ttxe = a * wrfbdy_a%ttxe + b * wrfbdy_b%ttxe
- wrfbdy_a%ttys = a * wrfbdy_a%ttys + b * wrfbdy_b%ttys
- wrfbdy_a%ttye = a * wrfbdy_a%ttye + b * wrfbdy_b%ttye
+  wrfbdy_a%txs = a * wrfbdy_a%txs + b * wrfbdy_b%txs
+  wrfbdy_a%txe = a * wrfbdy_a%txe + b * wrfbdy_b%txe
+  wrfbdy_a%tys = a * wrfbdy_a%tys + b * wrfbdy_b%tys
+  wrfbdy_a%tye = a * wrfbdy_a%tye + b * wrfbdy_b%tye
+  wrfbdy_a%ttxs = a * wrfbdy_a%ttxs + b * wrfbdy_b%ttxs
+  wrfbdy_a%ttxe = a * wrfbdy_a%ttxe + b * wrfbdy_b%ttxe
+  wrfbdy_a%ttys = a * wrfbdy_a%ttys + b * wrfbdy_b%ttys
+  wrfbdy_a%ttye = a * wrfbdy_a%ttye + b * wrfbdy_b%ttye
 
- wrfbdy_a%muxs = a * wrfbdy_a%muxs + b * wrfbdy_b%muxs
- wrfbdy_a%muxe = a * wrfbdy_a%muxe + b * wrfbdy_b%muxe
- wrfbdy_a%muys = a * wrfbdy_a%muys + b * wrfbdy_b%muys
- wrfbdy_a%muye = a * wrfbdy_a%muye + b * wrfbdy_b%muye
- wrfbdy_a%mutxs = a * wrfbdy_a%mutxs + b * wrfbdy_b%mutxs
- wrfbdy_a%mutxe = a * wrfbdy_a%mutxe + b * wrfbdy_b%mutxe
- wrfbdy_a%mutys = a * wrfbdy_a%mutys + b * wrfbdy_b%mutys
- wrfbdy_a%mutye = a * wrfbdy_a%mutye + b * wrfbdy_b%mutye
+  wrfbdy_a%muxs = a * wrfbdy_a%muxs + b * wrfbdy_b%muxs
+  wrfbdy_a%muxe = a * wrfbdy_a%muxe + b * wrfbdy_b%muxe
+  wrfbdy_a%muys = a * wrfbdy_a%muys + b * wrfbdy_b%muys
+  wrfbdy_a%muye = a * wrfbdy_a%muye + b * wrfbdy_b%muye
+  wrfbdy_a%mutxs = a * wrfbdy_a%mutxs + b * wrfbdy_b%mutxs
+  wrfbdy_a%mutxe = a * wrfbdy_a%mutxe + b * wrfbdy_b%mutxe
+  wrfbdy_a%mutys = a * wrfbdy_a%mutys + b * wrfbdy_b%mutys
+  wrfbdy_a%mutye = a * wrfbdy_a%mutye + b * wrfbdy_b%mutye
 
- if(wrfbdy_a%n_moist > 0) then
-    wrfbdy_a%qvxs = a * wrfbdy_a%qvxs + b * wrfbdy_b%qvxs
-    wrfbdy_a%qvxe = a * wrfbdy_a%qvxe + b * wrfbdy_b%qvxe
-    wrfbdy_a%qvys = a * wrfbdy_a%qvys + b * wrfbdy_b%qvys
-    wrfbdy_a%qvye = a * wrfbdy_a%qvye + b * wrfbdy_b%qvye
-    wrfbdy_a%qvtxs = a * wrfbdy_a%qvtxs + b * wrfbdy_b%qvtxs
-    wrfbdy_a%qvtxe = a * wrfbdy_a%qvtxe + b * wrfbdy_b%qvtxe
-    wrfbdy_a%qvtys = a * wrfbdy_a%qvtys + b * wrfbdy_b%qvtys
-    wrfbdy_a%qvtye = a * wrfbdy_a%qvtye + b * wrfbdy_b%qvtye
- endif
+  if(wrfbdy_a%n_moist > 0) then
+     wrfbdy_a%qvxs = a * wrfbdy_a%qvxs + b * wrfbdy_b%qvxs
+     wrfbdy_a%qvxe = a * wrfbdy_a%qvxe + b * wrfbdy_b%qvxe
+     wrfbdy_a%qvys = a * wrfbdy_a%qvys + b * wrfbdy_b%qvys
+     wrfbdy_a%qvye = a * wrfbdy_a%qvye + b * wrfbdy_b%qvye
+     wrfbdy_a%qvtxs = a * wrfbdy_a%qvtxs + b * wrfbdy_b%qvtxs
+     wrfbdy_a%qvtxe = a * wrfbdy_a%qvtxe + b * wrfbdy_b%qvtxe
+     wrfbdy_a%qvtys = a * wrfbdy_a%qvtys + b * wrfbdy_b%qvtys
+     wrfbdy_a%qvtye = a * wrfbdy_a%qvtye + b * wrfbdy_b%qvtye
+  endif
 
- if(wrfbdy_a%n_moist > 1) then
-    wrfbdy_a%qcxs = a * wrfbdy_a%qcxs + b * wrfbdy_b%qcxs
-    wrfbdy_a%qcxe = a * wrfbdy_a%qcxe + b * wrfbdy_b%qcxe
-    wrfbdy_a%qcys = a * wrfbdy_a%qcys + b * wrfbdy_b%qcys
-    wrfbdy_a%qcye = a * wrfbdy_a%qcye + b * wrfbdy_b%qcye
-    wrfbdy_a%qctxs = a * wrfbdy_a%qctxs + b * wrfbdy_b%qctxs
-    wrfbdy_a%qctxe = a * wrfbdy_a%qctxe + b * wrfbdy_b%qctxe
-    wrfbdy_a%qctys = a * wrfbdy_a%qctys + b * wrfbdy_b%qctys
-    wrfbdy_a%qctye = a * wrfbdy_a%qctye + b * wrfbdy_b%qctye
- endif
- if(wrfbdy_a%n_moist > 2) then
-    wrfbdy_a%qrxs = a * wrfbdy_a%qrxs + b * wrfbdy_b%qrxs
-    wrfbdy_a%qrxe = a * wrfbdy_a%qrxe + b * wrfbdy_b%qrxe
-    wrfbdy_a%qrys = a * wrfbdy_a%qrys + b * wrfbdy_b%qrys
-    wrfbdy_a%qrye = a * wrfbdy_a%qrye + b * wrfbdy_b%qrye
-    wrfbdy_a%qrtxs = a * wrfbdy_a%qrtxs + b * wrfbdy_b%qrtxs
-    wrfbdy_a%qrtxe = a * wrfbdy_a%qrtxe + b * wrfbdy_b%qrtxe
-    wrfbdy_a%qrtys = a * wrfbdy_a%qrtys + b * wrfbdy_b%qrtys
-    wrfbdy_a%qrtye = a * wrfbdy_a%qrtye + b * wrfbdy_b%qrtye
- endif
- if(wrfbdy_a%n_moist > 3) then
-    wrfbdy_a%qixs = a * wrfbdy_a%qixs + b * wrfbdy_b%qixs
-    wrfbdy_a%qixe = a * wrfbdy_a%qixe + b * wrfbdy_b%qixe
-    wrfbdy_a%qiys = a * wrfbdy_a%qiys + b * wrfbdy_b%qiys
-    wrfbdy_a%qiye = a * wrfbdy_a%qiye + b * wrfbdy_b%qiye
-    wrfbdy_a%qitxs = a * wrfbdy_a%qitxs + b * wrfbdy_b%qitxs
-    wrfbdy_a%qitxe = a * wrfbdy_a%qitxe + b * wrfbdy_b%qitxe
-    wrfbdy_a%qitys = a * wrfbdy_a%qitys + b * wrfbdy_b%qitys
-    wrfbdy_a%qitye = a * wrfbdy_a%qitye + b * wrfbdy_b%qitye
- endif
- if(wrfbdy_a%n_moist > 4) then
-    wrfbdy_a%qsxs = a * wrfbdy_a%qsxs + b * wrfbdy_b%qsxs
-    wrfbdy_a%qsxe = a * wrfbdy_a%qsxe + b * wrfbdy_b%qsxe
-    wrfbdy_a%qsys = a * wrfbdy_a%qsys + b * wrfbdy_b%qsys
-    wrfbdy_a%qsye = a * wrfbdy_a%qsye + b * wrfbdy_b%qsye
-    wrfbdy_a%qstxs = a * wrfbdy_a%qstxs + b * wrfbdy_b%qstxs
-    wrfbdy_a%qstxe = a * wrfbdy_a%qstxe + b * wrfbdy_b%qstxe
-    wrfbdy_a%qstys = a * wrfbdy_a%qstys + b * wrfbdy_b%qstys
-    wrfbdy_a%qstye = a * wrfbdy_a%qstye + b * wrfbdy_b%qstye
- endif
- if(wrfbdy_a%n_moist > 5) then
-    wrfbdy_a%qgxs = a * wrfbdy_a%qgxs + b * wrfbdy_b%qgxs
-    wrfbdy_a%qgxe = a * wrfbdy_a%qgxe + b * wrfbdy_b%qgxe
-    wrfbdy_a%qgys = a * wrfbdy_a%qgys + b * wrfbdy_b%qgys
-    wrfbdy_a%qgye = a * wrfbdy_a%qgye + b * wrfbdy_b%qgye
-    wrfbdy_a%qgtxs = a * wrfbdy_a%qgtxs + b * wrfbdy_b%qgtxs
-    wrfbdy_a%qgtxe = a * wrfbdy_a%qgtxe + b * wrfbdy_b%qgtxe
-    wrfbdy_a%qgtys = a * wrfbdy_a%qgtys + b * wrfbdy_b%qgtys
-    wrfbdy_a%qgtye = a * wrfbdy_a%qgtye + b * wrfbdy_b%qgtye
- endif
-if(wrfbdy_a%n_moist > 6) then
-   write(6,*) 'n_moist = ',wrfbdy_a%n_moist
-   call error_handler(E_ERR,'wrfbdy_add', &
-         'n_moist is too large.', source, revision, revdate)
-   stop
-endif
+  if(wrfbdy_a%n_moist > 1) then
+     wrfbdy_a%qcxs = a * wrfbdy_a%qcxs + b * wrfbdy_b%qcxs
+     wrfbdy_a%qcxe = a * wrfbdy_a%qcxe + b * wrfbdy_b%qcxe
+     wrfbdy_a%qcys = a * wrfbdy_a%qcys + b * wrfbdy_b%qcys
+     wrfbdy_a%qcye = a * wrfbdy_a%qcye + b * wrfbdy_b%qcye
+     wrfbdy_a%qctxs = a * wrfbdy_a%qctxs + b * wrfbdy_b%qctxs
+     wrfbdy_a%qctxe = a * wrfbdy_a%qctxe + b * wrfbdy_b%qctxe
+     wrfbdy_a%qctys = a * wrfbdy_a%qctys + b * wrfbdy_b%qctys
+     wrfbdy_a%qctye = a * wrfbdy_a%qctye + b * wrfbdy_b%qctye
+  endif
+  if(wrfbdy_a%n_moist > 2) then
+     wrfbdy_a%qrxs = a * wrfbdy_a%qrxs + b * wrfbdy_b%qrxs
+     wrfbdy_a%qrxe = a * wrfbdy_a%qrxe + b * wrfbdy_b%qrxe
+     wrfbdy_a%qrys = a * wrfbdy_a%qrys + b * wrfbdy_b%qrys
+     wrfbdy_a%qrye = a * wrfbdy_a%qrye + b * wrfbdy_b%qrye
+     wrfbdy_a%qrtxs = a * wrfbdy_a%qrtxs + b * wrfbdy_b%qrtxs
+     wrfbdy_a%qrtxe = a * wrfbdy_a%qrtxe + b * wrfbdy_b%qrtxe
+     wrfbdy_a%qrtys = a * wrfbdy_a%qrtys + b * wrfbdy_b%qrtys
+     wrfbdy_a%qrtye = a * wrfbdy_a%qrtye + b * wrfbdy_b%qrtye
+  endif
+  if(wrfbdy_a%n_moist > 3) then
+     wrfbdy_a%qixs = a * wrfbdy_a%qixs + b * wrfbdy_b%qixs
+     wrfbdy_a%qixe = a * wrfbdy_a%qixe + b * wrfbdy_b%qixe
+     wrfbdy_a%qiys = a * wrfbdy_a%qiys + b * wrfbdy_b%qiys
+     wrfbdy_a%qiye = a * wrfbdy_a%qiye + b * wrfbdy_b%qiye
+     wrfbdy_a%qitxs = a * wrfbdy_a%qitxs + b * wrfbdy_b%qitxs
+     wrfbdy_a%qitxe = a * wrfbdy_a%qitxe + b * wrfbdy_b%qitxe
+     wrfbdy_a%qitys = a * wrfbdy_a%qitys + b * wrfbdy_b%qitys
+     wrfbdy_a%qitye = a * wrfbdy_a%qitye + b * wrfbdy_b%qitye
+  endif
+  if(wrfbdy_a%n_moist > 4) then
+     wrfbdy_a%qsxs = a * wrfbdy_a%qsxs + b * wrfbdy_b%qsxs
+     wrfbdy_a%qsxe = a * wrfbdy_a%qsxe + b * wrfbdy_b%qsxe
+     wrfbdy_a%qsys = a * wrfbdy_a%qsys + b * wrfbdy_b%qsys
+     wrfbdy_a%qsye = a * wrfbdy_a%qsye + b * wrfbdy_b%qsye
+     wrfbdy_a%qstxs = a * wrfbdy_a%qstxs + b * wrfbdy_b%qstxs
+     wrfbdy_a%qstxe = a * wrfbdy_a%qstxe + b * wrfbdy_b%qstxe
+     wrfbdy_a%qstys = a * wrfbdy_a%qstys + b * wrfbdy_b%qstys
+     wrfbdy_a%qstye = a * wrfbdy_a%qstye + b * wrfbdy_b%qstye
+  endif
+  if(wrfbdy_a%n_moist > 5) then
+     wrfbdy_a%qgxs = a * wrfbdy_a%qgxs + b * wrfbdy_b%qgxs
+     wrfbdy_a%qgxe = a * wrfbdy_a%qgxe + b * wrfbdy_b%qgxe
+     wrfbdy_a%qgys = a * wrfbdy_a%qgys + b * wrfbdy_b%qgys
+     wrfbdy_a%qgye = a * wrfbdy_a%qgye + b * wrfbdy_b%qgye
+     wrfbdy_a%qgtxs = a * wrfbdy_a%qgtxs + b * wrfbdy_b%qgtxs
+     wrfbdy_a%qgtxe = a * wrfbdy_a%qgtxe + b * wrfbdy_b%qgtxe
+     wrfbdy_a%qgtys = a * wrfbdy_a%qgtys + b * wrfbdy_b%qgtys
+     wrfbdy_a%qgtye = a * wrfbdy_a%qgtye + b * wrfbdy_b%qgtye
+  endif
+  if(wrfbdy_a%n_moist > 6) then
+     wrfbdy_a%qnicexs = a * wrfbdy_a%qnicexs + b * wrfbdy_b%qnicexs
+     wrfbdy_a%qnicexe = a * wrfbdy_a%qnicexe + b * wrfbdy_b%qnicexe
+     wrfbdy_a%qniceys = a * wrfbdy_a%qniceys + b * wrfbdy_b%qniceys
+     wrfbdy_a%qniceye = a * wrfbdy_a%qniceye + b * wrfbdy_b%qniceye
+     wrfbdy_a%qnicetxs = a * wrfbdy_a%qnicetxs + b * wrfbdy_b%qnicetxs
+     wrfbdy_a%qnicetxe = a * wrfbdy_a%qnicetxe + b * wrfbdy_b%qnicetxe
+     wrfbdy_a%qnicetys = a * wrfbdy_a%qnicetys + b * wrfbdy_b%qnicetys
+     wrfbdy_a%qnicetye = a * wrfbdy_a%qnicetye + b * wrfbdy_b%qnicetye
+  endif
+  if(wrfbdy_a%n_moist > 7) then
+     write(6,*) 'n_moist = ',wrfbdy_a%n_moist
+     call error_handler(E_ERR,'wrfbdy_add', &
+          'n_moist is too large.', source, revision, revdate)
+     stop
+  endif
 
 end subroutine wrfbdy_add
 
