7cadfb94909e124dccb700108d4c0b726173a3ca
Jeff Anderson <jla@ucar.edu>
2005-08-31 15:45:44 -0600

    netcdf routines produce something for True_state.nc ...
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1484 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 3a107fdf5cfd8894f5045f260a7a1f867d9da7b0

--- models/sccm/model_mod.f90
+++ models/sccm/model_mod.f90
@@ -20,8 +20,9 @@ module model_mod
 ! Modules that are absolutely required for use are listed
 use        types_mod, only : r8
 use time_manager_mod, only : time_type, set_time, get_time, get_date, &
-                             set_date, operator(+)
-use     location_mod, only : location_type, set_location
+                             set_date, operator(+), set_calendar_type, &
+                             GREGORIAN
+use     location_mod, only : location_type, set_location, query_location
 use    utilities_mod, only : file_exist, open_file, check_nml_error, close_file, &
                              register_module, error_handler, E_ERR, E_MSG, logfileunit
 
@@ -74,6 +75,9 @@ type(location_type), allocatable :: state_loc(:)
 type(time_type) :: time_step
 integer :: model_size          ! Global storage for size of model
 
+! Storage for keeping updated pressure levels and soil level locations
+real(r8), allocatable :: pressure(:), depth(:), temp_state(:)
+
 
 contains
 
@@ -89,7 +93,8 @@ subroutine static_init_model()
 ! configuration details of the model.
 
 real(r8) :: x_loc
-integer  :: i, iunit, ierr, io
+integer  :: i, j, iunit, ierr, io, indx, g_offset, s_offset, tile
+type(time_type) :: temp_time
 
 ! Print module information to log file and stdout.
 call register_module(source, revision, revdate)
@@ -114,13 +119,42 @@ write(     *     , nml=model_nml)
 ! Compute the model state vector size
 model_size = (3 + pcnst) * plev + 1 + 4*kpt + 2*kpt*msl
 ! Create storage for locations
-allocate(state_loc(model_size))
+! Also storage to keep track of pressure of the atmospheric levels and 
+! depth of soil levels
+allocate(state_loc(model_size), pressure(plev), depth(msl), temp_state(model_size))
+
+! NOTICE: NEED TO GET THE ABILITY TO COMPUTE THE PRESSURE LEVELS SOMEHOW
+! SAME FOR SOIL IN THE LONG RUN
+! COULD READ A's AND B's FROM NETCDF FILE FOR MODEL AND COMPUTE EACH TIME???
+
 
 ! Define the locations of the model state variables
-! For now, just make all locations the same using 3D location
 ! For small model like this assume no localization will be used
+! Many variables are at surface, so begin by setting all to surface
 do i = 1, model_size
-   state_loc(i) =  set_location(0.0_r8, 0.0_r8, 1000.0_r8, 2)
+   state_loc(i) =  set_location(0.0_r8, 0.0_r8, 1.0_r8, -1)
+end do
+
+! Put in vertical level for u, v, t, tracers
+do i = 1, 3 + pcnst
+   do j = 1, plev
+      indx = (i - 1) * plev + j
+      state_loc(indx) = set_location(0.0_r8, 0.0_r8, 1.0_r8 * j, 1)
+   end do
+end do
+
+
+! The soil moisture and temperature have soil levels for vertical
+! Offsets for where the ground and soil state variables start in dart
+g_offset = (3 + pcnst) * plev + 2
+s_offset = g_offset + 4 * kpt
+do i = 1, 2
+   do tile = 1, kpt
+      do j = 1, msl
+         indx = s_offset + (i - 1) * kpt * msl + (tile - 1) * msl + (j - 1)
+         state_loc(indx) = set_location(0.0_r8, 0.0_r8, 1.0_r8 * j, 1)
+      end do
+   end do
 end do
 
 ! The time_step in terms of a time type must also be initialized. 
@@ -214,11 +248,13 @@ subroutine model_interpolate(x, location, itype, obs_val, istatus)
 ! 0 unless there is some problem in computing the interpolation in
 ! which case an alternate value should be returned. The itype variable
 ! is a model specific integer that specifies the type of field (for
-! instance temperature, zonal wind component, etc.). In low order
-! models that have no notion of types of variables, this argument can
-! be ignored. For applications in which only perfect model experiments
-! with identity observations (i.e. only the value of a particular
-! state variable is observerd), this can be a NULL INTERFACE.
+! instance temperature, zonal wind component, etc.). For this model,
+! the kinds are specified as in CAM
+!		U is itype 1
+!		V is itype 2
+! 		PS is itype 3
+!		T is itype 4
+!		Q is itype 5
 
 real(r8),            intent(in) :: x(:)
 type(location_type), intent(in) :: location
@@ -226,8 +262,72 @@ integer,             intent(in) :: itype
 real(r8),           intent(out) :: obs_val
 integer,            intent(out) :: istatus
 
+integer :: lo, hi, i, indx, base_indx, which_vert
+real(r8) :: fract, obs_press
+
 ! Default for successful return
 istatus = 0
+
+! Only types 1 to 5 currently supported
+if(itype < 1 .or. itype > 5) then
+   istatus = 1
+   obs_val = -1_r8
+   return
+endif
+
+! If itype is pressure, just return ps
+if(itype == 3) then
+   indx = (3 + pcnst) * plev + 1
+   obs_val = x(indx)
+   return
+endif
+
+! Only relevant part of location is vertical, need pressure for kind
+which_vert = query_location(location, 'which_vert')
+! YUQIONG: NEED TO PUT CHECKS FOR VERTICAL LOCATION TYPE HERE
+obs_press = query_location(location, 'vloc')
+
+write(*, *) 'obs_press is ', obs_press
+write(*, *) 'pressure(1), plev ', pressure(1), pressure(plev)
+if(1 == 1) stop
+
+! Search through the pressure level array and get upper, lower and fraction
+! If above the top or below the bottom, can't use for now
+if(obs_press < pressure(1) .or. obs_press > pressure(plev)) then
+   istatus = 1
+   obs_val = -1_r8
+   return
+endif
+
+do i = 2, plev
+   if(obs_press < pressure(i)) then
+      lo = i - 1
+      hi = i
+      fract = (obs_press - pressure(lo)) / (pressure(hi) - pressure(lo))
+      goto 111
+   endif
+end do
+
+! Pressure location found, now get correct type of observation
+111 continue
+if(itype == 1) then
+   base_indx = 0
+else if(itype == 2) then
+   base_indx = plev
+else if(itype == 4) then
+   base_indx = 2*plev
+else if(itype == 5) then
+   base_indx = 3*plev
+endif
+
+lo = lo + base_indx
+hi = lo + 1
+
+! Do the pressure interpolation (linear) and return    
+obs_val = x(lo) + fract * (x(hi) - x(lo))
+
+write(*, *) 'in model_interpolate ', lo, hi, fract, obs_val
+if(1 == 1) stop
 
 end subroutine model_interpolate
 
@@ -434,6 +534,12 @@ if ( TimeDimID /= unlimitedDimId ) then
 endif
 
 !-------------------------------------------------------------------------------
+! Define the model size, state variable dimension ... whatever ...
+!-------------------------------------------------------------------------------
+call check(nf90_def_dim(ncid=ncFileID, name="StateVariable", &
+                        len=model_size, dimid = StateVarDimID))
+
+!-------------------------------------------------------------------------------
 ! Write Global Attributes
 !-------------------------------------------------------------------------------
                                                                                                            
@@ -497,17 +603,20 @@ if ( output_state_vector ) then
    call check(nf90_put_att(ncFileID, StateVarVarID, "units",     "indexical") )
    call check(nf90_put_att(ncFileID, StateVarVarID, "valid_range", (/ 1, model_size /)))
 
+
    ! Define the actual state vector
    call check(nf90_def_var(ncid=ncFileID, name="state", xtype=nf90_real, &
               dimids = (/ StateVarDimID, MemberDimID, unlimitedDimID /), &
               varid = StateVarID))
    call check(nf90_put_att(ncFileID, StateVarID, "long_name", "model state or fcopy"))
 
+
    ! Leave define mode so we can fill
    call check(nf90_enddef(ncfileID))
 
    ! Fill the state variable coordinate variable
    call check(nf90_put_var(ncFileID, StateVarVarID, (/ (i,i=1,model_size) /) ))
+
 
 else
 
@@ -773,6 +882,7 @@ write(file_id, *) "advance-time ", delta_seconds
 
 ! First write day as date integer and seconds
 call get_date(time, year, month, day, hour, minute, second)
+write(*, *) 'get_date', year, month, day, hour, minute, second
 ! Create and write the date stamp for the sccm
 year = year - year / 100 * 100
 date_stamp = year * 10000 + month * 100 + day
@@ -785,6 +895,13 @@ write(file_id, *) 'KPT ', kpt
 write(file_id, *) 'MSL ', msl
 write(file_id, *) 'PLEV ', plev
 write(file_id, *) 'PCNST ', pcnst
+
+! Write the pressure level values
+write(file_id, *) 'press_lev', pressure
+
+! Write the soil level values
+write(file_id, *) 'soi_lev', depth
+
 
 ! Write out the atmospheric t
 atmos(:) = state(2*plev + 1 : 3*plev)
@@ -815,7 +932,7 @@ sfc = state(g_offset + kpt : g_offset + 2*kpt - 1)
 write(file_id, *) 'h20can(kpt) ', sfc
 ! Now soil moisture
 soil = state(s_offset : s_offset + msl*kpt - 1)
-write(file_id, *) 'h2osoi(kpt, msl) ', soil
+write(file_id, *) 'h2osoi(kpt,msl) ', soil
 ! Now temperature of vegetation
 sfc = state(g_offset + 2*kpt : g_offset + 3*kpt - 1)
 write(file_id, *) 'tv(kpt) ', sfc
@@ -850,6 +967,7 @@ read(file_id, *) temp_string, delta_seconds
 ! First read day as date integer and seconds
 read(file_id, *) temp_string, date_stamp
 read(file_id, *) temp_string, seconds
+write(*, *) 'date stamp is ', date_stamp
 year = date_stamp / 10000
 month = (date_stamp - year * 10000) / 100
 day = (date_stamp - year * 10000 - month * 100)
@@ -859,6 +977,7 @@ if(year > 50) then
 else
    year = year + 2000
 endif
+write(*, *) 'year month day ', year, month, day
 
 ! First, set a date for just the day part with 0 seconds
 time = set_date(year, month, day, 0, 0, 0)
@@ -875,27 +994,30 @@ if(kpt_in /= kpt .or. msl_in /= msl .or. plev_in /= plev .or. pcnst_in /= pcnst)
    stop
 endif
 
+! Read in the pressure level values
+read(file_id, *) temp_string, pressure
+! DART USES PRESSURE IN PA, NEED TO MULTIPLY BY 100
+pressure = pressure * 100.0_r8
+
+! Read in the soil level values
+read(file_id, *) temp_string, depth
+
 ! Read the atmospheric t
 read(file_id, *) temp_string, atmos
 state(2*plev + 1 : 3*plev) = atmos
-write(*, *) 't is ', atmos
 ! Read atmospheric u
 read(file_id, *) temp_string, atmos
 state(1 : plev) = atmos
-write(*, *) 'u is ', atmos
 ! Read atmospheric v
 read(file_id, *) temp_string, atmos
 state(plev + 1 : 2*plev) = atmos
-write(*, *) 'v is ', atmos
 ! Read the tracer fields
 do i = 1, pcnst
    read(file_id, *) temp_string, temp_string2, atmos
    state((2 + i)*plev + 1: (3 + i) * plev) = atmos
-   write(*, *) 'tracer ', i, '  is ', atmos
 end do
 ! Read the ps
 read (file_id, *) temp_string, state((3 + pcnst)* plev + 1)
-write(*, *) 'ps is ', state((3 + pcnst)*plev + 1)
 
 
 ! Offsets for where the ground and soil state variables start in dart
