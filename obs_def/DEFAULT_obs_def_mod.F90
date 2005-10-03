ed60d24969d682e7d0e0b4bce13f56607aacfaee
Tim Hoar <thoar@ucar.edu>
2005-10-03 16:25:58 -0600

    Removed check_nml_error from use statement.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@1627 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: d27fa5d03e340c8f6a7403c4347e9c30b1cd132d

--- obs_def/DEFAULT_obs_def_mod.F90
+++ obs_def/DEFAULT_obs_def_mod.F90
@@ -18,7 +18,7 @@ module obs_def_mod
 
 use        types_mod, only : r8, missing_i, missing_r8, RAD2DEG
 use    utilities_mod, only : register_module, error_handler, E_ERR, E_MSG, file_exist, &
-                             open_file, check_nml_error, logfileunit, close_file
+                             open_file, logfileunit, close_file
 use     location_mod, only : location_type, read_location, write_location, &
                              interactive_location, set_location_missing
 use time_manager_mod, only : time_type, read_time, write_time, set_time, set_time_missing, &
