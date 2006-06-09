7bf3d954ca366438828496aa25064d2883ebc57f
Tim Hoar <thoar@ucar.edu>
2006-06-09 16:12:03 -0600

    moved entire directory to prep_bufr/lib
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@2015 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 5d06bf03edbde01017f7ba5a81e0cb17d770b3ac

--- ncep_obs/bufr_lib/openbt.f
+++
@@ -1,7 +0,0 @@
-C-----------------------------------------------------------------------
-C  DEFAULT OPENBT RETURNS LUNDX=0 TO INDICATE NO LINKS TO BUFRTABLES
-C-----------------------------------------------------------------------
-      SUBROUTINE OPENBT(LUNDX,MTYP)
-      LUNDX = 0
-      RETURN
-      END
