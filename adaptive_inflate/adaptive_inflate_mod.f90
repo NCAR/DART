52a74f8151a3974fc8b2b20bce94dd4e4512dc54
Nancy Collins <nancy@ucar.edu>
2006-08-02 15:29:01 -0600

    add deterministic_inflate public interface
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@2257 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: dbc70d07beedecccbccfef8db1bfb23f21ee0a70

--- adaptive_inflate/adaptive_inflate_mod.f90
+++ adaptive_inflate/adaptive_inflate_mod.f90
@@ -30,7 +30,7 @@ public :: update_inflation,           adaptive_inflate_end,          do_obs_infl
           do_varying_ss_inflate,      do_single_ss_inflate,          inflate_ens,        &
           adaptive_inflate_init,      adaptive_inflate_type,         get_inflate,        &
           get_sd,                     set_inflate,                   set_sd,             &
-          output_inflate_diagnostics
+          output_inflate_diagnostics, deterministic_inflate
 
 
 ! CVS Generated file description for error handling, do not edit
@@ -254,6 +254,20 @@ type(adaptive_inflate_type), intent(in) :: inflate_handle
 do_single_ss_inflate = (inflate_handle%inflation_flavor == 3)
 
 end function do_single_ss_inflate
+
+
+!------------------------------------------------------------------
+
+function deterministic_inflate(inflate_handle)
+
+! Returns true if deterministic inflation is indicated
+
+logical :: deterministic_inflate
+type(adaptive_inflate_type), intent(in) :: inflate_handle
+
+deterministic_inflate = inflate_handle%deterministic
+
+end function deterministic_inflate
 
 !------------------------------------------------------------------
 
