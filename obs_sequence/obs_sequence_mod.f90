2559f4f97ce6d88b7a25fdc51b1a7c7200b6c68e
Jeff Anderson <jla@ucar.edu>
2006-10-06 13:19:28 -0600

    All obs at upper boundary of obs window now works.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@2403 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 787afbbcf2d8b8e0e43c21e1eaa98d9e9b35833d

--- obs_sequence/obs_sequence_mod.f90
+++ obs_sequence/obs_sequence_mod.f90
@@ -532,7 +532,7 @@ endif
 ! Check for all observations after the last time in the window
 call get_obs_def(seq%obs(current), obs_def)
 cur_time = get_obs_def_time(obs_def)
-if(cur_time >= time2) then
+if(cur_time > time2) then
    out_of_range = .true.
    return
 endif
