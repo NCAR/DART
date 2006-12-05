cd2ae73b96d7bce8c7a7e76f73eb379af4116d12
Tim Hoar <thoar@ucar.edu>
2006-12-05 10:55:56 -0700

    Adding a plot-by-variable-name routine in addition to a plot-by-variable-index routine. 'big' models have named variables, 'low-order' ones do not.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@2492 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: e8d8c8cb0dec5cf118dce4e7c191055ece148d23

--- matlab/PlotSawtooth.m
+++ matlab/PlotSawtooth.m
@@ -53,120 +53,214 @@ function PlotSawtooth( pinfo )
 CheckModelCompatibility(pinfo.prior_file, pinfo.posterior_file)
 
 % Get some information from the truth_file, if it exists.
-if ( isfield(pinfo,'truth_file') ) 
+if ( exist(pinfo.truth_file,'file') == 2 ) 
    CheckModelCompatibility(pinfo.truth_file, pinfo.posterior_file)
 
    ft = netcdf(pinfo.truth_file);
-   t.model      = ft.model(:);
-   t.num_vars   = ncsize(ft('StateVariable')); % determine # of state variables
-   t.num_copies = ncsize(ft('copy'));          % determine # of ensemble members
-   t.num_times  = ncsize(ft('time'));          % determine # of output times
+   truth.model      = ft.model(:);
+   truth.num_vars   = ncsize(ft('StateVariable')); % determine # of state variables
+   truth.num_copies = ncsize(ft('copy'));          % determine # of ensemble members
+   truth.num_times  = ncsize(ft('time'));          % determine # of output times
    close(ft);
 
-   truth_index  = get_copy_index(pinfo.truth_file, 'true state' );
+   truth.truth_index  = get_copy_index(pinfo.truth_file, 'true state' );
+else
+   truth = [];
 end
 
 % Get some information from the prior_file 
 fpr = netcdf(pinfo.prior_file);
-pr.model      = fpr.model(:);
-pr.num_vars   = ncsize(fpr('StateVariable')); % determine # of state variables
-pr.num_copies = ncsize(fpr('copy'));          % determine # of ensemble members
-pr.num_times  = ncsize(fpr('time'));          % determine # of output times
+prior.model      = fpr.model(:);
+prior.num_vars   = ncsize(fpr('StateVariable')); % determine # of state variables
+prior.num_copies = ncsize(fpr('copy'));          % determine # of ensemble members
+prior.num_times  = ncsize(fpr('time'));          % determine # of output times
+prior.times      = getnc(pinfo.prior_file,'time');
 close(fpr);
 
 % Get some information from the posterior_file 
 fpo = netcdf(pinfo.posterior_file);
-po.model      = fpo.model(:);
-po.num_vars   = ncsize(fpo('StateVariable')); % determine # of state variables
-po.num_copies = ncsize(fpo('copy'));          % determine # of ensemble members
-po.num_times  = ncsize(fpo('time'));          % determine # of output times
-po.timeunits  = fpo{'time'}.units(:);
-po.calendar   = fpo{'time'}.calendar(:);
-po.varunits   = fpo{pinfo.var}.units(:);
+post.model      = fpo.model(:);
+post.num_vars   = ncsize(fpo('StateVariable')); % determine # of state variables
+post.num_copies = ncsize(fpo('copy'));          % determine # of ensemble members
+post.num_times  = ncsize(fpo('time'));          % determine # of output times
+post.timeunits  = fpo{'time'}.units(:);
+post.calendar   = fpo{'time'}.calendar(:);
 close(fpr);
 
 % Get the indices for the true state and ensemble mean
 % The metadata is queried to determine which "copy" is appropriate.
-pr_ens_mean_index = get_copy_index(pinfo.prior_file, 'ensemble mean');
-po_ens_mean_index = get_copy_index(pinfo.posterior_file, 'ensemble mean');
+prior.ens_mean_index = get_copy_index(pinfo.prior_file,     'ensemble mean');
+ post.ens_mean_index = get_copy_index(pinfo.posterior_file, 'ensemble mean');
 
 % Get some useful plotting arrays
-times      = getnc(pinfo.prior_file,'time');
-num_times  = length(times);
-ens_mean   = zeros(2,num_times);   % trajectory of ensemble mean
-ens_member = zeros(2,num_times);   % trajectory of ensemble member
-x          = zeros(2,num_times);   % xaxis 
-x(1,:)     = times;
-x(2,:)     = times;
-xax        = x(:);
+ens_mean   = zeros(2,prior.num_times);   % trajectory of ensemble mean
+ens_member = zeros(2,prior.num_times);   % trajectory of ensemble member
+x          = zeros(2,prior.num_times);   % xaxis 
+x(1,:)     = prior.times;
+x(2,:)     = prior.times;
+pinfo.xax  = x(:);
 metadata   = getnc(pinfo.prior_file,'CopyMetaData'); % get all the metadata
 
-      nfigs = length(pinfo.var_inds);  % each variable gets its own figure
-      iplot = 0;
+if isfield(pinfo,'var_inds')
+   PlotGivenIndices( pinfo, prior, post, truth, metadata);
+elseif isfield(pinfo,'var_names')
+   PlotGivenVariable(pinfo, prior, post, truth, metadata);
+end
 
-      for ivar = pinfo.var_inds,
+%------------------------------------------------------------------------------
+% Plot given an index into the state-space vector
+%------------------------------------------------------------------------------
 
-         % Get the data from the netcdf files
+function PlotGivenIndices(pinfo, prior, post, truth, metadata)
 
-         po_ens_mean = get_var_series(pinfo.prior_file,    pinfo.var,po_ens_mean_index,ivar);
-         pr_ens_mean = get_var_series(pinfo.posterior_file,pinfo.var,pr_ens_mean_index,ivar);
+   nfigs = length(pinfo.var_inds);  % each variable gets its own figure
+   iplot = 0;
 
+   for ivar = pinfo.var_inds,
 
-         % Now we paste them together in a clever way to show the effect of the assimilation
+      % Get the data from the netcdf files
 
-	 ens_mean(  1,:) = po_ens_mean;
-	 ens_mean(  2,:) = pr_ens_mean;
-	 a = ens_mean(:);       % unrolls them post, prior, post, prior, post ...
+      po_ens_mean = get_var_series(pinfo.posterior_file, pinfo.var, ...
+                                   post.ens_mean_index, ivar);
+      pr_ens_mean = get_var_series(pinfo.prior_file, pinfo.var, ...
+                                   prior.ens_mean_index, ivar);
 
-	 % Plot the true trajectory if it exists; the ens mean; annotate
+      % Now we paste them together in a clever way to show 
+      % the effect of the assimilation
+      ens_mean(  1,:) = pr_ens_mean;
+      ens_mean(  2,:) = po_ens_mean;
+      a               = ens_mean(:);
 
-         iplot = iplot + 1;
-         figure(iplot); clf; 
+      % Plot the true trajectory if it exists; the ens mean; annotate
 
-	 if (isfield(pinfo,'truth_file'))
-            truth = get_var_series(pinfo.truth_file, pinfo.var, truth_index, ivar);
-	    h = plot(times, truth,'k-','linewidth',1.0); hold on;
-	    h = plot(xax, a, 'k-','linewidth',2.0);
-	    legend('truth','ensemble mean')
-         else 
-	    h = plot(xax, a, 'k-','linewidth',2.0); hold on;
-	    legend('ensemble mean')
-         end
+      iplot = iplot + 1;
+      figure(iplot); clf; 
 
-	 ylabel(sprintf('''%s'' index %d', pinfo.var, ivar ))
-	 xlabel(sprintf('model time (%d timesteps)', num_times ))
-	 title(sprintf('%s Trajectories',pinfo.model), ...  
+      if (isfield(pinfo,'truth_file'))
+         true_trajectory = get_var_series(pinfo.truth_file, pinfo.var, truth.truth_index, ivar);
+	 h = plot(prior.times, true_trajectory, 'k-','linewidth',1.0); hold on;
+	 h = plot(pinfo.xax, a, 'k-','linewidth',2.0);
+	 legend('truth','ensemble mean')
+      else 
+	 h = plot(pinfo.xax, a, 'k-','linewidth',2.0); hold on;
+	 legend('ensemble mean')
+      end
+
+      ylabel(sprintf('''%s'' index %d', pinfo.var, ivar ))
+      xlabel(sprintf('model time (%d timesteps)', prior.num_times ))
+      title(sprintf('%s Trajectories',pinfo.model), ...  
                      'interpreter','none','fontweight','bold')
 
-	 % Now check to see if we are overlaying any individual ensemble members.
-	 % if pinfo.copyindices = [], nothing happens. 
+      % Now check to see if we are overlaying any individual ensemble members.
+      % if pinfo.copyindices = [], nothing happens. 
 
-	 ens_colors = get(gca,'ColorOrder');   % trying to cycle through colors
-	 ncolors = size(ens_colors,1) - 1;     % last one is black, already used.
+      ens_colors = get(gca,'ColorOrder');   % trying to cycle through colors
+      ncolors = size(ens_colors,1) - 1;     % last one is black, already used.
 
-	 nmem = 0;
-	 for imem = pinfo.copyindices
-            nmem = nmem + 1;
+      nmem = 0;
+      for imem = pinfo.copyindices
+         nmem = nmem + 1;
 
-            po_series   = get_var_series(pinfo.prior_file,     pinfo.var, imem, ivar);
-            pr_series   = get_var_series(pinfo.posterior_file, pinfo.var, imem, ivar);
+	 %str1 = sprintf('ensemble member %d',imem);
+	 str1 = deblank(metadata(imem,:));
+         copy_index = get_copy_index(pinfo.prior_file, str1);
+         po_series  = get_var_series(pinfo.posterior_file, pinfo.var, copy_index, ivar);
+         pr_series  = get_var_series(pinfo.prior_file,     pinfo.var, copy_index, ivar);
 
-	    %str1 = sprintf('ensemble member %d',imem);
-	    str1 = deblank(metadata(imem,:));
+	 ens_member(1,:) = pr_series;
+	 ens_member(2,:) = po_series;
+	 b               = ens_member(:);
 
-	    ens_member(1,:) = po_series;
-	    ens_member(2,:) = pr_series;
-	    b               = ens_member(:);     % unrolls them post, prior, post, prior, post ...
+   	 hold on; 
+	 memcolor = 1 + mod(nmem-1,ncolors); % cycles through colors [1,6]
+	 h = plot(pinfo.xax, b,'linewidth',0.5,'Color',ens_colors(memcolor,:));
 
-   	    hold on; 
-	    memcolor = 1 + mod(nmem-1,ncolors); % cycles through colors [1,6]
-	    h = plot(xax, b,'linewidth',0.5,'Color',ens_colors(memcolor,:));
-
-	    [legh, objh, outh, outm] = legend;
-	    nlines = length(outm);
-            outm{nlines+1} = str1;
-	    [legh, objh, outh, outm] = legend([outh; h],outm,0);
-	 end
-	 legend boxoff
-
+	 [legh, objh, outh, outm] = legend;
+	 nlines = length(outm);
+         outm{nlines+1} = str1;
+	 [legh, objh, outh, outm] = legend([outh; h],outm,0);
       end
+      legend boxoff
+
+   end
+
+%------------------------------------------------------------------------------
+% Plot given an index into the state-space vector
+%------------------------------------------------------------------------------
+
+function PlotGivenVariable(pinfo, prior, post, truth, metadata)
+
+   var_names = strread(pinfo.var_names,'%s','delimiter',' ');
+
+   nfigs = length(var_names);  % each variable gets its own figure
+   iplot = 0;
+
+   for ivar = 1:nfigs
+
+      iplot = iplot + 1;
+      figure(iplot); clf; 
+
+      % Get the data from the netcdf files
+
+      vname  = var_names{ivar};
+
+      if ( strcmp(lower(vname),'ps') ==1 ) %  PS(time, copy, lat, lon)
+         corner = [  1 NaN pinfo.latindex pinfo.lonindex];
+         endpnt = [ -1 NaN pinfo.latindex pinfo.lonindex];
+      else % U(time, copy, lat, lon, lev)
+         corner = [  1 NaN pinfo.latindex pinfo.lonindex pinfo.levelindex];
+         endpnt = [ -1 NaN pinfo.latindex pinfo.lonindex pinfo.levelindex];
+      end
+
+      ens_colors = get(gca,'ColorOrder');   % trying to cycle through colors
+      ncolors = size(ens_colors,1) - 1;     % last one is black, already used.
+
+      for i = 1:pinfo.copies
+
+         imem = pinfo.copyindices(i);
+
+         corner(2) = imem;
+         endpnt(2) = imem;
+         pr_series = getnc(pinfo.prior_file,     vname, corner, endpnt);
+         po_series = getnc(pinfo.posterior_file, vname, corner, endpnt);
+
+	 str1 = deblank(metadata(imem,:));
+
+	 ens_member(1,:) = pr_series;
+	 ens_member(2,:) = po_series;
+	 b               = ens_member(:);
+
+   	 hold on; 
+	 memcolor = 1 + mod(i-1,ncolors); % cycles through colors [1,6]
+
+         if (strcmp(str1,'ensemble mean') == 1)
+	    h = plot(pinfo.xax, b,'k-','linewidth',2.0);
+         else
+	    h = plot(pinfo.xax, b,'linewidth',0.5,'Color',ens_colors(memcolor,:));
+         end
+
+	 [legh, objh, outh, outm] = legend;
+	 nlines = length(outm);
+         outm{nlines+1} = str1;
+	 [legh, objh, outh, outm] = legend([outh; h],outm,0);
+      end
+
+      % Plot the true trajectory if it exists; the ens mean; annotate
+
+      if ( exist(pinfo.truth_file) == 2 )
+         corner(2) = truth.truth_index;
+         endpnt(2) = truth.truth_index;
+         true_trajectory = getnc(pinfo.truth_file, vname, corner, endpnt);
+	 h = plot(prior.times, true_trajectory, 'k-','linewidth',1.0); hold on;
+	 h = plot(pinfo.xax, a, 'k-','linewidth',2.0);
+	 legend('truth','ensemble mean')
+      end
+
+      ylabel(sprintf('''%s''  (%.3f,%.3fE) level index %d', vname, ...
+                     pinfo.latitude, pinfo.longitude, pinfo.levelindex ))
+      xlabel(sprintf('model time (%d timesteps)', prior.num_times ))
+      title(sprintf('%s Trajectories',pinfo.model), ...  
+                     'interpreter','none','fontweight','bold')
+      legend boxoff
+
+   end
