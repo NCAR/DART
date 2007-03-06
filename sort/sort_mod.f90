6c1b01d11e72029e7fefa82605ec2427d8572c69
Tim Hoar <thoar@ucar.edu>
2007-03-06 15:50:23 -0700

    Replace slow o(n*n) bubble sort with heap sort. (CVS 1.9)
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@2649 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: d10a594f29200f3f2d3f20e22643258d031f9211

--- sort/sort_mod.f90
+++ sort/sort_mod.f90
@@ -18,7 +18,7 @@ use utilities_mod, only : register_module
 implicit none
 private
 
-public :: sort, index_sort
+public :: slow_sort, slow_index_sort, sort, index_sort
 
 ! let CVS fill strings ... DO NOT EDIT ...
 character(len=128) :: &
@@ -44,36 +44,36 @@ end subroutine initialize_module
 
 ! a silly, inefficient sort for real(r8) array data
 
-function sort(x)
+function slow_sort(x)
 
 implicit none
 
 real(r8), intent(in) :: x(:)
-real(r8) :: sort(size(x))
+real(r8) :: slow_sort(size(x))
 real(r8) :: tmp
 integer j, k
 
 if ( .not. module_initialized ) call initialize_module
 
-! Copy to sort
-sort = x
+! Copy to slow_sort
+slow_sort = x
 
 !  DO A SILLY N^2 SORT
 do j = 1, size(x) - 1
    do k = j + 1, size(x)
 !  EXCHANGE TWO ELEMENTS IF THEY'RE IN THE WRONG ORDER
-      if(sort(j) .gt. sort(k)) then
-         tmp = sort(k)
-         sort(k) = sort(j)
-         sort(j) = tmp
+      if(slow_sort(j) .gt. slow_sort(k)) then
+         tmp = slow_sort(k)
+         slow_sort(k) = slow_sort(j)
+         slow_sort(j) = tmp
       end if
    end do
 end do
-end function sort
+end function slow_sort
 
 !=======================================================================
 
-   subroutine index_sort(dist, index, num)
+   subroutine slow_index_sort(dist, index, num)
 
 !  real(r8) indexed sort
 
@@ -106,7 +106,144 @@ end do
 !   do 30 j = 1, num
 ! 30      write(*, *) j, dist(index(j))
 !   return
-   end subroutine index_sort
+   end subroutine slow_index_sort
+
+
+!=========================================================================
+
+function sort(x)
+
+! Uses a heap sort alogrithm on x, returns sorted array
+implicit none
+
+real(r8), intent(in) :: x(:)
+real(r8)             :: sort(size(x))
+
+integer  :: num, level, ind, i, j
+real(r8) :: l_val
+
+! Get the size
+num = size(x)
+
+! Initial copy over
+sort = x
+
+! Only one element, just send it back
+if(num <= 1) return
+
+level = num / 2 + 1
+ind = num
+
+! Keep looping until finished
+do
+   ! Keep going down levels until bottom
+   if(level > 1) then
+      level = level - 1
+      l_val = sort(level)
+   else
+      l_val = sort(ind)
+      sort(ind) = sort(1)
+      ind = ind - 1
+      if(ind == 1) then
+         sort(1) = l_val
+         return
+      endif
+   endif
+
+   i = level
+   j = 2 * level
+
+   do while(j <= ind)
+      if(j < ind) then
+         if(sort(j) < sort(j + 1)) j = j + 1
+      endif
+      if(l_val < sort(j)) then
+         sort(i) = sort(j)
+         i = j
+         j = 2 * j
+      else
+         j = ind + 1
+      endif
+      
+   end do
+   sort(i) = l_val
+
+end do
+
+end function sort
+
+
+!=========================================================================
+
+
+subroutine index_sort(x, index, num)
+
+! Uses a heap sort alogrithm on x, returns array of sorted indices
+implicit none
+
+integer,  intent(in)  :: num
+real(r8), intent(in)  :: x(num)
+integer,  intent(out) :: index(num)
+
+integer  :: evel, ind, i, j, l_val_index, level 
+real(r8) :: l_val
+
+
+if ( .not. module_initialized ) call initialize_module
+
+!  INITIALIZE THE INDEX ARRAY TO INPUT ORDER
+do i = 1, num
+   index(i) = i
+end do
+
+! Only one element, just send it back
+if(num <= 1) return
+
+level = num / 2 + 1
+ind = num
+
+! Keep looping until finished
+do
+   ! Keep going down levels until bottom
+   if(level > 1) then
+      level = level - 1
+      l_val = x(index(level))
+      l_val_index = index(level)
+   else
+      l_val = x(index(ind))
+      l_val_index = index(ind)
+
+
+      index(ind) = index(1)
+      ind = ind - 1
+      if(ind == 1) then
+         index(1) = l_val_index
+         return
+      endif
+   endif
+
+   i = level
+   j = 2 * level
+
+   do while(j <= ind)
+      if(j < ind) then
+         if(x(index(j)) < x(index(j + 1))) j = j + 1
+      endif
+      if(l_val < x(index(j))) then
+         index(i) = index(j)
+         i = j
+         j = 2 * j
+      else
+         j = ind + 1
+      endif
+      
+   end do
+   index(i) = l_val_index
+
+end do
+
+end subroutine index_sort
+
 
 !=========================================================================
 
