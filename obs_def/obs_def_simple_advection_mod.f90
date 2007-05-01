d68956943ea32fa8e7f9a564dc9717c9bcca6151
Tim Hoar <thoar@ucar.edu>
2007-05-01 15:27:04 -0600

    initial commit of a 1D advection-diffusion model. very limited matlab support at this time
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@2887 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: d299f21e24b6011bffa0a20f004a8187f9548ddb

---
+++ obs_def/obs_def_simple_advection_mod.f90
@@ -0,0 +1,44 @@
+! Data Assimilation Research Testbed -- DART
+! Copyright 2004-2007, Data Assimilation Research Section
+! University Corporation for Atmospheric Research
+! Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+
+! <next few lines under version control, do not edit>
+! $URL: http://subversion.ucar.edu/DAReS/DART/trunk/obs_def/obs_def_simple_advection_mod.f90 $
+! $Id: obs_def_simple_advection_mod.f90 2713 2007-03-26 04:09:04Z thoar $
+! $Revision: 2713 $
+! $Date: 2007-03-25 22:09:04 -0600 (Sun, 25 Mar 2007) $
+
+! BEGIN DART PREPROCESS KIND LIST
+!VELOCITY,                     KIND_VELOCITY
+!TRACER_CONCENTRATION,         KIND_TRACER_CONCENTRATION
+!TRACER_SOURCE,                KIND_TRACER_SOURCE
+! END DART PREPROCESS KIND LIST
+
+! BEGIN DART PREPROCESS USE OF SPECIAL OBS_DEF MODULE
+!! No use statements are required for the simple_advection obs_def module
+! END DART PREPROCESS USE OF SPECIAL OBS_DEF MODULE
+
+! BEGIN DART PREPROCESS GET_EXPECTED_OBS_FROM_DEF
+!         case(VELOCITY)
+!            call interpolate(state, location, KIND_VELOCITY, obs_val, istatus)         
+!         case(TRACER_CONCENTRATION)
+!            call interpolate(state, location, KIND_TRACER_CONCENTRATION, obs_val, istatus)         
+!         case(TRACER_SOURCE)
+!            call interpolate(state, location, KIND_TRACER_SOURCE, obs_val, istatus)
+! END DART PREPROCESS GET_EXPECTED_OBS_FROM_DEF
+
+! BEGIN DART PREPROCESS READ_OBS_DEF
+!case(VELOCITY, TRACER_CONCENTRATION, TRACER_SOURCE)
+!      continue
+! END DART PREPROCESS READ_OBS_DEF
+
+! BEGIN DART PREPROCESS WRITE_OBS_DEF
+!case(VELOCITY, TRACER_CONCENTRATION, TRACER_SOURCE)
+!      continue
+! END DART PREPROCESS WRITE_OBS_DEF
+
+! BEGIN DART PREPROCESS INTERACTIVE_OBS_DEF
+!case(VELOCITY, TRACER_CONCENTRATION, TRACER_SOURCE)
+!      continue
+! END DART PREPROCESS INTERACTIVE_OBS_DEF
