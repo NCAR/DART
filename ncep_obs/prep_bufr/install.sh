93c360b4f561e5fac1ee5e47e3cf94a5b40ee516
Nancy Collins <nancy@ucar.edu>
2007-07-27 09:45:41 -0600

    no need for a whole subdirectory for a simple install script.  is it much more visible here in the top level dir anyway
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@3027 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 463ebb86a712e1826637e3e2ce2d35ee13a6a8b9

---
+++ ncep_obs/prep_bufr/install.sh
@@ -0,0 +1,93 @@
+#!/bin/sh 
+#
+# <next few lines under version control, do not edit>
+# $URL: http://subversion.ucar.edu/DAReS/DART/trunk/ncep_obs/prep_bufr/install/install.sh $
+# $Id: install.sh 2691 2007-03-11 18:18:09Z thoar $
+# $Revision: 2691 $
+# $Date: 2007-03-11 12:18:09 -0600 (Sun, 11 Mar 2007) $
+
+#  ------------------------------------------------------------------------
+#  This script will make executables which extract data
+#  from ADP BUFR input files, and place the data into a basic text file.
+#  prepbufr.x:  used to extract data from prepbufr files
+#  ** Make sure the "ar" command location has been set in your path
+#  environment variable.  Type "which ar" to check if this is done. **
+#  ------------------------------------------------------------------------
+ 
+set -eua
+ 
+#  ------------------------------------------------------------------------
+#  CPLAT - platform type (linux, sgi, aix, sun, macosx)
+#  ------------------------------------------------------------------------
+ 
+CPLAT=macosx
+#CPLAT=linux
+
+#  set up the compilers to use
+#  -----------------------------------------------------
+ 
+if [ $CPLAT = sgi ]
+then
+   cc=cc; ff=f77
+elif [ $CPLAT = linux ]
+then
+   cc=cc; ff=pgf90
+elif [ $CPLAT = aix ]
+then
+   cc=cc; ff=f77
+elif [ $CPLAT = sun ]
+then
+   cc=cc; ff=f77
+elif [ $CPLAT = macosx ]
+then
+   cc=gcc; ff=gfortran
+fi
+
+#  Compile and archive the Bufr Library
+#  ------------------------------------
+
+echo 'Compiling the Bufr library'
+
+cd lib
+$ff -c *.f
+ar crv bufrlib.a *.o
+rm *.o
+ 
+#  Compile and link the decode programs
+#  ---------------------------------------
+
+echo 'Compiling the prepbufr programs'
+
+cd ../src
+$ff prepbufr.f     ../lib/bufrlib.a -o ../exe/prepbufr.x 
+$ff prepbufr_03Z.f ../lib/bufrlib.a -o ../exe/prepbufr_03Z.x 
+
+ 
+# ONLY IF NEEDED, compile the 2 auxiliary conversion programs
+#  (comment one or both of these sections in)
+
+#  Compile the binary format converter program
+#  ---------------------------------------
+ 
+#echo 'Compiling the grabbufr converter program'
+ 
+#cd ../convert_bufr
+#$ff grabbufr.f ../lib/bufrlib.a -o ../exe/grabbufr.x
+ 
+ 
+#  Compile the block/unblock converter program
+#  ---------------------------------------
+ 
+#echo 'Compiling the blk/ublk converter program'
+ 
+#cd ../blk_ublk
+#./cwordsh.make
+ 
+
+#  clean up
+#  --------
+
+echo 'Finished making executables'
+cd ..
+
+exit 0
