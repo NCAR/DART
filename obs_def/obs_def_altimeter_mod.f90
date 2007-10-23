85f8eaa6ae3a665a6c8c8d359dcf0bd13af99482
Nancy Collins <nancy@ucar.edu>
2007-10-23 16:15:49 -0600

    initial version of an altimeter observation type.  requires that the model be able to interpolate both a surface pressure (in Pa) and a surface elevation (in meters above sea level).   original code from ryan torn with input from david dowell
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@3122 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 6cd9dc907814bf8a546ece8eb29aef1c9c0e36d0

---
+++ obs_def/obs_def_altimeter_mod.f90
@@ -0,0 +1,123 @@
+module obs_def_altimeter_mod
+
+! BEGIN DART PREPROCESS KIND LIST
+! MARINE_SFC_PRESSURE, KIND_SURFACE_PRESSURE
+! LAND_SFC_PRESSURE, KIND_SURFACE_PRESSURE
+! END DART PREPROCESS KIND LIST
+
+! BEGIN DART PREPROCESS USE OF SPECIAL OBS_DEF MODULE
+!   use obs_def_altimeter_mod, only : get_expected_altimeter, compute_altimeter
+! END DART PREPROCESS USE OF SPECIAL OBS_DEF MODULE
+
+! BEGIN DART PREPROCESS GET_EXPECTED_OBS_FROM_DEF
+!         case(MARINE_SFC_PRESSURE, LAND_SFC_PRESSURE)
+!            call get_expected_altimeter(state, location, obs_val, istatus)
+! END DART PREPROCESS GET_EXPECTED_OBS_FROM_DEF
+
+! BEGIN DART PREPROCESS READ_OBS_DEF
+!         case(MARINE_SFC_PRESSURE, LAND_SFC_PRESSURE)
+!            continue
+! END DART PREPROCESS READ_OBS_DEF
+
+! BEGIN DART PREPROCESS WRITE_OBS_DEF
+!         case(MARINE_SFC_PRESSURE, LAND_SFC_PRESSURE)
+!            continue
+! END DART PREPROCESS WRITE_OBS_DEF
+
+! BEGIN DART PREPROCESS INTERACTIVE_OBS_DEF
+!         case(MARINE_SFC_PRESSURE, LAND_SFC_PRESSURE)
+!            continue
+! END DART PREPROCESS INTERACTIVE_OBS_DEF
+
+
+use        types_mod, only : r8, missing_r8, t_kelvin
+use    utilities_mod, only : register_module, error_handler, E_ERR, E_MSG
+use     location_mod, only : location_type, set_location, get_location , write_location, &
+                             read_location
+use  assim_model_mod, only : interpolate
+use     obs_kind_mod, only : KIND_SURFACE_PRESSURE, KIND_SURFACE_ELEVATION
+
+implicit none
+private
+
+public :: get_expected_altimeter, compute_altimeter
+
+! CVS Generated file description for error handling, do not edit
+character(len=128) :: &
+source   = "$Source: /home/thoar/CVS.REPOS/DART/obs_def/obs_def_altimeter_mod.f90,v $", &
+revision = "$Revision: 3093 $", &
+revdate  = "$Date: 2007-09-01 16:01:48 -0600 (Sat, 01 Sep 2007) $"
+
+logical, save :: module_initialized = .false.
+
+contains
+
+!----------------------------------------------------------------------
+
+subroutine initialize_module
+
+call register_module(source, revision, revdate)
+module_initialized = .true.
+
+end subroutine initialize_module
+
+
+subroutine get_expected_altimeter(state_vector, location, altimeter_setting, istatus)
+
+real(r8),            intent(in)  :: state_vector(:)
+type(location_type), intent(in)  :: location
+real(r8),            intent(out) :: altimeter_setting     ! altimeter (hPa)
+integer,             intent(out) :: istatus
+
+real(r8) :: psfc                ! surface pressure value   (Pa)
+real(r8) :: hsfc                ! surface elevation level  (m above SL)
+
+character(len=129) :: errstring
+
+if ( .not. module_initialized ) call initialize_module
+
+!  interpolate the surface pressure to the desired location
+call interpolate(state_vector, location, KIND_SURFACE_PRESSURE, psfc, istatus)
+if (istatus /= 0) then
+   altimeter_setting = missing_r8
+   return
+endif
+
+!  interpolate the surface elevation to the desired location
+call interpolate(state_vector, location, KIND_SURFACE_ELEVATION, hsfc, istatus)
+if (istatus /= 0) then
+   altimeter_setting = missing_r8
+   return
+endif
+
+!  Compute the altimeter setting given surface pressure and height, altimeter is hPa
+altimeter_setting = compute_altimeter(psfc * 0.01, hsfc)
+
+if (altimeter_setting < 880.0_r8 .or. altimeter_setting >= 1100.0_r8) then
+   altimeter_setting = missing_r8
+   if (istatus == 0) istatus = 1
+   return
+endif
+
+return
+end subroutine get_expected_altimeter
+
+
+function compute_altimeter(psfc, hsfc)
+
+real(r8), parameter :: k1 = 0.190284_r8
+real(r8), parameter :: k2 = 8.4228807E-5_r8
+
+real(r8), intent(in) :: psfc  !  (hPa)
+real(r8), intent(in) :: hsfc  !  (m above MSL)
+
+real(r8) :: compute_altimeter !  (hPa)
+
+compute_altimeter = ((psfc - 0.3) ** k1 + k2 * hsfc) ** (1 / k1)
+
+return
+end function compute_altimeter
+
+!----------------------------------------------------------------------------
+
+end module obs_def_altimeter_mod
