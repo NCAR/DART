b07653122b5d63ff97d6a3c7153408b683d4f955
Tim Hoar <thoar@ucar.edu>
2007-11-15 16:33:57 -0700

    Returns just the 'atmospheric' variable names from an open netcdf file - the coordinate variables are ignored.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@3143 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 1b972df965eb2da7748768373cd5bba54b847c49

---
+++ diagnostics/matlab/get_varnames.m
@@ -0,0 +1,59 @@
+function bob = get_varnames(fid)
+% get_varnames returns JUST the 'atmospheric' variable names in the netCDF file - ie - not the coordinate variables.
+%
+% the result is a cell array of strings ... must use {} notation to address elements.
+%
+% EXAMPLE:
+% fid = netcdf('obs_seq.final.nc','nowrite');
+% varnames = get_varnames(fid);
+% varnames{:}
+% nvars = length(varnames);
+% disp(sprintf('first atmospheric variable (of %d) is %s',nvars,varnames{1}))
+
+% Data Assimilation Research Testbed -- DART
+% Copyright 2004-2007, Data Assimilation Research Section
+% University Corporation for Atmospheric Research
+% Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+%
+% <next few lines under version control, do not edit>
+% $URL$
+% $Id$
+% $Revision$
+% $Date$
+
+% Use the netcdf primitives 'var','coord' to get complete list
+% of (all) variables and the coordinate variables, respectively.
+
+variables  =   var(fid);
+coordvars  = coord(fid);
+
+atmosvarinds = VariableIndices(coordvars,variables);
+
+% coerce just the names into a cell array 
+
+for i = 1:length(atmosvarinds)
+   bob{i} = name(variables{atmosvarinds(i)});
+end
+
+
+
+function inds = VariableIndices(coordvars,variables)
+% returns the indices of the atmospheric variables.
+
+inds = [];
+
+for i = 1:length(variables)
+
+   varname    = name(variables{i});
+   isatmosvar = 1;
+   
+   for j = 1:length(coordvars)
+      if (strcmp( varname , name(coordvars{j}))), isatmosvar = 0; end 
+      if (strcmp( varname ,      'time_bounds')), isatmosvar = 0; end 
+      if (strcmp( varname ,     'region_names')), isatmosvar = 0; end 
+   end
+
+   if (isatmosvar > 0)
+    inds = [inds i];
+   end
+end
