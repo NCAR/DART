a6526810d6e22748281dbc538bb47a065521280b
Tim Hoar <thoar@ucar.edu>
2007-11-15 16:08:21 -0700

    support function to get copy index given copy name. case-insensitive, whitespace-insensitive
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@3141 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 20b727c2df3a69d95981e6d209523a8056be7748

---
+++ diagnostics/matlab/get_obs_copy_index.m
@@ -0,0 +1,36 @@
+function indx = get_obs_copy_index(fid,copystring)
+% 
+%
+
+% Data Assimilation Research Testbed -- DART
+% Copyright 2004-2007, Data Assimilation Research Section
+% University Corporation for Atmospheric Research
+% Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+%
+% <next few lines under version control, do not edit>
+% $URL$
+% $Id$
+% $Revision$
+% $Date$
+
+
+copynames = fid{'copy'};
+ncopies   = size(copynames,1);
+
+indx      = -1; % Assume Failure
+
+for i = 1:ncopies
+
+   x = lower(deblank(copystring));
+   y = lower(deblank(copynames(i,:)));
+
+   if ( strcmp(x,y) )
+      indx = i;
+      break
+   end 
+
+end
+
+if (indx < 1)
+   error(sprintf('Did not find copy index for %s ',copystring))
+end
