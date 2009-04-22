5f5c46b2c1ac7360603de6e3fe06c99070bb8a1e
Nancy Collins <nancy@ucar.edu>
2009-04-22 09:14:01 -0600

    make the default to have the system interface block in, which matches most compilers, and add an implicit none.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@3829 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: f6c533f5e0abc942a7e1c474ff7dde9c27c443a5

--- doc/mpi/ftest_mpi.f90
+++ doc/mpi/ftest_mpi.f90
@@ -31,8 +31,11 @@ program ftest_mpi
 ! which defines the interfaces to the MPI library routines, or an include
 ! file which defines constants.  Try to use the module if it is available.
 
-!use mpi
-include "mpif.h"
+use mpi
+
+implicit none
+
+!include "mpif.h"
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -45,18 +48,17 @@ include "mpif.h"
 ! you an error about an undefined symbol (something like '_system_').  
 ! Comment this block in or out as needed.
 
-! ! interface block for getting return code back from system() routine
-! interface
-!  function system(string)
-!   character(len=*) :: string
-!   integer :: system
-!  end function system
-! end interface
-! ! end block
+ ! interface block for getting return code back from system() routine
+ interface
+  function system(string)
+   character(len=*) :: string
+   integer :: system
+  end function system
+ end interface
+ ! end block
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-implicit none
 
 ! integer variables
 integer :: ierror, myrank, totalprocs, rc
