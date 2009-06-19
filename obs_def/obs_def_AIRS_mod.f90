bb5338411bc0a5a03626b7b0dfa7f62b7166eda2
Nancy Collins <nancy@ucar.edu>
2009-06-19 13:03:16 -0600

    Definitions for the AIRS observation types.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@3938 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 7acf257698f13a063179e5975dc54a1a25d22532

---
+++ obs_def/obs_def_AIRS_mod.f90
@@ -0,0 +1,20 @@
+! Data Assimilation Research Testbed -- DART
+! Copyright 2004-2007, Data Assimilation Research Section
+! University Corporation for Atmospheric Research
+! Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+
+! <next few lines under version control, do not edit>
+! $URL: https://subversion.ucar.edu/DAReS/DART/trunk/obs_def/obs_def_QuikSCAT_mod.f90 $
+! $Id: obs_def_QuikSCAT_mod.f90 3441 2008-07-01 23:43:29Z thoar $
+! $Revision: 3441 $
+! $Date: 2008-07-01 17:43:29 -0600 (Tue, 01 Jul 2008) $
+
+! This module supports the observation types from the AIRS instruments.
+! http://winds.jpl.nasa.gov/missions/quikscat/index.cfm
+
+! BEGIN DART PREPROCESS KIND LIST
+! AIRS_TEMPERATURE,        KIND_TEMPERATURE,        COMMON_CODE
+! AIRS_SPECIFIC_HUMIDITY,  KIND_SPECIFIC_HUMIDITY,  COMMON_CODE
+! END DART PREPROCESS KIND LIST
+
+
