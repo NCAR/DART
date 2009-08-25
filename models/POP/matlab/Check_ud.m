75e358d777d2ac82c11d48dd5cbb6992a568ebc4
Tim Hoar <thoar@ucar.edu>
2009-08-25 17:22:31 -0600

    Better output format for reporting differences. Also creates an 'advance-to' file for testing.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@4012 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: d05220e37f3bbb923ca37cae3dcc4b70d5893bb3

--- models/POP/matlab/Check_ud.m
+++ models/POP/matlab/Check_ud.m
@@ -1,14 +1,24 @@
-function [dart pop] = Check_ud(dartfile,popfile)
-% Check_ud : check the conversion of a POP restart to a DART state vector.
-% dartfile = 'assim_model_state_ud';
+function [dart pop] = Check_ud(popfile,dartfile)
+% Check_ud : check pop_to_dart.f90 ... the conversion of a POP restart to a DART state vector file.
+%
 %  popfile = 'pop.r.nc' 
-% x        = Check_ud(dartfile,popfile);
+% dartfile = 'assim_model_state_ud';
+% x        = Check_ud(popfile, dartfile);
 %
-%
-% dartfile = '/fs/image/home/thoar/SVN/DART/models/POP/work/assim_model_state_ud';
 %  popfile = '/fs/image/home/thoar/SVN/DART/models/POP/work/cx3.dart.001.pop.r.0002-01-01-00000.nc';
-% [dart pop] = Check_ud(dartfile,popfile);
+% dartfile = '/fs/image/home/thoar/SVN/DART/models/POP/work/perfect_ics';
+% [dart pop] = Check_ud(popfile, dartfile);
+
+% Data Assimilation Research Testbed -- DART
+% Copyright 2004-2007, Data Assimilation Research Section
+% University Corporation for Atmospheric Research
+% Licensed under the GPL -- www.gpl.org/licenses/gpl.html
 %
+% <next few lines under version control, do not edit>
+% $URL$
+% $Id$
+% $Revision$
+% $Date$
 
 % Read the original POP file values.
 % The nc_varget() function returns the variables with the fastest 
@@ -80,11 +90,45 @@ dart.days     = days;
 
 % Find the range of the mismatch
 
-Sdiff     = pop.S     - dart.S;     [min(    Sdiff(:)) max(    Sdiff(:))]
-Tdiff     = pop.T     - dart.T;     [min(    Tdiff(:)) max(    Tdiff(:))]
-Udiff     = pop.U     - dart.U;     [min(    Udiff(:)) max(    Udiff(:))]
-Vdiff     = pop.V     - dart.V;     [min(    Vdiff(:)) max(    Vdiff(:))]
-PSURFdiff = pop.PSURF - dart.PSURF; [min(PSURFdiff(:)) max(PSURFdiff(:))]
+d = pop.S     - dart.S;     disp(sprintf('S     diffs are %0.8g %0.8g',min(d(:)),max(d(:))))
+d = pop.T     - dart.T;     disp(sprintf('T     diffs are %0.8g %0.8g',min(d(:)),max(d(:))))
+d = pop.U     - dart.U;     disp(sprintf('U     diffs are %0.8g %0.8g',min(d(:)),max(d(:))))
+d = pop.V     - dart.V;     disp(sprintf('V     diffs are %0.8g %0.8g',min(d(:)),max(d(:))))
+d = pop.PSURF - dart.PSURF; disp(sprintf('PSURF diffs are %0.8g %0.8g',min(d(:)),max(d(:))))
+
+% As an added bonus, we create an 'assim_model_state_ic' file with an 
+% advance-to-time one day in the future.
+
+% Open and read timetag for state
+fid     = fopen(dartfile,'rb','ieee-le');
+trec1   = fread(fid,1,'int32');
+seconds = fread(fid,1,'int32');
+days    = fread(fid,1,'int32');
+trecN   = fread(fid,1,'int32');
+
+% read state vector variables.
+rec1     = fread(fid,     1,  'int32');
+datvec   = fread(fid, n3ditems+n2ditems, 'float64');
+recN     = fread(fid,     1,  'int32');
+fclose(fid);
+
+% Open and write advance_to_time
+fid     = fopen('test.ic','wb','ieee-le');
+fwrite(fid,  trec1,'int32');
+fwrite(fid,seconds,'int32');
+fwrite(fid,   days,'int32');
+fwrite(fid,  trecN,'int32');
+
+fwrite(fid,  trec1,'int32');
+fwrite(fid,seconds,'int32');
+fwrite(fid,   days+1,'int32');
+fwrite(fid,  trecN,'int32');
+
+% read state vector variables.
+fwrite(fid,   rec1, 'int32');
+fwrite(fid, datvec, 'float64');
+fwrite(fid,   recN, 'int32');
+fclose(fid);
 
 
 function C = get_3D_permuted(fid, shape, typestr)
