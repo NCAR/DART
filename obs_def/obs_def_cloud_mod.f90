f2a015c55a15a77f67887e81a290284e4d487d98
Nancy Collins <nancy@ucar.edu>
2009-11-06 12:46:23 -0700

    Only added comment about the inconsistency with the names of the mixing ratio value.  No actual code change.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@4135 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 37c48afa8086ac6c240632f5993731309f2ff3e2

--- obs_def/obs_def_cloud_mod.f90
+++ obs_def/obs_def_cloud_mod.f90
@@ -9,6 +9,14 @@
 !CLOUD_FRACTION,     KIND_CLOUD_FRACTION,      COMMON_CODE
 ! END DART PREPROCESS KIND LIST
 
+! eventually these should become the following to be consistent
+! with other types of mixing ratios.  this is just a proposed
+! rename of the KIND; any code using it would remain the same.
+! but multiple models and forward operators could be using the old
+! names; the change must be coordinated across the entire project.
+!!KIND_CLOUD_LIQUID_WATER -> KIND_CLOUDWATER_MIXING_RATIO
+!!KIND_CLOUD_ICE -> KIND_ICE_MIXING_RATIO
+
 ! <next few lines under version control, do not edit>
 ! $URL$
 ! $Id$
