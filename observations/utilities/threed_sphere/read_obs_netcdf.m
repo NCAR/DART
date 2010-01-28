19afb63cb9ecc912ca0f133575b149790c62eb4b
Tim Hoar <thoar@ucar.edu>
2010-01-28 15:54:26 -0700

    new terms of use - no other changes
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@4237 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 1c447899af56e47ce4904bde65dbf794b5d1e8ca

--- observations/utilities/threed_sphere/read_obs_netcdf.m
+++ observations/utilities/threed_sphere/read_obs_netcdf.m
@@ -32,10 +32,9 @@ function obsstruct = read_obs_netcdf(fname, ObsTypeString, region, CopyString, .
 %              qc: [2343x1 double]
 %          badobs: [1x1 struct]
 
-%% Data Assimilation Research Testbed -- DART
-%  Copyright 2004-2007, Data Assimilation Research Section
-%  University Corporation for Atmospheric Research
-%  Licensed under the GPL -- www.gpl.org/licenses/gpl.html
+%% DART software - Copyright © 2004 - 2010 UCAR. This open source software is
+% provided by UCAR, "as is", without charge, subject to all terms of use at
+% http://www.image.ucar.edu/DAReS/DART/DART_download
 %
 %  <next few lines under version control, do not edit>
 %  $URL$
