4d876059cf38949a830b33c84261fa39866d67f6
Tim Hoar <thoar@ucar.edu>
2010-05-17 15:13:13 -0600

    This is needed by the TIEGCM model.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@4368 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 6978cadb0fabed07c810d8b6d888d5cf9dd55f4e

---
+++ obs_def/obs_def_upper_atm_mod.f90
@@ -0,0 +1,18 @@
+! DART software - Copyright © 2004 - 2010 UCAR. This open source software is
+! provided by UCAR, "as is", without charge, subject to all terms of use at
+! http://www.image.ucar.edu/DAReS/DART/DART_download
+
+! This module supports the observation types from the AIRS instruments.
+! http://winds.jpl.nasa.gov/missions/quikscat/index.cfm
+
+! BEGIN DART PREPROCESS KIND LIST
+! CHAMP_DENSITY,             KIND_DENSITY,           COMMON_CODE
+! GPS_PROFILE,               KIND_ELECTRON_DENSITY,  COMMON_CODE
+! END DART PREPROCESS KIND LIST
+
+! <next few lines under version control, do not edit>
+! $URL$
+! $Id$
+! $Revision$
+! $Date$
+
