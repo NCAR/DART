de714734639fbe1cbd443e036611795084ef4ea7
Nancy Collins <nancy@ucar.edu>
2011-01-27 10:45:50 -0700

    revamped a while ago but never committed; i cleaned it up more with help from tim.  it should now be in shape to use as a good example of how to write an obs_def for observations with additional metadata. also updated the documentation to match.  the original version of this file wrote all the metadata for all 1d integral obs when it read or wrote the first obs of this type; i removed that code and now each obs writes its own metadata embedded in each observation.  if we do want to support global metadata we should make a new section and interface routine that is called only once per file when it's reading the header info.  or as tim suggests, just go to netcdf 4 for the obs_seq file format.  anyway - i could find no obs_seq files committed to the repository that used the old format so there should be no problems.  the tutorial uses this file but it goes through the interactive routine to generate obs_seq files on the fly and doesn't rely on any files in the repository.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@4677 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 2ed93cba244d666aaa62b20ee93f4e3b9fea3c9c

--- obs_def/obs_def_1d_state_mod.f90
+++ obs_def/obs_def_1d_state_mod.f90
@@ -41,28 +41,38 @@ module obs_def_1d_state_mod
 ! $Revision$
 ! $Date$
 
-use        types_mod, only : r8
-use    utilities_mod, only : register_module, error_handler, E_ERR, E_MSG, &
-                             ascii_file_format
+! This code currently does not require a namelist, but to add one search for
+! the string 'NML' and comment in the code lines in the 4 marked sections.
+
+use        types_mod, only : r8, missing_r8
+use    utilities_mod, only : register_module, error_handler,               &
+                             E_ERR, E_MSG, ascii_file_format !,            & 
+                             ! find_namelist_in_file, check_namelist_read, &
+                             ! nmlfileunit, do_nml_file, do_nml_term
+                             !! Routines only needed for namelist support. NML1
 use     location_mod, only : location_type, set_location, get_location 
 use  assim_model_mod, only : interpolate
 use   cov_cutoff_mod, only : comp_cov_factor
 
 implicit none
 
-public :: write_1d_integral, read_1d_integral, interactive_1d_integral, &
-          get_expected_1d_integral
+! These are the required interfaces for an obs_def module.  
+public :: write_1d_integral, read_1d_integral, &
+          interactive_1d_integral, get_expected_1d_integral
 
 ! Storage for the special information required for observations of this type
-integer, parameter                      :: max_1d_integral_obs = 100
-integer                                 :: num_1d_integral_obs = 0
-real(r8)                                :: half_width(max_1d_integral_obs)
-integer, dimension(max_1d_integral_obs) :: num_points, localization_type
+integer               :: num_1d_integral_obs = 0     ! current count of obs
+integer               :: max_1d_integral_obs = 1000  ! allocation size limit
+real(r8), allocatable :: half_width(:)         ! metadata storage
+integer,  allocatable :: num_points(:)         ! ditto
+integer,  allocatable :: localization_type(:)  ! ditto
 
-! For now, read in all info on first read call, write all info on first write call
-logical :: already_read = .false., already_written = .false.
 
-! version controlled file description for error handling, do not edit
+! Set to .true. to get debugging output
+logical :: debug = .false.
+
+
+! Version controlled file description for error handling, do not edit
 character(len=128), parameter :: &
    source   = "$URL$", &
    revision = "$Revision$", &
@@ -70,215 +80,280 @@ character(len=128), parameter :: &
 
 logical, save :: module_initialized = .false.
 
+!! To enable the namelist, comment this in.  NML2
+!namelist /one_d_integral_nml/  debug, max_1d_integral_obs
+
 contains
 
 !----------------------------------------------------------------------
 
-  subroutine initialize_module
-!----------------------------------------------------------------------------
-! subroutine initialize_module
+subroutine initialize_module
 
+! Anything in this routine is executed exactly once.  It could be used
+! to read in a namelist, precompute values needed later, etc. 
+! Currently this routine logs the version information and allocates
+! space for the metadata arrays.
+
+!! Only needed if you've commented in the namelist code.  NML3
+!integer :: iunit, io
+
+! Execute the code here just once
+if (module_initialized) return
+
+! Logs version information to file.
 call register_module(source, revision, revdate)
+
+! To enable a namelist (e.g to set max number of obs, or to turn
+! on the debug messages at run time, or whatever) search for 'NML'
+! and comment in the 4 code sections marked with that string.  NML4
+!
+!! Read the namelist input
+!call find_namelist_in_file("input.nml", "one_d_integral_nml", iunit)
+!read(iunit, nml = one_d_integral_nml, iostat = io)
+!call check_namelist_read(iunit, io, "one_d_integral_nml")
+!
+!! Record the namelist values used for the run 
+!if (do_nml_file()) write(nmlfileunit, nml=one_d_integral_nml)
+!if (do_nml_term()) write(     *     , nml=one_d_integral_nml)
+
+! Allocate space for the metadata
+allocate(half_width(max_1d_integral_obs),  &
+         num_points(max_1d_integral_obs),  &
+         localization_type(max_1d_integral_obs))
+
 module_initialized = .true.
+if(debug) print*, 'module initialized'
 
 end subroutine initialize_module
 
-
-
- subroutine write_1d_integral(key, ifile, fileformat)
 !----------------------------------------------------------------------------
-!subroutine write_1d_integral(key, ifile, fileformat)
 
-integer, intent(in)             :: key, ifile
-character(len=32), intent(in)   :: fileformat
+subroutine write_1d_integral(igrkey, ifile, fform)
+ integer,          intent(in)           :: igrkey, ifile
+ character(len=*), intent(in), optional :: fform
 
-integer :: i
+! Write out the additional data associated with this observation.
+! The obs is identified by the incoming 'key' argument.
+
 logical :: is_ascii
 
 if ( .not. module_initialized ) call initialize_module
 
-is_ascii = ascii_file_format(fileformat)
+! Make sure key value is within valid range -- it will be used as an index below.
+call check_valid_key(igrkey, 'GIVEN', 'write_1d_integral')
 
-! Philosophy, dump ALL information about this special obs_type at once???
-! For now, this means you can only write ONCE (that's all we're doing 3 June 05)
-! Toggle the flag to control this writing
-if(.not. already_written) then
-   already_written = .true.   
-   ! Write out the number of 1d_integral obs descriptions
-   if (is_ascii) then
-      write(ifile, *) num_1d_integral_obs
-   else
-      write(ifile)    num_1d_integral_obs
-   endif
+is_ascii = ascii_file_format(fform)
+if(debug) print*, 'write_1d_integral: ascii format = ', is_ascii
 
-   ! Write out the half_width, num_points, and localization_type for each  
-   do i = 1, num_1d_integral_obs
-      if (is_ascii) then
-         write(ifile, *) half_width(i), num_points(i), localization_type(i) 
-      else
-         write(ifile)    half_width(i), num_points(i), localization_type(i) 
-      endif
-   end do
-endif
+! Write out the half_width, num_points, and localization_type for each  
+! observation embedded in the observation.  The old key is written out
+! for tracking/debug use if needed.
 
-! Write out the obs_def key for this observation
 if (is_ascii) then
-   write(ifile, *) key
+   write(ifile, *) half_width(igrkey), num_points(igrkey), localization_type(igrkey)
+   write(ifile, *) igrkey
 else
-   write(ifile)    key
+   write(ifile)    half_width(igrkey), num_points(igrkey), localization_type(igrkey)
+   write(ifile)    igrkey
 endif
+if(debug) print*, 'writing out metadata for 1D integral obs ', igrkey
+if(debug) print*, 'metadata values are: ', half_width(igrkey), num_points(igrkey), localization_type(igrkey)
 
 end subroutine write_1d_integral
 
-
-
- subroutine read_1d_integral(key, ifile, fileformat)
 !----------------------------------------------------------------------
-!subroutine read_1d_integral(key, ifile, fileformat)
 
-integer, intent(out)            :: key
-integer, intent(in)             :: ifile
-character(len=32), intent(in)   :: fileformat
+subroutine read_1d_integral(igrkey, ifile, fform)
+ integer,          intent(out)          :: igrkey
+ integer,          intent(in)           :: ifile
+ character(len=*), intent(in), optional :: fform
 
-integer :: i
-logical :: is_ascii
+! Read in the additional data associated with this observation.
+! The key value in the file will be read and then discarded, and a new key
+! will be generated based on the next available index in the metadata arrays.
+! Notice that key is intent(out) here, not (in) as in some other routines.
+
+logical            :: is_ascii
+integer            :: ignored_igrkey
 
 if ( .not. module_initialized ) call initialize_module
 
-is_ascii = ascii_file_format(fileformat)
+! Increment the counter so all key values are unique
+num_1d_integral_obs = num_1d_integral_obs + 1
 
-! Philosophy, read ALL information about this special obs_type at once???
-! For now, this means you can only read ONCE (that's all we're doing 3 June 05)
-! Toggle the flag to control this reading
-if(.not. already_read) then
-   already_read = .true.   
-   ! Read the number of 1d_integral obs descriptions
-   if (is_ascii) then
-      read(ifile, *) num_1d_integral_obs
-   else
-      read(ifile)    num_1d_integral_obs
-   endif
-   
-   ! Read the half_width, num_points, and localization_type for each  
-   do i = 1, num_1d_integral_obs
-      if (is_ascii) then
-         read(ifile, *) half_width(i), num_points(i), localization_type(i) 
-      else
-         read(ifile)    half_width(i), num_points(i), localization_type(i) 
-      endif
-   end do
-endif
+! Set the return value for the key, and use it as the index below
+igrkey = num_1d_integral_obs
 
-! Read in the key for this particular observation
+! Make sure key is within valid range
+call check_valid_key(igrkey, 'GENERATED', 'read_1d_integral')
+
+is_ascii = ascii_file_format(fform)
+if(debug) print*, 'read_1d_integral: ascii format = ', is_ascii
+
+! Read in the additional metadata for this observation, and discard the old key.
 if (is_ascii) then
-   read(ifile, *) key
+   read(ifile, *) half_width(igrkey), num_points(igrkey), localization_type(igrkey) 
+   read(ifile, *) ignored_igrkey
 else
-   read(ifile)    key
+   read(ifile)    half_width(igrkey), num_points(igrkey), localization_type(igrkey) 
+   read(ifile)    ignored_igrkey
 endif
+if(debug) print*, 'read in metadata for 1D integral obs ', igrkey
+if(debug) print*, 'metadata values are: ', half_width(igrkey), num_points(igrkey), localization_type(igrkey)
+
+if(debug) print *, 'ignoring old key', ignored_igrkey
+if(debug) print *, 'return key set to ', igrkey
 
 end subroutine read_1d_integral
 
-
-
- subroutine interactive_1d_integral(key)
 !----------------------------------------------------------------------
-!subroutine interactive_1d_integral(key)
-!
-! Initializes the specialized part of a 1d_integral observation
-! Passes back up the key for this one
 
-integer, intent(out) :: key
+subroutine interactive_1d_integral(igrkey)
+ integer, intent(out) :: igrkey
 
-character(len=129) :: msgstring
+! Initializes the specialized part of a 1d_integral observation.
+! A new key will be generated based on the next available index
+! in the metadata arrays.
+
 
 if ( .not. module_initialized ) call initialize_module
 
-! Make sure there's enough space, if not die for now (clean later)
-if(num_1d_integral_obs >= max_1d_integral_obs) then
-   ! PUT IN ERROR HANDLER CALL
-   write(msgstring, *)'Not enough space for a 1d_integral_obs.'
-   call error_handler(E_MSG,'interactive_1d_integral',msgstring,source,revision,revdate)
-   write(msgstring, *)'Can only have max_1d_integral_obs (currently ',max_1d_integral_obs,')'
-   call error_handler(E_ERR,'interactive_1d_integral',msgstring,source,revision,revdate)
-endif
-
-! Increment the index
+! Increment the counter so all key values are unique
 num_1d_integral_obs = num_1d_integral_obs + 1
-key = num_1d_integral_obs
 
-! Otherwise, prompt for input for the three required beasts
+! Set the return value for the key, and use it as the index below
+igrkey = num_1d_integral_obs
+
+! Make sure key is within valid range
+call check_valid_key(igrkey, 'GENERATED', 'interactive_1d_integral')
+
+! Prompt for input for the three required metadata items
 write(*, *) 'Creating an interactive_1d_integral observation'
+
 write(*, *) 'Input half width of integral '
-read(*, *) half_width(num_1d_integral_obs)
-write(*, *) 'Input the number of evaluation points (??? recommended) '
-read(*, *) num_points(num_1d_integral_obs)
+ read(*, *) half_width(igrkey)
+
+write(*, *) 'Input the number of evaluation points (5-20 recommended) '
+ read(*, *) num_points(igrkey)
+
 write(*, *) 'Input localization type: 1=Gaspari-Cohn; 2=Boxcar; 3=Ramped Boxcar'
-read(*, *) localization_type(num_1d_integral_obs)
+ read(*, *) localization_type(igrkey)
+
+if(debug) print *, 'return key set to ', igrkey
 
 end subroutine interactive_1d_integral
 
-
-
- subroutine get_expected_1d_integral(state, location, key, val, istatus)
 !----------------------------------------------------------------------
-!subroutine get_expected_1d_integral(state, location, key, val, istatus)
 
-real(r8), intent(in)            :: state(:)
-type(location_type), intent(in) :: location
-integer, intent(in)             :: key
-real(r8), intent(out)           :: val
-integer, intent(out)            :: istatus
+subroutine get_expected_1d_integral(state, location, igrkey, val, istatus)
+ real(r8),            intent(in)  :: state(:)
+ type(location_type), intent(in)  :: location
+ integer,             intent(in)  :: igrkey
+ real(r8),            intent(out) :: val
+ integer,             intent(out) :: istatus
 
-integer :: i
-real(r8) :: range, loc, bottom, dx, x, sum, dist, weight, weight_sum
+! The forward operator interface for this type of observation.  It is
+! called with a state vector, a location, and a key to identify which
+! observation is being processed.  The return 'val' is the expected
+! observation value, and istatus is the return code.  0 is ok, 
+! > 0 signals an error, and < 0 values are reserved for system use.
+! The call to 'interpolate()' below calls the forward operator in 
+! whatever model this code has been compiled with.
+
 type(location_type) :: location2
+integer             :: i
+real(r8)            :: range, loc, bottom, dx, x, sum, dist, weight, weight_sum
 
 if ( .not. module_initialized ) call initialize_module
 
+! Make sure key is within valid range
+call check_valid_key(igrkey, 'GIVEN', 'get_expected_1d_integral')
+
 ! Figure out the total range of the integrated funtion (1 is max)
-range = 4.0_r8 * half_width(key)
+range = 4.0_r8 * half_width(igrkey)
 if(range > 1.0_r8) range = 1.0_r8
-!write(*, *) 'range is ', range
+if(debug) print*, 'range is ', range
 
 ! Get the location value
 loc = get_location(location)
-!write(*, *) 'loc base is ', loc
+if(debug) print*, 'loc base is ', loc
 
 ! Compute the bottom and top of the range
 bottom = loc - range / 2.0_r8
 if(bottom < 0.0_r8) bottom = bottom + 1.0_r8
-!write(*, *) 'bottom is ', bottom
+if(debug) print*, 'bottom is ', bottom
 
 ! Next figure out where to put all the points
-dx = range / (num_points(key) - 1)
-!write(*, *) 'dx is ', dx
+dx = range / (num_points(igrkey) - 1)
+if(debug) print*, 'dx is ', dx
 
 ! Loop to compute the value at each point, then multiply by localization
 ! to get weighted integral
 sum = 0.0_r8
 weight_sum = 0.0_r8
-do i = 1, num_points(key)
+do i = 1, num_points(igrkey)
    x = bottom + (i - 1) * dx
    if(x > 1.0_r8) x = x - 1.0_r8
-!write(*, *) 'location for int ', i, 'is ', x
+if(debug) print*, 'location for int ', i, 'is ', x
    location2 = set_location(x)
    call interpolate(state, location2, 1, val, istatus)
+if(debug) print*, 'model forward operator for ', i, ' returns ', val
+   if (istatus /= 0) then
+if(debug) print*, 'forward operator returned error, returning'
+      val = missing_r8
+      return 
+   endif
    dist = abs(loc - x)
    if(dist > 0.5_r8) dist = 1.0_r8 - dist
-!write(*, *) 'dist ', i, dist
-   weight = comp_cov_factor(dist, half_width(key), &
-      localization_override = localization_type(key))
-!write(*, *) 'weight ', i, weight
+if(debug) print*, 'dist ', i, dist
+   weight = comp_cov_factor(dist, half_width(igrkey), &
+      localization_override = localization_type(igrkey))
+if(debug) print*, 'weight ', i, weight
    sum = sum + weight * val
    weight_sum = weight_sum + weight
 end do
 
 val = sum / weight_sum
 
-!write(*, *) 'get_expected_1d_integral key is ', key
-!write(*, *) half_width(key), num_points(key), localization_type(key)
+if(debug) print*, 'get_expected_1d_integral key is ', igrkey
+if(debug) print*, 'metadata values are: ', half_width(igrkey), num_points(igrkey), localization_type(igrkey)
+if(debug) print*, 'return value for forward operator is ', val
+if(debug) print*, 'return status (0 good; >0 error; <0 reserved for system use) is ', istatus
 
 end subroutine get_expected_1d_integral
+
+!----------------------------------------------------------------------
+
+subroutine check_valid_key(igrkey, what, fromwhere)
+ integer, intent(in)          :: igrkey
+ character(len=*), intent(in) :: what, fromwhere
+
+! Internal subroutine that verifies that we haven't incremented the key value
+! past the size of the allocated space, or that a routine hasn't been called
+! with a out-of-range key (which would indicate an internal error of some kind).
+! If an error is found, an fatal message is printed and this routine doesn't return.
+! The 'what' argument is either 'GIVEN' for a key value that's passed in from
+! another routine; or 'GENERATED' for one we have just made and are planning to
+! return to the caller.  The 'fromwhere' argument is the name of the calling 
+! subroutine so the error message can report where it was called from.
+
+character(len=128) :: msgstring
+
+if (igrkey <= 0 .or. igrkey > max_1d_integral_obs) then
+   if (what == 'GENERATED' .and. igrkey > max_1d_integral_obs) then
+      ! generating a new key and ran out of space
+      write(msgstring, *)'Out of space, max_1d_integral_obs limit ',max_1d_integral_obs
+      call error_handler(E_ERR,trim(fromwhere),msgstring,source,revision,revdate, &
+                         text2='Increase value of max_1d_integral_obs in obs_def_1d_state_mod')
+   else
+      ! called with a bad key or a negative key generated somehow. "shouldn't happen".
+      write(msgstring, *)'Key is ',igrkey,' must be between 1 and ',max_1d_integral_obs
+      call error_handler(E_ERR,trim(fromwhere),msgstring,source,revision,revdate, &
+                         text2='Internal error: Invalid key value in RAW_STATE_1D_INTEGRAL obs')
+   endif
+endif
+
+end subroutine
 
 !----------------------------------------------------------------------
 
