0eec5061b9c664b8ea2e304e1edc16a41796ba85
Tomoko Matsuo <Tomoko>
2011-02-03 12:00:16 -0700

    read_TIEGCM_namelist is called to fill in var%vars_1d with TIEGCM forcing parameters from tiegcm.nml
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@4690 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: ace280241a86f0b0901f9eeaf475b4bd6d80b697

--- models/tiegcm/model_to_dart.f90
+++ models/tiegcm/model_to_dart.f90
@@ -23,7 +23,7 @@ use        types_mod, only : r8
 use    utilities_mod, only : get_unit, initialize_utilities, timestamp
 use        model_mod, only : model_type, static_init_model, get_model_size, &
                              init_model_instance, read_TIEGCM_restart, &
-                             prog_var_to_vector 
+                             read_TIEGCM_namelist, prog_var_to_vector 
 use  assim_model_mod, only : open_restart_write, awrite_state_restart, close_restart
 use time_manager_mod, only : time_type
 
@@ -36,8 +36,9 @@ character(len=128), parameter :: &
    revdate  = "$Date$"
 
 character (len = 128) ::  &
-   file_name = 'tiegcm_restart_p.nc', & 
-   file_out  = 'temp_ud'
+   file_namelist = 'tiegcm.nml', &
+   file_name     = 'tiegcm_restart_p.nc', & 
+   file_out      = 'temp_ud'
 
 ! Temporary allocatable storage to read in a native format for TIEGCM state
 type(model_type)       :: var
@@ -61,6 +62,9 @@ call init_model_instance(var)
 ! Read the TIEGCM state variables into var and set the model_time
 ! to reflect the valid time of the TIEGCM state.
 call read_TIEGCM_restart(file_name, var, model_time)
+
+! Read the TIEGCM input variables into var
+call read_TIEGCM_namelist(file_namelist, var)
 
 ! transform fields into state vector for DART
 call prog_var_to_vector(var, x_state)
