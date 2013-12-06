82fb9f4f3efc07217a4adb27b8e8186209312262
Tim Hoar <thoar@ucar.edu>
2013-12-06 21:47:26 +0000

    Adding the 4 observation converters that Alexey Morozov used with GITM.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@6655 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 756f95695b9dee895dd878f035d689a1a10433be

---
+++ observations/CHAMP/work/convert.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# DART software - Copyright 2004 - 2013 UCAR. This open source software is
+# provided by UCAR, "as is", without charge, subject to all terms of use at
+# http://www.image.ucar.edu/DAReS/DART/DART_download
+#
+# DART $Id$
+#
+# runs t2o multiple times to convert multiple CHAMP files and append the 
+# resulting files to obs_seq.out (or the obs_out_file specified in 
+# input.nml&text_to_obs_nml )
+
+# grep gets the line, awk extracts the name
+name=`grep "obs_out_file" < input.nml | awk -F\" '{for(i=2;i<=NF;i+=2)print $i}'`
+echo $name
+# remove the old file before starting the append-process
+rm $name
+
+# what is the first day in 2002 (year is hardcoded for now)
+d1=335
+# how many days do you want to append together?
+# (note, if you need more than 2, you need to download them from
+# http://sisko.colorado.edu/sutton/data/ver2.2/champ/density/2002/ascii/ )
+nd=4
+
+for (( i = 1 ; i <= $nd; i++ ))
+do
+    echo $i
+    sed -i '.tmp' 's/text_input_file.*/text_input_file = "Density_3deg_02_'$[$d1+$i-1]'.ascii"/' input.nml
+    ./text_to_obs > temp
+done
+
+#shows how many observations are there total
+tail temp
+
+# <next few lines under version control, do not edit>
+# $URL$
+# $Revision$
+# $Date$
+
