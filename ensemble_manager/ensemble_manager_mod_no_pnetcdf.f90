0ffe6559c8cee041924a2ab0af31f85a750e0362
kershaw-dart <Helen>
2014-04-30 22:01:19 +0000

    Duplicate versions that don't use pnetcdf.  You cannot have parallel_state_diag = .true. in the input.nml.  Just temporary.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma@6965 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 304aa8984e59b903ffe32bd5cf5f485f6eb2bd7a

--- ensemble_manager/ensemble_manager_mod_no_pnetcdf.f90
+++ ensemble_manager/ensemble_manager_mod_no_pnetcdf.f90
@@ -254,10 +254,6 @@ end subroutine init_ensemble_manager
 subroutine read_ensemble_restart(ens_handle, start_copy, end_copy, &
    start_from_restart, file_name, init_time, force_single_file)
 
-use pnetcdf_utilities_mod, only : pnet_check
-use mpi
-use pnetcdf
-
 type(ensemble_type),  intent(inout)           :: ens_handle
 integer,              intent(in)              :: start_copy, end_copy
 logical,              intent(in)              :: start_from_restart
@@ -282,146 +278,10 @@ logical                             :: single_file_override
 type(random_seq_type)               :: random_seq
 integer                             :: my_num_vars
 
-! Parallel netcdf variables
-integer                       :: ret !< return code for pnetcdf calls
-integer                       :: ncfile !< ncfile
-integer                       :: stateId !< Id for state_vector
-integer                       :: timeId !< Id for time
-integer                       :: stateDimId !< Id of the dimension of state
-integer                       :: timeDimId !< Id of the dimension of time
-integer                       :: varsDimId !< for giant read
-integer                       :: copiesDimId !< for giant read
-integer(KIND=MPI_OFFSET_KIND) :: start(1) ! state is one dimensional
-integer(KIND=MPI_OFFSET_KIND) :: count(1)
-integer(KIND=MPI_OFFSET_KIND) :: stride(1)
-integer(KIND=MPI_OFFSET_KIND) :: state_length, time_length
-integer(KIND=MPI_OFFSET_KIND) :: num_blocks
-integer(KIND=MPI_OFFSET_KIND) :: num_copies
-
 real(r8), allocatable :: model_state(:,:)
 
 ! timing variables
 double precision :: time_at_start
-
-
-num_blocks = ens_handle%my_num_vars ! Just me
-
-if (no_complete_state) then
-
-   time_at_start = MPI_WTIME()
-
-   if (giant_restart) then
-
-      this_file_name = 'filter_ic_giant.nc'
-
-      ! Open netcdf file
-      ret = nfmpi_open(mpi_comm_world, this_file_name, NF_NOWRITE, mpi_info_null, ncfile)
-      call pnet_check(ret, 'read_ensemble_restart', 'open file')
-
-      ! get id for vars_dim - the length of the state
-      ret = nfmpi_inq_dimid(ncfile, 'vars', varsDimId)
-      call pnet_check(ret, 'read_ensemble_restart', 'cannot get vars dim')
-
-      ! get num vars
-      ret = nfmpi_inq_dimlen(ncfile, varsDimId, state_length)
-      call pnet_check(ret, 'read_ensemble_restart', 'get state_length')
-
-      ! get id for copies_dim - the number of copies
-      ret = nfmpi_inq_dimid(ncfile, 'copies', copiesDimId)
-      call pnet_check(ret, 'read_ensemble_restart', 'cannot get state dim')
-
-      ! get num_copies
-      ret = nfmpi_inq_dimlen(ncfile, copiesDimId, num_copies)
-      call pnet_check(ret, 'read_ensemble_restart', 'get num_copies')
-
-      ! get id for time_dim - the length of the time (should be 2)
-      ret = nfmpi_inq_dimid(ncfile, 'time', timeDimId)
-      call pnet_check(ret, 'read_ensemble_restart', 'cannot get time dim')
-
-      ! Get status of varible 
-      ret = nfmpi_inq_varid(ncfile, 'state_vector', stateId)
-      call pnet_check(ret, 'read_ensemble_restart', 'cannot get state_vector id')
-
-      ! Get status of varible 
-      ret = nfmpi_inq_varid(ncfile, 'time', timeId)
-      call pnet_check(ret, 'read_ensemble_restart', 'cannot get time id')
-
-      if (transpose_giant) then
-
-         ! need to read transposed state
-         allocate(model_state(num_blocks, num_copies))
-
-         call aread_state_restart(state_length, stateId, timeId, num_blocks, num_copies, ens_handle%time, model_state, transpose_giant, ncfile)
-
-         ens_handle%copies(1:num_copies,:) = transpose(model_state)
-
-         deallocate(model_state)
-
-      else
-
-         call aread_state_restart(state_length, stateId, timeId, num_blocks, num_copies, ens_handle%time, ens_handle%copies(1:num_copies,:),transpose_giant, ncfile)
-      endif
-
-      ret = nfmpi_close(ncfile)
-      call pnet_check(ret, 'read_ensemble_restart', 'closing file')
-
-      if(present(init_time)) ens_handle%time(1:num_copies) = init_time
-
-      if (my_task_id() == 0) print*, 'giant read time ', MPI_WTIME() - time_at_start, 'copies = ', end_copy - start_copy + 1
-   else
-
-      ! Loop to read in all ensemble members
-      PNETCDF_RESTARTS: do i = start_copy, end_copy
-
-         ! Get global index for my ith ensemble
-         write(extension, '(i4.4)') i
-         this_file_name = trim(file_name) // '.' // extension // '.nc'
-         ! Open netcdf file
-         ret = nfmpi_open(mpi_comm_world, this_file_name, NF_NOWRITE, mpi_info_null, ncfile)
-         call pnet_check(ret, 'read_ensemble_restart', 'open file')
-
-         ! get id for state_dim - the length of the state
-         ret = nfmpi_inq_dimid(ncfile, 'state', stateDimId)
-         call pnet_check(ret, 'read_ensemble_restart', 'get state_dim')
-
-         ! get id for time_dim - the length of the time (should be 2)
-         ret = nfmpi_inq_dimid(ncfile, 'time', timeDimId)
-         call pnet_check(ret, 'read_ensemble_restart', 'get time_dim')
-
-         ! get state length ( a dimension called state )
-         ret = nfmpi_inq_dimlen(ncfile, stateDimId, state_length)
-         call pnet_check(ret, 'read_ensemble_restart', 'get state length')
-
-         ! get time length ( a dimension called time ) should be 2
-         ret = nfmpi_inq_dimlen(ncfile, timeDimId, time_length)
-         call pnet_check(ret, 'read_ensemble_restart', 'get time length')
-
-         ! I think the model_mod gets the model size (state_length) separately
-
-         ! Get status of varible - Do you need to do this to get the variable id?
-         ret = nfmpi_inq_varid(ncfile, 'state_vector', stateId)
-         call pnet_check(ret, 'read_ensemble_restart', 'get state id')
-
-         ! Get status of varible - Do you need to do this to get the variable id?
-         ret = nfmpi_inq_varid(ncfile, 'time', timeId)
-         call pnet_check(ret, 'read_ensemble_restart', 'get time id')
-
-         ! Read the file directly into storage
-         my_num_vars = ens_handle%my_num_vars
-         call aread_state_restart(stateId, timeId, my_num_vars, ens_handle%time(i), ens_handle%copies(i, :), ncfile)
-         if(present(init_time)) ens_handle%time(i) = init_time
-
-         ! Close the restart file
-         ret = nfmpi_close(ncfile)
-         call pnet_check(ret, 'read_ensemble_restart', 'close restart file')
-
-      end do PNETCDF_RESTARTS
-
-   if (my_task_id() == 0) print*, 'Read time : ', MPI_WTIME() - time_at_start, 'average :', (MPI_WTIME() - time_at_start) / (end_copy - start_copy + 1)
-
-   endif
-
-else ! use binary restart files
 
    ! Does not make sense to have start_from_restart and single_restart_file_in BOTH false
    if(.not. start_from_restart .and. .not. single_restart_file_in) then
@@ -519,7 +379,6 @@ else ! use binary restart files
       end do PERTURB_MY_RESTARTS
    endif
 
-endif
 
 end subroutine read_ensemble_restart
 
@@ -528,10 +387,8 @@ end subroutine read_ensemble_restart
 subroutine write_ensemble_restart(ens_handle, file_name, start_copy, end_copy, &
    force_single_file)
 
-use pnetcdf_utilities_mod, only : pnet_check
 use mpi_utilities_mod,     only : datasize
 use mpi
-use pnetcdf
 
 type(ensemble_type),  intent(inout) :: ens_handle
 character(len = *),   intent(in)    :: file_name
@@ -547,161 +404,9 @@ character(len = LEN(file_name) + 10) :: this_file_name
 character(len = 4)                  :: extension
 logical                             :: single_file_forced
 
-! pnetcdf variables
-integer                       :: ret !< return code for pnetcdf calls
-integer                       :: ncfile !< ncfile
-integer                       :: stateId !< Id for state_vector
-integer                       :: timeId !< Id for time
-integer                       :: stateDimId !< Id of the dimension of state
-integer                       :: timeDimId !< Id of the dimension of time
-integer                       :: varsDimId
-integer                       :: copiesDimId
-integer                       :: stateDim(1) !< Array needed to hold state dimension
-integer                       :: timeDim(1) !< Array needed to hold state dimension
-integer                       :: varsCopiesDim(2) !< Array needed to hold state dimension
-integer                       :: num_dims
-integer(KIND=MPI_OFFSET_KIND) :: time_length
-integer(KIND=MPI_OFFSET_KIND) :: state_length
-integer(KIND=MPI_OFFSET_KIND) :: num_blocks
-integer(KIND=MPI_OFFSET_KIND) :: num_copies
-
-real(r8), allocatable :: model_state(:,:)
 
 ! timing variables
 double precision :: start_at_time
-
-time_length = 2
-state_length = ens_handle%num_vars ! Whole state
-num_blocks = ens_handle%my_num_vars ! Just me
-num_copies = ens_handle%num_copies -6
-
-if (no_complete_state) then
-
-   start_at_time = MPI_WTIME()
-
-   if (giant_restart) then
-
-      this_file_name = 'filter_restart_giant.nc'
-
-      ! create netcdf file
-      ret = nfmpi_create(mpi_comm_world, this_file_name, NF_CLOBBER, mpi_info_null, ncfile)
-      call pnet_check(ret, 'write_ensemble_restart', 'cannot create netcdf file')
-
-      ! load up state and time in netcdf metadata
-      ! Define the dimensions
-      ret = nfmpi_def_dim(ncfile, 'time', time_length, timeDimId) !time
-      call pnet_check(ret, 'write_ensemble_restart', 'defining time dim')
-
-      ret = nfmpi_def_dim(ncfile, 'vars', state_length, varsDimId) !vars
-      call pnet_check(ret, 'write_ensemble_restart', 'defining state dim')
-
-      ret = nfmpi_def_dim(ncfile, 'copies', num_copies, copiesDimId) !copies
-      call pnet_check(ret, 'write_ensemble_restart', 'defining state dim')
-
-      ! The dimids array is used to pass the IDs of the dimensions of the variables
-      if ( transpose_giant) then
-         varsCopiesDim = (/varsDimId, copiesDimId/)
-      else
-         varsCopiesDim = (/copiesDimId, varsDimId/) !- these are backwards?
-      endif
-      timeDim = (/timeDimId/)
-
-      ! Define the variables - state needs to go last so it can be unlimited size
-      num_dims = 1
-      ret = nfmpi_def_var(ncfile, "time", NF_INT, num_dims, timeDim, timeId) ! time
-      call pnet_check(ret, 'write_ensemble_restart', 'defining time var')
-
-      num_dims = 2
-      if (datasize == MPI_REAL4) then ! single precision state
-
-         ret = nfmpi_def_var(ncfile, "state_vector", NF_FLOAT, num_dims, varsCopiesDim, stateId) ! state
-         call pnet_check(ret, 'write_ensemble_restart', 'defining state var')
-
-      else ! double precision
-
-         ret = nfmpi_def_var(ncfile, "state_vector", NF_DOUBLE, num_dims, varsCopiesDim, stateId) ! state
-         call pnet_check(ret, 'write_ensemble_restart', 'defining state var')
-
-      endif
-
-      ret = nfmpi_enddef(ncfile) ! metadata IO occurs in this
-      call pnet_check(ret, 'write_ensemble_restart', 'metadata IO')
-
-      if (transpose_giant) then
-
-         allocate(model_state(num_blocks, num_copies))
-         model_state = transpose(ens_handle%copies(1:num_copies,:))
-
-         call awrite_state_restart(timeId, stateId, num_blocks, num_copies, ens_handle%time(1), model_state, transpose_giant, ncfile)
-
-         deallocate(model_state)
-
-      else
-
-         call awrite_state_restart(timeId, stateId, num_blocks, num_copies, ens_handle%time(1), ens_handle%copies(1:num_copies,:), transpose_giant, ncfile)
-
-      endif
-
-      ret = nfmpi_close(ncfile)
-      call pnet_check(ret, 'write_ensemble_restart', 'closing file')
-
-      if (my_task_id() == 0) print*, 'giant write time ', MPI_WTIME() - start_at_time, 'copies = ', end_copy - start_copy + 1
-
-   else
-
-      PNETCDF_RESTARTS: do i = start_copy, end_copy
-         write(extension, '(i4.4)') i
-         this_file_name = trim(file_name) // '.' // extension // '.nc'
-
-         ! create netcdf file
-         ret = nfmpi_create(mpi_comm_world, this_file_name, NF_CLOBBER, mpi_info_null, ncfile)
-         call pnet_check(ret, 'write_ensemble_restart', 'cannot create netcdf file')
-
-         ! load up state and time in netcdf metadata
-         ! Define the dimensions
-         ret = nfmpi_def_dim(ncfile, 'time', time_length, timeDimId) !time
-         call pnet_check(ret, 'write_ensemble_restart', 'defining time dim')
-
-         ret = nfmpi_def_dim(ncfile, 'state', state_length, stateDimId) !state
-         call pnet_check(ret, 'write_ensemble_restart', 'defining state dim')
-
-         ! The dimids array is used to pass the IDs of the dimensions of the variables
-         stateDim = (/stateDimId/)
-         timeDim = (/timeDimId/)
-
-         ! Define the variables - state needs to go last because it is huge
-
-         num_dims = 1
-         ret = nfmpi_def_var(ncfile, "time", NF_INT, num_dims, timeDim, timeId) ! time
-         call pnet_check(ret, 'write_ensemble_restart', 'defining time var')
-
-         if (datasize == MPI_REAL4) then ! single precision state
-
-            ret = nfmpi_def_var(ncfile, "state_vector", NF_FLOAT, num_dims, stateDim, stateId) ! state
-            call pnet_check(ret, 'write_ensemble_restart', 'defining state var')
-
-         else ! double precision
-
-            ret = nfmpi_def_var(ncfile, "state_vector", NF_DOUBLE, num_dims, stateDim, stateId) ! state
-            call pnet_check(ret, 'write_ensemble_restart', 'defining state var')
-
-         endif
-
-         ret = nfmpi_enddef(ncfile) ! metadata IO occurs in this
-         call pnet_check(ret, 'write_ensemble_restart', 'metadata IO')
-
-         call awrite_state_restart(timeId, stateId, num_blocks, ens_handle%time(i), ens_handle%copies(i, :), state_length,ncfile)
-
-         ret = nfmpi_close(ncfile)
-         call pnet_check(ret, 'write_ensemble_restart', 'closing file')
-
-      end do PNETCDF_RESTARTS
-
-   if (my_task_id() == 0) print*, 'write time: ', MPI_WTIME() - start_at_time, 'average :', (MPI_WTIME() - start_at_time) / (end_copy - start_copy + 1)
-
-   endif
-
-else
 
 if (present(force_single_file) ) then
    single_file_forced = force_single_file
@@ -770,8 +475,6 @@ endif
          endif
       end do
    endif
-
-endif
 
 end subroutine write_ensemble_restart
 
--- smoother/smoother_mod_no_pnetcdf.f90
+++ smoother/smoother_mod_no_pnetcdf.f90
@@ -553,90 +553,13 @@ end subroutine filter_state_space_diagnostics_complete
 !> creating the file with netcdf.
 subroutine filter_state_space_diagnostics_parallel(state_ens_handle, start_copy, end_copy, diag_filename)
 
-use pnetcdf_utilities_mod, only : pnet_check
-use mpi_utilities_mod,     only : my_task_id, task_count, datasize
-
-use mpi
-use pnetcdf
-
 type(ensemble_type), intent(inout) :: state_ens_handle !< why does this want to be inout?
 integer,             intent(in)    :: start_copy !< copy to start from. This is going to be annoying for the posterior output.
 integer,             intent(in)    :: end_copy !< copy to output up to
 character(len=*),    intent(in)    :: diag_filename
 
-! pnetcdf variables
-integer                       :: ret !< pnetcdf return code
-integer                       :: ncfile !< pnetcdf file identifier
-integer                       :: ndims, dimIds(2), stateId
-integer(KIND=MPI_OFFSET_KIND) :: num_copies, num_vars, my_num_vars
-integer                       :: copies_dim, vars_dim
-integer(KIND=MPI_OFFSET_KIND) :: start(2), count(2), stride(2) !< for state copies
-integer(KIND=MPI_OFFSET_KIND) :: bufcount !< my_num_vars * output num_copies
-! timing variables
-double precision :: start_at_time
+call error_handler(E_ERR, 'filter_state_space_diagnostics_parallel', 'no pnetcdf')
 
-start_at_time = MPI_WTIME()
-
-! open diagnostic file
-ret = nfmpi_open(mpi_comm_world, diag_filename, NF_WRITE, mpi_info_null, ncfile)
-call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'opening file')
-
-! define variables
-ret = nfmpi_redef(ncfile)
-call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'going into define mode')
-
-! define state(copies, vars)
-ndims = 2 ! two dimensional state
-num_copies = end_copy - start_copy + 1!> @todo only output what you need to?
-num_vars = state_ens_handle%num_vars
-my_num_vars = state_ens_handle%my_num_vars
-
-ret = nfmpi_def_dim(ncfile, 'vars', num_vars, vars_dim) ! length of state vector
-call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'defining vars')
-
-ret = nfmpi_def_dim(ncfile, 'copies', num_copies, copies_dim) ! number of copies
-call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'defining copies')
-
-dimIds = (/copies_dim, vars_dim/)
-
-if (datasize == MPI_REAL4) then ! single precsion state
-
-   ret = nfmpi_def_var(ncfile, 'state', NF_FLOAT, ndims, dimIds, stateId)
-   call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'defining variables')
-
-else ! double precision
-
-   ret = nfmpi_def_var(ncfile, 'state', NF_DOUBLE, ndims, dimIds, stateId)
-   call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'defining variables')
-
-endif
-
-ret = nfmpi_enddef(ncfile) ! metadata IO occurs in this
-call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'metadata IO')
-
-! write state copies to diagnostic file
-start = (/1 , my_task_id() + 1/)
-count = (/num_copies, my_num_vars/) ! my_num_vars
-stride = (/1, task_count()/)
-bufcount = state_ens_handle%my_num_vars * num_copies
-
-if (datasize == MPI_REAL4) then ! single precsion state
-
-   ret = nfmpi_put_vars_all(ncfile, stateId, start, count, stride, state_ens_handle%copies(start_copy:end_copy, :), bufcount, MPI_REAL4)
-   call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'writing copies')
-
-else ! double precision
-
-   ret = nfmpi_put_vars_all(ncfile, stateId, start, count, stride, state_ens_handle%copies(start_copy:end_copy, :), bufcount, MPI_REAl8)
-   call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'writing copies')
-
-endif
-
-! close netcdf file
-ret = nfmpi_close(ncfile)
-call pnet_check(ret, 'filter_state_space_diagnostics_parallel', 'closing file')
-
-if (my_task_id() == 0) print*, 'parallel diagnostic time :', MPI_WTIME() - start_at_time
 
 end subroutine filter_state_space_diagnostics_parallel
 
