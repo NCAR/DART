29dcdc20f147143482236fcf559d5aaf6e07a801
kershaw-dart <Helen>
2014-04-30 20:48:35 +0000

    Version of the window module that does not use cray pointers.  I'm not sure whether there will be a performance penalty by not using MPI_ALLOC_MEM to create the memory for the window.  Also, I am not sure if it is best to attach the window to a contiguous piece of memory.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma@6960 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 379c25a9d79e7c01b123b92097b70ee052ee96a6

--- distributed/fwd_op_win_mod_no_cray.f90
+++ distributed/fwd_op_win_mod_no_cray.f90
@@ -20,12 +20,6 @@ integer(KIND=MPI_ADDRESS_KIND) window_size_state
 integer(KIND=MPI_ADDRESS_KIND) window_size_mean
 integer mean_row !< The row in copies that has the mean copy
 
-real(r8) :: duplicate_state(*)  !< duplicate array for cray pointer fwd
-pointer(a, duplicate_state)
-
-real(r8) :: duplicate_mean(*)  !< duplicate array for cray pointer vert convert
-pointer(b, duplicate_mean)
-
 interface get_state
    module procedure get_fwd
    module procedure get_mean
@@ -48,27 +42,11 @@ my_num_vars = state_ens_handle%my_num_vars
 !> @todo number of copies in the window
 copies_in_window = state_ens_handle%num_copies -5
 
-! allocate some RDMA accessible memory
-! using MPI_ALLOC_MEM because the MPI standard allows vendors to require MPI_ALLOC_MEM for remote memory access
 call mpi_type_size(datasize, bytesize, ierr)
 window_size_state = my_num_vars*copies_in_window*bytesize
-a = malloc(my_num_vars*copies_in_window)
-call MPI_ALLOC_MEM(window_size_state, MPI_INFO_NULL, a, ierr)
-
-count = 1
-! create a duplicate copies array for remote memory access
-! Doing this because you cannot use a cray pointer with an allocatable array
-! Can't do array assignment with a cray pointer, so you need to loop
-!> @todo should you only fill the window with actual ensemble members?
-do ii = 1, my_num_vars
-   do jj = 1, copies_in_window
-      duplicate_state(count) = state_ens_handle%copies(jj,ii)
-      count = count + 1
-   enddo
-enddo
 
 ! Expose local memory to RMA operation by other processes in a communicator.
-call mpi_win_create(duplicate_state, window_size_state, bytesize, MPI_INFO_NULL, mpi_comm_world, state_win, ierr)
+call mpi_win_create(state_ens_handle%copies(1:copies_in_window, :), window_size_state, bytesize, MPI_INFO_NULL, mpi_comm_world, state_win, ierr)
 
 end subroutine create_state_window
 
@@ -90,15 +68,9 @@ mean_row = state_ens_handle%num_copies -5!> @todo How to do this nicely
 ! Have a look at MPI-3, I think this removes cray pointers.
 call mpi_type_size(datasize, bytesize, ierr)
 window_size_mean = my_num_vars*bytesize
-b = malloc(my_num_vars)
-call MPI_ALLOC_MEM(window_size_mean, MPI_INFO_NULL, b, ierr)
-
-do ii = 1, my_num_vars
-   duplicate_mean(ii) = state_ens_handle%copies(mean_row, ii)
-enddo
 
 ! Expose local memory to RMA operation by other processes in a communicator.
-call mpi_win_create(duplicate_mean, window_size_mean, bytesize, MPI_INFO_NULL, mpi_comm_world, mean_win, ierr)
+call mpi_win_create(state_ens_handle%copies(mean_row, :), window_size_mean, bytesize, MPI_INFO_NULL, mpi_comm_world, mean_win, ierr)
 
 end subroutine create_mean_window
 
@@ -109,7 +81,6 @@ subroutine free_state_window
 integer :: ierr
 
 call mpi_win_free(state_win, ierr)
-call MPI_FREE_MEM(duplicate_state, ierr) ! not a
 
 end subroutine free_state_window
 
@@ -119,7 +90,6 @@ subroutine free_mean_window
 integer :: ierr
 
 call mpi_win_free(mean_win, ierr)
-call MPI_FREE_MEM(duplicate_mean, ierr) ! not a
 
 end subroutine free_mean_window
 
