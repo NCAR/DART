e6da7deb5cf3dfd9f74a8dad237f2f66f0befbcb
kershaw-dart <Helen>
2014-06-09 16:20:39 +0000

    Multiple domains
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma@7011 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: e4406f75de0e08541aacae308bd13462883faa7c

--- io/state_vector_io_mod.f90
+++ io/state_vector_io_mod.f90
@@ -44,7 +44,8 @@ implicit none
 
 private
 
-public :: netcdf_filename, get_state_variable_info, &
+public :: intialize_arrays_for_read, &
+          netcdf_filename, get_state_variable_info, &
           read_transpose, &
           limit_mem, limit_procs, &
           transpose_write, &
@@ -55,62 +56,71 @@ integer :: ncfile !< netcdf input file identifier
 integer :: ncfile_out !< netcdf output file handle
 character(len=256) :: netcdf_filename !< needs to be different for each task
 character(len=256) :: netcdf_filename_out !< needs to be different for each task
-integer, parameter :: MAXDIMS = 10 ! FIXME netcdf-max_dims?
+integer, parameter :: MAXDIMS = NF90_MAX_VAR_DIMS ! FIXME netcdf-max_dims?
 integer :: limit_mem !< This is the number of elements (not bytes) so you don't have times the number by 4 or 8
 integer :: limit_procs
 
-integer,            allocatable :: variable_ids(:)
-integer,            allocatable :: variable_sizes(:)
-integer,            allocatable :: variable_xtypes(:) !< variable type: NF90_FLOAT, NF90_DOUBLE, etc.
-character(len=256), allocatable :: variable_names_local(:) !< local copy of variable names, not sure whether this is the way to go.
-integer,   allocatable :: dimensions_and_lengths(:,:) !< number of dimensions and length of each dimension
+integer,            allocatable :: variable_ids(:, :)
+integer,            allocatable :: variable_sizes(:, :)
+integer,   allocatable :: dimensions_and_lengths(:,:, :) !< number of dimensions and length of each dimension
 integer :: num_state_variables
 
 ! record dimensions for output netcdf file
-!integer              :: dimIds(MAXDIMS) !< dimension ids
-!integer              :: length(MAXDIMS) !< dimension length
-integer,            allocatable :: dimIds(:, :) !< dimension ids
-integer,            allocatable :: length(:, :) !< dimension length
-character(len=256), allocatable :: dim_names(:, :)
+integer,            allocatable :: dimIds(:, :, :) !< dimension ids
+integer,            allocatable :: length(:, :, :) !< dimension length
+character(len=256), allocatable :: dim_names(:, :, :)
 
 contains
 
 !-------------------------------------------------
+!> Initialize arrays.  Need to know the number of domains
+subroutine intialize_arrays_for_read(n, num_domains)
+
+integer, intent(in) :: n !< number of state variables
+integer, intent(in) :: num_domains !< number of domains (and therfore netcdf files) to read
+
+if(allocated(variable_ids))   call error_handler(E_ERR, 'intialize_arrays_for_read', 'already called this routine')
+if(allocated(variable_sizes)) call error_handler(E_ERR, 'intialize_arrays_for_read', 'already called this routine')
+
+num_state_variables = n
+
+allocate(variable_ids(n, num_domains), variable_sizes(n, num_domains))
+allocate(dimIds(n, MAXDIMS, num_domains), length(n, MAXDIMS, num_domains), dim_names(n, MAXDIMS, num_domains))
+allocate(dimensions_and_lengths(n, MAXDIMS +1, num_domains))
+
+dimensions_and_lengths = -1  ! initialize to a nonsense value
+
+end subroutine intialize_arrays_for_read
+
+!-------------------------------------------------
 !> Need list of variables in the state
 !> Each task grabs the variable dimensions from the netcdf file
-subroutine get_state_variable_info(n, variable_names, model_size)
+subroutine get_state_variable_info(n, variable_names, domain, domain_size)
 
 integer,            intent(in)  :: n !< number of state variables
 character(len=256), intent(in)  :: variable_names(n)
-integer,            intent(out) :: model_size
+integer,            intent(in)  :: domain !< which domain info you are grabbing
+integer,            intent(out) :: domain_size
 
 integer :: i
-
-if(allocated(variable_ids))   call error_handler(E_ERR, 'get_state_variable_info', 'already called this routine')
-if(allocated(variable_sizes)) call error_handler(E_ERR, 'get_state_variable_info', 'already called this routine')
-allocate(variable_ids(n), variable_sizes(n), variable_xtypes(n), variable_names_local(n))
-allocate(dimIds(n, MAXDIMS), length(n, MAXDIMS), dim_names(n, MAXDIMS))
-
-num_state_variables = n
-variable_names_local = variable_names
 
 ! open netcdf file - all restart files have the same info?
 ret = nf90_open(netcdf_filename, NF90_NOWRITE, ncfile)
 call nc_check(ret, 'get_state_variable_info', 'opening')
 
 ! get all variable ids
-call get_variable_ids(variable_names, variable_ids)
+call get_variable_ids(variable_names, domain, variable_ids(:, domain))
 
 ! get all variable sizes, only readers store dimensions?
-variable_sizes = total_size(n, variable_ids)
-model_size = sum(variable_sizes)
+variable_sizes(:, domain) = total_size(n, variable_ids(:, domain), domain)
+domain_size = sum(variable_sizes(:, domain))
 if(my_task_id() == 0) then
    do i = 1, n
-      print*, i, 'variable_sizes', variable_sizes(i)
+      print*, i, 'variable_sizes', variable_sizes(i, domain)
    enddo
 endif
 
-if ( any(variable_sizes>limit_mem) ) then
+if ( any(variable_sizes(:, domain)>limit_mem) ) then
    print*, 'memory limit = ', limit_mem
    print*, variable_sizes
    call error_handler(E_ERR, 'get_state_variable_info', 'netcdf variables larger than memory limit')
@@ -124,9 +134,10 @@ end subroutine get_state_variable_info
 
 !-------------------------------------------------
 !> Get netcdf variable ids
-subroutine get_variable_ids(variable_names, variable_ids)
+subroutine get_variable_ids(variable_names, domain, variable_ids)
 
 character(len=*), intent(in)    :: variable_names(:) !< netcdf variable names
+integer,          intent(in)    :: domain
 integer,          intent(inout) :: variable_ids(:) !< netcdf variable ids
 
 integer :: n !< number of variables in the state
@@ -147,32 +158,31 @@ end subroutine get_variable_ids
 !> Get state variable size, i.e. how many elements are in the variable
 !> readers need to store the dimension information
 !> Every task is storing the dimension information at the moment.
-function total_size(n, varId)
+function total_size(n, varId, domain)
 
 integer, intent(in)  :: n !< number of variables in the state vector
 integer, intent(in)  :: varId(n) !< variable ids
+integer, intent(in)  :: domain
 integer              :: total_size(n) ! NOTE RETURN VALUE LAST
 
 integer              :: ndims !< number of dimensions
 integer              :: i, j !< loop variable
-
-allocate(dimensions_and_lengths(n, MAXDIMS +1))
-dimensions_and_lengths = -1  ! initialize to a nonsense value
+integer              :: xtype !< variable type: NF90_FLOAT, NF90_DOUBLE, etc. I don't think we need to store this
 
 do i = 1, n
 
-   ret = nf90_inquire_variable(ncfile, varId(i), ndims=ndims, dimids=dimIds(i, :), xtype=variable_xtypes(i))
+   ret = nf90_inquire_variable(ncfile, varId(i), ndims=ndims, dimids=dimIds(i, :, domain), xtype=xtype)
    call nc_check(ret, 'totalsize', 'inq_var')
 
    do j = 1, ndims
-      ret = nf90_inquire_dimension(ncfile, dimIds(i, j), name=dim_names(i, j), len=length(i, j))
+      ret = nf90_inquire_dimension(ncfile, dimIds(i, j, domain), name=dim_names(i, j, domain), len=length(i, j, domain))
       call nc_check(ret, 'totalsize', 'inq_dimlen')
    enddo
 
-   total_size(i) = product(length(i, 1:ndims))
+   total_size(i) = product(length(i, 1:ndims, domain))
 
-   dimensions_and_lengths(i, 1) = ndims
-   dimensions_and_lengths(i, 2:ndims+1) = length(i, 1:ndims)
+   dimensions_and_lengths(i, 1, domain) = ndims
+   dimensions_and_lengths(i, 2:ndims+1, domain) = length(i, 1:ndims, domain)
 
 enddo
 
@@ -186,9 +196,11 @@ end function total_size
 !> limit_mem AND the task limit imposed by limit_procs
 !> limit_procs is used to devide the pes into groups.  Note that the
 !> groups are not created using mpi_group_incl.
-subroutine read_transpose(state_ens_handle)
+subroutine read_transpose(state_ens_handle, domain, dart_index)
 
 type(ensemble_type), intent(inout) :: state_ens_handle
+integer,             intent(in)    :: domain
+integer,             intent(inout) :: dart_index !< This is for mulitple domains
 
 integer :: i
 integer :: start_var, end_var !< start/end variables in a read block
@@ -213,7 +225,7 @@ my_pe = state_ens_handle%my_pe
 
 ! what to do if a variable is larger than the memory limit?
 start_var = 1 ! read first variable first
-starting_point = 1 ! position in state_ens_handle%copies
+starting_point = dart_index ! position in state_ens_handle%copies
 
 ! need to calculate RECEIVING_PE_LOOP start:end, group size, sending_pe start:end for each group.
 call get_pe_loops(my_pe, ens_size, group_size, recv_start, recv_end, send_start, send_end)
@@ -231,16 +243,17 @@ do dummy_loop = 1, num_state_variables
    if (start_var > num_state_variables) exit ! instead of using do while loop
 
    ! calculate how many variables will be read
-   end_var = calc_end_var(start_var)
+   end_var = calc_end_var(start_var, domain)
    if (my_task_id() == 0) print*, 'start_var, end_var', start_var, end_var
-   block_size = sum(variable_sizes(start_var:end_var))
+   block_size = sum(variable_sizes(start_var:end_var, domain))
 
    if ((my_pe >= send_start) .and. (my_pe <= send_end)) then ! I am a reader 
       allocate(var_block(block_size))
-      call read_variables(var_block, start_var, end_var)
+      call read_variables(var_block, start_var, end_var, domain)
    endif
 
-   start_rank = mod(sum(variable_sizes(1:start_var-1)), task_count())
+   start_rank = mod(sum_variables_below(start_var,domain), task_count())
+   if(my_task_id()==0) print*, 'start rank', start_rank, sum_variables_below(start_var,domain)
 
    ! loop through and post recieves 
    RECEIVING_PE_LOOP: do recv_pe = recv_start, recv_end
@@ -269,6 +282,7 @@ do dummy_loop = 1, num_state_variables
                state_ens_handle%copies(ensemble_member, starting_point:ending_point ) = &
                   var_block(i:count*task_count():task_count())
             else ! post receive
+               !print*, 'start:end', starting_point, ending_point, 'dom', domain
                call receive_from(map_pe_to_task(state_ens_handle, sending_pe), &
                                  state_ens_handle%copies(ensemble_member, starting_point:ending_point))
             endif
@@ -278,6 +292,7 @@ do dummy_loop = 1, num_state_variables
          enddo RECEIVE_FROM_EACH
 
          ! update starting point
+
          starting_point = starting_point + count
 
       elseif ((my_pe >= send_start) .and. (my_pe <= send_end)) then ! sending
@@ -295,12 +310,13 @@ do dummy_loop = 1, num_state_variables
 
 enddo
 
-
 ! close netcdf file
 if ((my_pe >= send_start) .and. (my_pe <= send_end)) then ! I am a reader 
    ret = nf90_close(ncfile)
    call nc_check(ret, 'read_transpose','closing')
 endif
+
+dart_index = starting_point
 
 end subroutine read_transpose
 
@@ -312,9 +328,11 @@ end subroutine read_transpose
 !> 
 !> Start with limited memory only ( use all tasks )
 !> Using the term collectors = first stage of amalgamating 
-subroutine transpose_write(state_ens_handle)
+subroutine transpose_write(state_ens_handle, domain, dart_index)
 
 type(ensemble_type), intent(inout) :: state_ens_handle
+integer,             intent(in)    :: domain
+integer,             intent(inout) :: dart_index
 
 integer :: i
 integer :: start_var, end_var !< start/end variables in a read block
@@ -346,7 +364,7 @@ ens_size = state_ens_handle%num_copies ! this is not true, this is copies + extr
 my_pe = state_ens_handle%my_pe
 
 start_var = 1 ! collect first variable first
-starting_point = 1 ! position in state_ens_handle%copies
+starting_point = dart_index ! position in state_ens_handle%copies
 
 ! post recieves
 ! need to calculate RECEIVING_PE_LOOP start:end, group size, sending_pe start:end for each group.
@@ -360,18 +378,16 @@ endif
 
 do while (start_var <= num_state_variables)
 
-   print*, 'starting point', starting_point
-
    ! calculate how many variables will be sent to writer
-   end_var = calc_end_var(start_var)
+   end_var = calc_end_var(start_var, domain)
    if (my_task_id() == 0) print*, 'start_var, end_var', start_var, end_var
-   num_vars = sum(variable_sizes(start_var:end_var))
+   num_vars = sum(variable_sizes(start_var:end_var, domain))
 
    if ((my_pe >= recv_start) .and. (my_pe <= recv_end)) then ! I am a collector
       allocate(var_block(num_vars))
    endif
 
-   start_rank = mod(sum(variable_sizes(1:start_var-1)), task_count())
+   start_rank =  mod(sum_variables_below(start_var,domain), task_count())
 
    SENDING_PE_LOOP: do sending_pe = send_start, send_end
 
@@ -415,7 +431,7 @@ do while (start_var <= num_state_variables)
 
       if (my_pe < ens_size) then ! I am a writer
          !var_block = MISSING_R8  ! if you want to create a file for bitwise testing
-         call write_variables(var_block, start_var, end_var)
+         call write_variables(var_block, start_var, end_var, domain)
          deallocate(var_block)
       endif
 
@@ -440,7 +456,7 @@ do while (start_var <= num_state_variables)
             deallocate(array_of_blocks, array_of_displacements)
             call mpi_type_free(collector_type, ierr)
 
-         call write_variables(var_block, start_var, end_var)
+         call write_variables(var_block, start_var, end_var, domain)
          deallocate(var_block)
 
          enddo
@@ -459,14 +475,17 @@ if (my_pe < ens_size ) then ! I am a writer
    call nc_check(ret, 'transpose_write', 'closing')
 endif
 
+dart_index= starting_point
+
 end subroutine transpose_write
 
 !-------------------------------------------------
 !> Calculate how many variables to read in one go.
-function calc_end_var(start_var)
+function calc_end_var(start_var, domain)
 
-integer :: calc_end_var !< end variable index
-integer :: start_var !< start variable index
+integer              :: calc_end_var !< end variable index
+integer, intent(in)  :: start_var !< start variable index
+integer, intent(in)  :: domain
 
 integer :: i, count
 integer, allocatable :: num_elements(:) !< cummulative size
@@ -478,7 +497,7 @@ calc_end_var = num_state_variables ! assume you can fit them all to start with
 count = 0
 
 do i = 1, num_state_variables - start_var + 1
-   num_elements(i) = sum(variable_sizes(start_var:start_var + count))
+   num_elements(i) = sum(variable_sizes(start_var:start_var + count, domain))
    count = count + 1
 enddo
 
@@ -513,11 +532,12 @@ end function calc_end_var
 
 !-------------------------------------------------
 !> Read in variables from start_var to end_var
-subroutine read_variables(var_block, start_var, end_var)
+subroutine read_variables(var_block, start_var, end_var, domain)
 
 real(r8), intent(inout) :: var_block(:)
 integer,  intent(in)    :: start_var
 integer,  intent(in)    :: end_var
+integer,  intent(in)    :: domain
 
 integer :: i
 integer :: start_in_var_block
@@ -525,18 +545,19 @@ integer :: var_size
 integer, allocatable :: dims(:)
 
 start_in_var_block = 1
+
 do i = start_var, end_var
 
-   var_size = variable_sizes(i)
+   var_size = variable_sizes(i, domain)
 
    ! number of dimensions and length of each
-   allocate(dims(dimensions_and_lengths(i,1)))
+   allocate(dims(dimensions_and_lengths(i,1, domain)))
 
-   dims = dimensions_and_lengths(i, 2:dimensions_and_lengths(i,1) + 1)
+   dims = dimensions_and_lengths(i, 2:dimensions_and_lengths(i,1, domain) + 1, domain)
 
-   ret = nf90_get_var(ncfile, variable_ids(i), var_block(start_in_var_block:start_in_var_block+var_size-1), count=dims)
-
+   ret = nf90_get_var(ncfile, variable_ids(i, domain), var_block(start_in_var_block:start_in_var_block+var_size-1), count=dims)
    call nc_check(ret, 'read_variables','reading')
+
    start_in_var_block = start_in_var_block + var_size
 
    deallocate(dims)
@@ -547,11 +568,12 @@ end subroutine read_variables
 
 !-------------------------------------------------
 !> Write variables from start_var to end_var
-subroutine write_variables(var_block, start_var, end_var)
+subroutine write_variables(var_block, start_var, end_var, domain)
 
 real(r8), intent(in) :: var_block(:)
 integer,  intent(in) :: start_var
 integer,  intent(in) :: end_var
+integer,  intent(in) :: domain 
 
 integer :: i
 integer :: count_displacement
@@ -562,13 +584,13 @@ integer, allocatable :: dims(:)
 start_in_var_block = 1
 do i = start_var, end_var
 
-   var_size = variable_sizes(i)
+   var_size = variable_sizes(i, domain)
    
    ! number of dimensions and length of each
-   allocate(dims(dimensions_and_lengths(i, 1))) 
-   dims = dimensions_and_lengths(i, 2:dimensions_and_lengths(i,1) + 1)
+   allocate(dims(dimensions_and_lengths(i, 1, domain)))
+   dims = dimensions_and_lengths(i, 2:dimensions_and_lengths(i,1, domain) + 1, domain)
 
-   ret = nf90_put_var(ncfile_out, variable_ids(i), var_block(start_in_var_block:start_in_var_block+var_size-1), count=dims)
+   ret = nf90_put_var(ncfile_out, variable_ids(i, domain), var_block(start_in_var_block:start_in_var_block+var_size-1), count=dims)
    call nc_check(ret, 'write_variables', 'writing')
    start_in_var_block = start_in_var_block + var_size
 
@@ -670,6 +692,26 @@ endif
 
 end function find_start_point
 
+!------------------------------------------------------
+!> finds number of elements in the state already
+function sum_variables_below(start_var, domain)
+
+integer, intent(in) :: start_var
+integer, intent(in) :: domain
+integer             :: sum_variables_below
+
+integer :: i
+
+sum_variables_below = 0
+
+do i = 1, domain -1
+   sum_variables_below = sum(variable_sizes(:, i)) ! whole domain below
+enddo
+
+sum_variables_below = sum_variables_below + sum(variable_sizes(1:start_var-1, i))
+
+
+end function sum_variables_below
 !-------------------------------------------------------
 !> @}
 end module state_vector_io_mod
