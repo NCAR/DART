3f75d37305609803e3b54ecf074f3c0d3cadcd86
kershaw-dart <Helen>
2014-07-03 21:17:15 +0000

    Little module to store filenames for now.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma@7045 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 32ae3f4e597846136122fa37ada17cae77af09d3

---
+++ io/io_filenames_mod.f90
@@ -0,0 +1,47 @@
+module io_filenames_mod
+
+!> \defgroup io_filenames io_filenames
+!> Aim is to store the io filenames
+!>  * Restarts
+!>  * Diagnostics
+!>  * Inflation files
+!>
+!> Any module can set the filenames here, then state_vector_io_mod
+!> can read from this module to get the filenames.
+!> Maybe this is a bit lazy, but I just want a way for the 
+!> different modules to set the filenames
+!>
+!> @{
+
+implicit none
+
+private
+
+! These should probably be set and get functions rather than 
+! direct access
+
+public :: restart_stub, &
+          prior_diagnostic_file, &
+          post_diagnostic_file, &
+          prior_mean_inf_file, &
+          prior_sd_inf_file, &
+          post_mean_inf_file, &
+          post_sd_inf_file
+
+! How do people name there restart files?
+! What about domains?
+character(len=256) :: restart_stub = 'restart'
+
+character(len=256) :: prior_diagnostic_file = 'Prior_diag'
+character(len=256) :: post_diagnostic_file = 'Posterior_diag'
+
+character(len=256) :: prior_mean_inf_file = 'prior_inf_ic_old_d'
+character(len=256) :: prior_sd_inf_file   = 'prior_inf_ic_sd_old_d'
+character(len=256) :: post_mean_inf_file  = 'post_inf_ic_old_d'
+character(len=256) :: post_sd_inf_file    = 'post_inf_ic_sd_old_d'
+
+contains
+
+
+!> @}
+end module io_filenames_mod
