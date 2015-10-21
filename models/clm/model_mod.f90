2caa4dee5411357e2ac2eae14b375899a2afbb7b
Tim Hoar <thoar@ucar.edu>
2015-10-21 20:10:33 +0000

    Fixed logic in FindVarIndex() ... could not possibly have exited correctly if the variable was not found. The upshot is that if the target variable was not part of the DART state, the _last_ variable in the DART state would have been used for the forward observation operator! This would have undoubtedly resulted in outlier rejection ...
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@8880 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 97cbcb747bea08dec76a09cf7c7d45feb7879ec5

--- models/clm/model_mod.f90
+++ models/clm/model_mod.f90
@@ -4635,8 +4635,10 @@ findVarIndex = -1
 
 ! Skip to the right variable
 VARTYPES : do i = 1,nfields
-    findVarIndex = i
-    if ( trim(progvar(i)%varname) == varstring) exit VARTYPES
+    if ( trim(progvar(i)%varname) == varstring) then
+       findVarIndex = i
+       exit VARTYPES
+    endif
 enddo VARTYPES
 
 if (findVarIndex < 1) then
