96034023befc8e05903e58767451616c9755ba52
Tim Hoar <thoar@ucar.edu>
2016-04-20 15:29:05 +0000

    The long-awaited new set of Matlab GUIs for the DART tutorial. There is a TRELLO board for improvements to these, but at least they work pretty well with both 2012a and 2015b ..
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/trunk@10125 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: a435b4e4290967ef17c6dc44d109f8af01885925

--- DART_LAB/matlab/private/lorenz_63_static_init_model.m
+++ DART_LAB/matlab/private/lorenz_63_static_init_model.m
@@ -1,3 +1,5 @@
+function L63 = lorenz_63_static_init_model()
+
 %% lorenz_63_static_init_model Initializes class data for L63, sets up global storage
 % and reads in control data from input file
 
@@ -8,30 +10,15 @@
 % DART $Id$
 
 % Lorenz-63 model parameters
-global SIGMA
-global R
-global B
-global DELTAT
-global TIME_STEP_DAYS
-global TIME_STEP_SECONDS
 
-% Set default values for the model parameters
-SIGMA = 10;
-R = 28;
-B = 8/3;
-DELTAT = 0.01;
-TIME_STEP_DAYS = 0;
-TIME_STEP_SECONDS = 0;
-
-
-% Lorenz-63 fixed model parameters
-global MODEL_SIZE
-
-MODEL_SIZE = 3;
-
-global STATE_LOC
-
-STATE_LOC = (0:2) / 3;
+L63.sigma             = 10;
+L63.r                 = 28;
+L63.b                 = 8/3;
+L63.deltat            = 0.01;
+L63.time_step_days    = 0;
+L63.time_step_seconds = 0;
+L63.model_size        = 3;
+L63.state_loc         = (0:2) / 3;
 
 % <next few lines under version control, do not edit>
 % $URL$
--- DART_LAB/matlab/private/lorenz_96_static_init_model.m
+++ DART_LAB/matlab/private/lorenz_96_static_init_model.m
@@ -1,5 +1,6 @@
-%% lorenz_96_static_init_model Initializes class data for L96, sets up global storage
-% and reads in control data from input file
+function L96 = lorenz_96_static_init_model()
+
+%% lorenz_96_static_init_model Initializes class data for L96
 
 %% DART software - Copyright 2004 - 2013 UCAR. This open source software is
 % provided by UCAR, "as is", without charge, subject to all terms of use at
@@ -8,26 +9,13 @@
 % DART $Id$
 
 % Lorenz-96 model parameters
-global FORCING
-global DELTA_T
-global TIME_STEP_DAYS
-global TIME_STEP_SECONDS
 
-% Set default values for the model parameters
-FORCING = 8;
-DELTA_T = 0.05;
-TIME_STEP_DAYS = 0;
-TIME_STEP_SECONDS = 0;
-
-
-% Lorenz-96 fixed model parameters
-global MODEL_SIZE
-
-MODEL_SIZE = 40;
-
-global STATE_LOC
-
-STATE_LOC = (0:MODEL_SIZE - 1) / MODEL_SIZE;
+L96.time_step_days    = 0;
+L96.time_step_seconds = 0;
+L96.delta_t           = 0.05;
+L96.forcing    = 8;
+L96.model_size = 40;
+L96.state_loc  = (0:L96.model_size - 1) / L96.model_size;
 
 % <next few lines under version control, do not edit>
 % $URL$
