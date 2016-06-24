118cfcb6ba46a8c1e6bf12320998d579f3792d33
Nancy Collins <nancy@ucar.edu>
2016-06-24 21:25:04 +0000

    add some new options for obs types to assimilate.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@10395 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 806bedf818fb9acc3e81e3e157e3a2e0d2031cd7

--- obs_def/obs_def_cice_mod.f90
+++ obs_def/obs_def_cice_mod.f90
@@ -4,7 +4,15 @@
 ! at
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
 
+! FIXME: check to see if obs are of volume or thickness - for now we
+! will assume volume.
+
+! FIXME: do we want to identify the satellite? (yes)
+!  AMSRE is a passive microwave
+
 ! BEGIN DART PREPROCESS KIND LIST
-!SAT_SEAICE_AGREG_CONCENTR,         KIND_SEAICE_CONCENTR,   COMMON_CODE
+!SAT_SEAICE_AGREG_CONCENTR,         KIND_SEAICE_AGREG_CONCENTR,   COMMON_CODE
+!SAT_SEAICE_VOLUME,                 KIND_SEAICE_VOLUME,           COMMON_CODE
+!AMSRE_SEAICE_VOLUME,               KIND_SEAICE_VOLUME,           COMMON_CODE
 ! END DART PREPROCESS KIND LIST
 
