f2fea16ca590c092289fc53bd677f34c56af022b
Johnny Hendricks <Johnny>
2016-06-24 17:09:29 +0000

    adding cice KINDS  and cice obs def module.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@10388 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 20074d6b8b8e6ff884040b26e99125da1c521754

---
+++ obs_def/obs_def_cice_mod.f90
@@ -0,0 +1,10 @@
+! DART software - Copyright 2004 - 2011 UCAR. This open source software
+! is
+! provided by UCAR, "as is", without charge, subject to all terms of use
+! at
+! http://www.image.ucar.edu/DAReS/DART/DART_download
+
+! BEGIN DART PREPROCESS KIND LIST
+!SAT_SEAICE_AGREG_CONCENTR,         KIND_SEAICE_CONCENTR,   COMMON_CODE
+! END DART PREPROCESS KIND LIST
+
