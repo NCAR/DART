f11b30caf5671e3aaee47b58f262a948ff86b141
Tim Hoar <thoar@ucar.edu>
2016-06-29 21:01:15 +0000

    has debugging options and runs - correctly. If something changes, the new values are written - without corrupting the rest of the file. If nothing changes, nothing gets written but the log message reflects that.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@10440 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: 95dfc1bd63f33c1af1b9091a634cade60f41cd82

--- models/cice/dart_to_cice.f90
+++ models/cice/dart_to_cice.f90
@@ -18,8 +18,8 @@ program dart_to_cice
 use        types_mod, only : r8
 use    utilities_mod, only : initialize_utilities, finalize_utilities, &
                              find_namelist_in_file, check_namelist_read, &
-                             logfileunit, nc_check, E_ERR, file_exist, &
-                             error_handler
+                             logfileunit, nc_check, file_exist, &
+                             error_handler, E_ERR, E_MSG, to_upper
 use netcdf 
 
 implicit none
@@ -32,12 +32,13 @@ character(len=128), parameter :: revdate  = "$Date$"
 
 !------------------------------------------------------------------
 
-character(len=256) :: dart_to_cice_input_file   = 'dart_restart.nc'
-character(len=256) :: balance_method = 'simple_squeeze'
+character(len=256) :: dart_to_cice_input_file = 'dart_restart.nc'
+character(len=128) :: balance_method = 'simple_squeeze'
 namelist /dart_to_cice_nml/ dart_to_cice_input_file, balance_method
 
-character(len=512) :: string1, string2, string3, msgstring
-character(len=15) :: varname
+character(len=512) :: string1, string2, msgstring
+character(len=15)  :: varname
+character(len=128) :: method
 
 integer :: Nx, Ny
 integer :: Ncat   ! number of categories in ice-thickness dist
@@ -47,18 +48,12 @@ real(r8), allocatable :: vicen(:,:,:)
 real(r8), allocatable :: vsnon(:,:,:)
 real(r8), allocatable ::  aice(:,:)
 
-! real(r8) :: aicen(Nx,Ny,Ncat), vicen(Nx,Ny,Ncat), vsnon(Nx,Ny,Ncat)
-! real(r8) :: aice(Nx,Ny)
-
 integer  :: i, j, k
-integer  :: DimID, VarID, ncid, iunit, io, ndims
+integer  :: VarID, ncid, iunit, io, ndims
 real(r8) :: squeeze
 
-! TODO FIXME ... CC ... don't understand the purpose of this.
+! If nothing needs to be rebalanced, no need to write to file.
 logical  :: update_restart = .false.
-
-integer, dimension(NF90_MAX_VAR_DIMS) :: dimIDs, dimLengths
-character(len=NF90_MAX_NAME)          :: dimName
 
 !----------------------------------------------------------------------
 
@@ -68,10 +63,13 @@ call find_namelist_in_file("input.nml", "dart_to_cice_nml", iunit)
 read(iunit, nml = dart_to_cice_nml, iostat = io)
 call check_namelist_read(iunit, io, "dart_to_cice_nml")
 
-write(*,*)
-write(*,'(''dart_to_cice:converting DART output restart file '',A, &
-      &'' to one CICE will like'')') &
-     trim(dart_to_cice_input_file)
+method = balance_method
+call to_upper(method)
+
+write(string1,*) 'converting DART output file "'// &
+                 &trim(dart_to_cice_input_file)//'" to one CICE will like'
+write(string2,*) 'using the "'//trim(balance_method)//'" method.'
+call error_handler(E_MSG,'dart_to_cice',string1,text2=string2)
 
 if ( .not. file_exist(dart_to_cice_input_file) ) then
    write(string1,*) 'cannot open file ', trim(dart_to_cice_input_file),' for updating.'
@@ -100,43 +98,54 @@ do k = 2, Ncat
   aice = aice+aicen(:,:,k)
 enddo
 
-! fix me if (.not. present(balance_method)) balance_method='simple_squeeze'
+SELECT CASE (method)
 
-SELECT CASE (balance_method)
-  CASE ('simple_squeeze')
+  CASE ('SIMPLE_SQUEEZE')
      do j = 1, Ny
         do i = 1, Nx
-           if (aice(i,j).lt.0.) then
-              aicen(i,j,:)=0.
-              vicen(i,j,:)=0.
-              vsnon(i,j,:)=0.
+           if (aice(i,j) < 0.0_r8) then
+              aicen(i,j,:)   = 0.0_r8
+              vicen(i,j,:)   = 0.0_r8
+              vsnon(i,j,:)   = 0.0_r8
               update_restart = .true.
-           elseif (aice(i,j).gt.1.) then
-              squeeze=1./aice(i,j)
-              aicen(i,j,:)=aicen(i,j,:)*squeeze
-              vicen(i,j,:)=vicen(i,j,:)*squeeze
-              vsnon(i,j,:)=vsnon(i,j,:)*squeeze
+
+           elseif (aice(i,j) > 1.0_r8) then
+              squeeze        = 1.0_r8 / aice(i,j)
+              aicen(i,j,:)   = aicen(i,j,:)*squeeze
+              vicen(i,j,:)   = vicen(i,j,:)*squeeze
+              vsnon(i,j,:)   = vsnon(i,j,:)*squeeze
               update_restart = .true.
+
            endif
         enddo
      enddo
-  CASE ('tendency_weight')  ! this is identical to the above for now
+
+  CASE ('TENDENCY_WEIGHT')  ! this is identical to the above for now
      do j = 1, Ny
         do i = 1, Nx
-           if (aice(i,j).lt.0.) then
-              aicen(i,j,:)=0.
-              vicen(i,j,:)=0.
-              vsnon(i,j,:)=0.
+           if (aice(i,j) < 0.0_r8) then
+              aicen(i,j,:)   = 0.0_r8
+              vicen(i,j,:)   = 0.0_r8
+              vsnon(i,j,:)   = 0.0_r8
               update_restart = .true.
-           elseif (aice(i,j).gt.1.) then
-              squeeze=1./aice(i,j)
-              aicen(i,j,:)=aicen(i,j,:)*squeeze
-              vicen(i,j,:)=vicen(i,j,:)*squeeze
-              vsnon(i,j,:)=vsnon(i,j,:)*squeeze
+
+           elseif (aice(i,j) > 1.0_r8) then
+              squeeze        = 1.0_r8 / aice(i,j)
+              aicen(i,j,:)   = aicen(i,j,:)*squeeze
+              vicen(i,j,:)   = vicen(i,j,:)*squeeze
+              vsnon(i,j,:)   = vsnon(i,j,:)*squeeze
               update_restart = .true.
+
            endif
         enddo
      enddo
+
+   CASE DEFAULT
+
+      write(string1,*)'input.nml:dart_to_cice_nml:balance_method "'//trim(balance_method)//'" unsupported.'
+      write(string2,*)'valid values are "simple_squeeze" or "tendency_weight"'
+      call error_handler(E_ERR,'dart_to_cice',string1,source, revision, revdate, text2=string2)
+
 END SELECT
 
 !for testing make something to fix
@@ -144,30 +153,43 @@ END SELECT
 !  aicen(10,10,1)=1.1
 !  write(*,*) (aicen(10,10,k), k=1,5)
 
-if (update_restart) then
-   ! now update the variables
+if (update_restart) then ! if something changed, write the new values
    
    varname='aicen'
    io = nf90_inq_varid(ncid, trim(varname), VarID)
-   call nc_check(io, 'dart_to_cice', 'inq_varid '//trim(varname)//' '//trim(dart_to_cice_input_file))
+   call nc_check(io, 'dart_to_cice', &
+                 'inq_varid '//trim(varname)//' '//trim(dart_to_cice_input_file))
    io = nf90_put_var(ncid, VarID, aicen)
-   call nc_check(io, 'dart_to_cice', 'put_var '//trim(varname)//' '//trim(dart_to_cice_input_file))
+   call nc_check(io, 'dart_to_cice', &
+                 'put_var '//trim(varname)//' '//trim(dart_to_cice_input_file))
    
    varname='vicen'
    io = nf90_inq_varid(ncid, trim(varname), VarID)
-   call nc_check(io, 'dart_to_cice', 'inq_varid '//trim(varname)//' '//trim(dart_to_cice_input_file))
+   call nc_check(io, 'dart_to_cice', &
+                 'inq_varid '//trim(varname)//' '//trim(dart_to_cice_input_file))
    io = nf90_put_var(ncid, VarID, vicen)
-   call nc_check(io, 'dart_to_cice', 'put_var '//trim(varname)//' '//trim(dart_to_cice_input_file))
+   call nc_check(io, 'dart_to_cice', &
+                 'put_var '//trim(varname)//' '//trim(dart_to_cice_input_file))
    
    varname='vsnon'
    io = nf90_inq_varid(ncid, trim(varname), VarID)
-   call nc_check(io, 'dart_to_cice', 'inq_varid '//trim(varname)//' '//trim(dart_to_cice_input_file))
+   call nc_check(io, 'dart_to_cice', &
+                 'inq_varid '//trim(varname)//' '//trim(dart_to_cice_input_file))
    io = nf90_put_var(ncid, VarID, vsnon)
-   call nc_check(io, 'dart_to_cice', 'put_var '//trim(varname)//' '//trim(dart_to_cice_input_file))
+   call nc_check(io, 'dart_to_cice', &
+                 'put_var '//trim(varname)//' '//trim(dart_to_cice_input_file))
+
+else
+
+   write(string1,*)'Nothing changed in "'//trim(dart_to_cice_input_file)//'"'
+   write(string2,*)'No need to update.' 
+   call error_handler(E_MSG,'dart_to_cice',string1,text2=string2)
    
 endif
 
 call nc_check(nf90_close(ncid),'dart_to_cice', 'close '//trim(dart_to_cice_input_file))
+
+deallocate( aicen, vicen, vsnon, aice )
 
 call finalize_utilities('dart_to_cice')
 
