9ec48b410c6af9769fe161dd9b3d1435a4364f5d
Nancy Collins <nancy@ucar.edu>
2016-08-16 14:36:18 +0000

    clean up the comments around the fortran SELECTED_KIND sections, including some comments from the MPI standards on how to select a variable of the intended number of bytes.
    replaced parmnamelength with varnamelength, and will be committing
    other code that uses obstypelength only for obs strings and uses
    varnamelength for things unrelated to observations.  the fortran 90/95
    standard sets the max var name length as 31 (our 'preprocess' program
    generates integer parameter variables from strings and the limit
    applies there.)  apparently fortran 2003 raises that limit to 63 but
    at this time we're sticking with 31.
    
    
    
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@10624 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: df87ccc47f93d46095b6572685ed24b70b7f5e76

--- common/types_mod.f90
+++ common/types_mod.f90
@@ -4,8 +4,12 @@
 !
 ! $Id$
 
-MODULE types_mod
+!> Should only have fortran "kinds".  the constants should
+!> go into their own module, or this should be renamed 'constants'
+!> or something more descriptive than 'types' (which overlaps
+!> into our obs kinds and types).
 
+module types_mod
 
 implicit none
 private 
@@ -16,9 +20,7 @@ public :: MISSING_I, MISSING_DATA
 public :: SECPERDAY
 public :: t_kelvin, es_alpha, es_beta, es_gamma, gas_constant_v, gas_constant
 public :: L_over_Rv, ps0, earth_radius, gravity
-public :: metadatalength
-public :: obstypelength
-public :: parmnamelength
+public :: metadatalength, obstypelength, varnamelength, vtablenamelength
 
 ! version controlled file description for error handling, do not edit
 character(len=256), parameter :: source   = &
@@ -26,74 +28,96 @@ character(len=256), parameter :: source   = &
 character(len=32 ), parameter :: revision = "$Revision$"
 character(len=128), parameter :: revdate  = "$Date$"
 
-SAVE
-
 !----------------------------------------------------------------------------
 ! constants that need to be shared - really has nothing to do with types ...
 !----------------------------------------------------------------------------
 
-integer, parameter :: metadatalength = 64 ! 75 is max w/out wrapping in ncdump
-integer, parameter :: obstypelength  = 32 ! set by compiler/param limits
-integer, parameter :: parmnamelength = 32 ! max name of any parameter
+integer, parameter :: metadatalength   = 64 ! 75 is max w/out wrapping in ncdump
+integer, parameter :: varnamelength    = 31 ! max name of any fortran variable
+                                            ! in F90/95. increased to 63 in F2003
+integer, parameter :: obstypelength    = 31 ! because of variable name limits.
+                                            ! use this for obs types/kinds
+integer, parameter :: vtablenamelength = 64 ! use this for the model_mod variable table items
+                                           
 
 !----------------------------------------------------------------------------
-! Attributes for variable kinds -- no need to rely on -r8 switch in compiler
-! all real variables are 64bit, ditto for all integer variables.
+! Attributes for variable kinds -- declaring sizes explicitly means we know
+! exactly what precision we are using and are not relying on compiler flags
+! to set the defaults for real and int.
+!
+! from the MPI documentation, they say:
+!
+! for reals, p and r values result in:
+!   if      (p > 33) or (r > 4931) then  external32 representation 
+!                                        is undefined   
+!   else if (p > 15) or (r >  307) then  external32_size = 16 
+!   else if (p >  6) or (r >   37) then  external32_size =  8 
+!   else                                 external32_size =  4 
+!
+!
+! for integers, r results in:
+!   if      (r > 38) then  external32 representation is undefined 
+!   else if (r > 18) then  external32_size =  16  
+!   else if (r >  9) then  external32_size =  8  
+!   else if (r >  4) then  external32_size =  4 
+!   else if (r >  2) then  external32_size =  2  
+!   else                   external32_size =  1  
+!
 !----------------------------------------------------------------------------
-! These are TJH's favorites
-! integer, parameter :: i4 = SELECTED_INT_KIND(8)
-! integer, parameter :: i8 = SELECTED_INT_KIND(17)
-! integer, parameter :: r4 = SELECTED_REAL_KIND(6,30)
-! integer, parameter :: c4 = SELECTED_REAL_KIND(6,30)
-! integer, parameter :: r8 = SELECTED_REAL_KIND(12,100)
-! integer, parameter :: c8 = SELECTED_REAL_KIND(12,100)
 
-! These comply with the CCM4 standard, as far as I can tell.
+! integer precision:
+integer, parameter :: i2 = SELECTED_INT_KIND(3)
+integer, parameter :: i4 = SELECTED_INT_KIND(8)
+integer, parameter :: i8 = SELECTED_INT_KIND(13)
 
-  integer, parameter :: i2 = SELECTED_INT_KIND(3)
-  integer, parameter :: i4 = SELECTED_INT_KIND(8)
-  integer, parameter :: i8 = SELECTED_INT_KIND(13)
-  integer, parameter :: r4 = SELECTED_REAL_KIND(6,30)
-  integer, parameter :: c4 = SELECTED_REAL_KIND(6,30)
-! comment in only one of the following lines, not both.   the first is the default.
-  integer, parameter :: r8 = SELECTED_REAL_KIND(12)   ! real r8
-!!integer, parameter :: r8 = r4                       ! alias r8 to r4
-  integer, parameter :: c8 = SELECTED_REAL_KIND(12)
+! real precision:
+integer, parameter :: r4 = SELECTED_REAL_KIND(6,30)
+integer, parameter :: r8 = SELECTED_REAL_KIND(12)   ! real r8
+
+! complex precision:
+integer, parameter :: c4 = SELECTED_REAL_KIND(6,30)
+integer, parameter :: c8 = SELECTED_REAL_KIND(12)
+
+! TO RUN WITH REDUCED PRECISION REALS (and use correspondingly less memory)
+! comment OUT the r8 definition above and use this one:
+! integer, parameter :: r8 = r4                       ! alias r8 to r4
+
 
 ! 'digits12' is reserved for real variables that MUST retain 64 bits of
 ! precision. DO NOT CHANGE '12' to a smaller number. BAD BAD BAD things happen.
 ! This is a small subset of the variables. Changing this will ruin the ability
 ! to distinguish timesteps that are a few seconds apart, for instance.
 
-  integer, parameter :: digits12 = SELECTED_REAL_KIND(12)
+integer, parameter :: digits12 = SELECTED_REAL_KIND(12)
 
 !----------------------------------------------------------------------------
-! Constants ... 
+! Constants, some of which are model dependent and shouldn't be in this
+! module.
 !----------------------------------------------------------------------------
 
-integer,       PARAMETER ::  SECPERDAY = 24*60*60
+integer,       parameter ::  SECPERDAY = 24*60*60
 
 real(kind=r8), parameter :: PI = 3.14159265358979323846_r8
 real(kind=r8), parameter :: DEG2RAD = PI / 180.0_r8
 real(kind=r8), parameter :: RAD2DEG = 180.0_r8 / PI
 
-integer,       PARAMETER ::  MISSING_I    = -888888
-integer,       PARAMETER ::  MISSING_DATA = -88
-real(kind=r4), PARAMETER ::  MISSING_R4   = -888888.0_r4
-real(kind=r8), PARAMETER ::  MISSING_R8   = -888888.0_r8
+integer,       parameter ::  MISSING_I    = -888888
+integer,       parameter ::  MISSING_DATA = -88
+real(kind=r4), parameter ::  MISSING_R4   = -888888.0_r4
+real(kind=r8), parameter ::  MISSING_R8   = -888888.0_r8
 
-real(r8), PARAMETER :: t_kelvin       = 273.15_r8
-real(r8), PARAMETER :: es_alpha       = 611.2_r8
-real(r8), PARAMETER :: es_beta        = 17.67_r8
-real(r8), PARAMETER :: es_gamma       = 243.5_r8
-real(r8), PARAMETER :: gas_constant_v = 461.6_r8
-real(r8), PARAMETER :: gas_constant   = 287.0_r8 ! wikipedia has 287.06, WRF has 287.05 ...
-real(r8), PARAMETER :: L_over_Rv      = 5418.12_r8
-real(r8), PARAMETER :: ps0            = 100000.0_r8    ! Base sea level pressure
-real(r8), PARAMETER :: earth_radius   = 6370.0_r8      ! km, consistant with WRF
-real(r8), PARAMETER :: gravity        = 9.81_r8        ! wikipedia has 9.80665
+real(r8), parameter :: t_kelvin       = 273.15_r8
+real(r8), parameter :: es_alpha       = 611.2_r8
+real(r8), parameter :: es_beta        = 17.67_r8
+real(r8), parameter :: es_gamma       = 243.5_r8
+real(r8), parameter :: gas_constant_v = 461.6_r8
+real(r8), parameter :: gas_constant   = 287.0_r8 ! wikipedia has 287.06, WRF has 287.05 ...
+real(r8), parameter :: L_over_Rv      = 5418.12_r8
+real(r8), parameter :: ps0            = 100000.0_r8    ! Base sea level pressure
+real(r8), parameter :: earth_radius   = 6370.0_r8      ! km, consistant with WRF
+real(r8), parameter :: gravity        = 9.81_r8        ! wikipedia has 9.80665
 
-END MODULE types_mod
+end module types_mod
 
 ! <next few lines under version control, do not edit>
 ! $URL$
