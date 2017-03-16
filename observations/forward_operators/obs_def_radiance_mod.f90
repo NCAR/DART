981c899d3cb6eeea805943825f8691b1d27bd0aa
Nancy Collins <nancy@ucar.edu>
2017-03-16 15:30:23 +0000

    observation types and quantities for radiance obs.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@11358 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: bc9c36cd734581f5e476a475806f0af2fa878f44

---
+++ observations/forward_operators/obs_def_radiance_mod.f90
@@ -0,0 +1,43 @@
+! DART software - Copyright 2004 - 2013 UCAR. This open source software is
+! provided by UCAR, "as is", without charge, subject to all terms of use at
+! http://www.image.ucar.edu/DAReS/DART/DART_download
+!
+! $Id$
+
+! This module supports the observation types from the AIRS instruments.
+! http://winds.jpl.nasa.gov/missions/quikscat/index.cfm
+
+! BEGIN DART PREPROCESS KIND LIST
+! AMSUA_METOP_A_CH5,            QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_METOP_A_CH6,            QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_METOP_A_CH7,            QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_METOP_A_CH8,            QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_METOP_A_CH9,            QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N15_CH5,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N15_CH6,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N15_CH7,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N15_CH8,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N15_CH9,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N18_CH5,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N18_CH6,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N18_CH7,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N18_CH8,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N18_CH9,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N19_CH5,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N19_CH6,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N19_CH7,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N19_CH8,                QTY_TEMPERATURE,        COMMON_CODE
+! AMSUA_N19_CH9,                QTY_TEMPERATURE,        COMMON_CODE
+! MHS_METOP_A_CH3,              QTY_TEMPERATURE,        COMMON_CODE
+! MHS_METOP_A_CH4,              QTY_TEMPERATURE,        COMMON_CODE
+! MHS_METOP_A_CH5,              QTY_TEMPERATURE,        COMMON_CODE
+! MHS_N18_CH3,                  QTY_TEMPERATURE,        COMMON_CODE
+! MHS_N18_CH4,                  QTY_TEMPERATURE,        COMMON_CODE
+! MHS_N18_CH5,                  QTY_TEMPERATURE,        COMMON_CODE
+! END DART PREPROCESS KIND LIST
+
+! <next few lines under version control, do not edit>
+! $URL$
+! $Id$
+! $Revision$
+! $Date$
