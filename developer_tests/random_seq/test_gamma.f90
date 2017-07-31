f1b2460ac0bf46ca5ff9e6b165cddeb883097b17
Nancy Collins <nancy@ucar.edu>
2017-07-31 17:21:33 +0000

    added gamma, inverse gamma, and exponential distributions to the random number generator code.  includes updated documentation and a range of test programs to verify correctness.
    also updated the CHANGELOG in preparation for pushing these
    changes to the release branch.
    
    
    
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@11849 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: cf05c505fb02934a1dfdf666d0c27540a96e4fd9

--- developer_tests/random_seq/test_gamma.f90
+++ developer_tests/random_seq/test_gamma.f90
@@ -4,9 +4,9 @@
 !
 ! $Id$
 
-program test_gaussian
+program test_gamma
 
-! test the gaussian distribution random number generator routine
+! test the gamma distribution random number generator routine
 ! with different means, standard deviations, and iteration counts.
 
 use      types_mod, only : r8
@@ -14,7 +14,7 @@ use  utilities_mod, only : register_module, &
                            open_file, close_file, &
                            initialize_utilities, finalize_utilities, &
                            squeeze_out_blanks
-use random_seq_mod, only : random_seq_type, init_random_seq, random_gaussian
+use random_seq_mod, only : random_seq_type, init_random_seq, random_gamma
 
 implicit none
 
@@ -26,38 +26,45 @@ character(len=128), parameter :: revdate  = "$Date$"
 
 type (random_seq_type) :: r
 integer :: i, j, n, f
-real(r8) :: r1, sqdist, mean_sqdist
-real(r8) :: mean, sd, var, compvar, compsd, compdiff
+real(r8) :: r1, sum, sumsq, k, h
+real(r8) :: mean, var, sd, compmean, compvar, compsd, compdiffm, compdiffsd
 logical  :: write_this_one
+character(len=50) :: formf = '(I12,2(F8.3),4(F12.5),2(F16.10),2(F12.5))'
 character(len=256) :: fname, temp
-character(len=50) :: formf = '(I12,2(F14.6),1(F24.16),2(F12.6))'
+
 logical :: write_me = .true.       ! if true, write each distribution into a file for later diagnostics
 integer :: write_limit = 1000000   ! but only if rep count is not greater than this limit
 
 type t_inputs
- real(r8) :: t_mean
- real(r8) :: t_stddev
+ real(r8) :: t_kappa
+ real(r8) :: t_theta
  integer  :: t_nreps
 end type
 
 
 ! to add more tests or change the parameters, specify a test count
-! and update the sets of (mean, stddev, sample size) here:
+! and update the sets of inputs here:
 
-integer, parameter :: ntests = 21
+integer, parameter :: ntests = 27
 type(t_inputs) :: t(ntests) = (/ &
-    t_inputs(  0.0_r8,  1.0_r8,       100), &
-    t_inputs(  0.0_r8,  1.0_r8,     10000), &
-    t_inputs(  0.0_r8,  1.0_r8,   1000000), &
-    t_inputs(  0.0_r8,  1.0_r8, 100000000), &
+    t_inputs(  1.0_r8,  1.0_r8,       100), &
+    t_inputs(  1.0_r8,  1.0_r8,     10000), &
+    t_inputs(  1.0_r8,  1.0_r8,   1000000), &
+    t_inputs(  1.0_r8,  1.0_r8, 100000000), &
+    t_inputs(  1.0_r8,  1.5_r8,   5000000), &
+    t_inputs(  1.0_r8,  2.5_r8,   5000000), &
     t_inputs(  1.0_r8,  5.0_r8,       100), &
     t_inputs(  1.0_r8,  5.0_r8,     10000), &
     t_inputs(  1.0_r8,  5.0_r8,   1000000), &
     t_inputs(  1.0_r8,  5.0_r8, 100000000), &
+    t_inputs(  1.0_r8,  5.0_r8,   1000000), &
+    t_inputs(  2.0_r8,  5.0_r8,   1000000), &
+    t_inputs(  4.0_r8,  5.0_r8,   1000000), &
+    t_inputs(  8.0_r8,  5.0_r8,   1000000), &
     t_inputs(  6.0_r8,  8.0_r8,   1000000), &
-    t_inputs(-86.0_r8,  8.0_r8,   1000000), &
+    t_inputs( 86.0_r8,  8.0_r8,   1000000), &
     t_inputs(  0.6_r8,  8.0_r8,   1000000), &
-    t_inputs( -6.0_r8,  8.0_r8,   1000000), &
+    t_inputs(  6.0_r8,  8.0_r8,   1000000), &
     t_inputs(956.0_r8,  8.0_r8,   1000000), &
     t_inputs( 36.0_r8,  8.0_r8,   1000000), &
     t_inputs(  6.0_r8,  1.0_r8,   1000000), &
@@ -69,57 +76,65 @@ type(t_inputs) :: t(ntests) = (/ &
     t_inputs(  6.0_r8, 33.5_r8,   1000000)  /)
 
 
-call initialize_utilities('test_gaussian')
+call initialize_utilities('test_gamma')
 call register_module(source,revision,revdate)
 
 write(*, *) ''
-write(*, *) 'sample size       input mean & std dev        computed std dev       diff       % diff '
+write(*, *) 'sample size     input k & t       computed mean, sd         actual mean, sd       diff mean         diff sd        % diff mean,  sd'
 write(*, *) ''
 
 do j=1, ntests
 
    call init_random_seq(r, 5)
 
-   mean = t(j)%t_mean
-   sd = t(j)%t_stddev
+   k = t(j)%t_kappa
+   h = t(j)%t_theta
    n = t(j)%t_nreps
 
    ! save all values in a file for post-plotting?
    write_this_one = (write_me .and. n <= write_limit)
 
    if (write_this_one) then
-      write(temp, "(A,F8.3,A,F8.3,A,I10)"), "gauss_", mean, "_", sd, "_", n
+      write(temp, "(A,F8.3,A,F8.3,A,I10)"), "gamma_", k, "_", h, "_", n
       call squeeze_out_blanks(temp, fname)
       f = open_file(fname)
    endif
 
    ! analytical values:
-   mean_sqdist = 0.0_r8
+   mean = k * h  
+   sd = sqrt(k * (h*h))
+
+   sum = 0.0_r8
+   sumsq = 0.0_r8
 
    do i = 1, n
-
-      r1 = random_gaussian(r, mean, sd)
+      r1 = random_gamma(r, k, h)
 
       if (write_this_one) write(f,*) r1
 
-      sqdist = (mean-r1)**2
-      mean_sqdist = mean_sqdist + sqdist
+      sum = sum + r1
+      sumsq = sumsq + (r1 - mean)**2
    end do
 
    if (write_this_one) call close_file(f)
 
    ! computed values:
-   compvar = mean_sqdist / n
-   compsd = sqrt(compvar)
-   compdiff = compsd - sd
+   compmean = sum / n
+   compsd = sqrt(sumsq/n)
 
-   write(*, formf) n, mean, sd, compsd, compdiff, abs(compdiff/sd) * 100._r8
+   ! differences
+   compdiffm  = compmean - mean
+   compdiffsd = compsd - sd
+
+   write(*, formf) n, k, h, mean, sd, compmean, compsd, compdiffm, compdiffsd, &
+                      abs(compdiffm/mean) * 100._r8, abs(compdiffsd/sd) * 100._r8
    
+
 enddo
 
 call finalize_utilities()
 
-end program test_gaussian
+end program test_gamma
 
 ! <next few lines under version control, do not edit>
 ! $URL$
--- developer_tests/random_seq/test_random.f90
+++ developer_tests/random_seq/test_random.f90
@@ -6,8 +6,14 @@
 
 program test_random
 
-use random_seq_mod, only : random_seq_type, init_random_seq, random_gaussian
-use  nag_wrap_mod, only : g05ddf_wrap
+! test the uniform random number generator routine
+
+use      types_mod, only : r8
+use  utilities_mod, only : register_module, &
+                           open_file, close_file, &
+                           initialize_utilities, finalize_utilities, &
+                           squeeze_out_blanks
+use random_seq_mod, only : random_seq_type, init_random_seq, random_uniform
 
 implicit none
 
@@ -17,41 +23,90 @@ character(len=256), parameter :: source   = &
 character(len=32 ), parameter :: revision = "$Revision$"
 character(len=128), parameter :: revdate  = "$Date$"
 
-integer :: i, n1, n2
-double precision :: rb(10), r2(10)
 type (random_seq_type) :: r
+integer :: i, j, n, f
+real(r8) :: r1, sum
+real(r8) :: mean, compmean, compdiffm
+logical  :: write_this_one
+character(len=50) :: formf = '(I12,4(F16.5))'
+character(len=256) :: fname, temp
 
-write(*, *) 'how many in first slice'
-read(*, *) n1
-write(*, *) 'how many in second slice'
-read(*, *) n2
+logical :: write_me = .true.       ! if true, write each distribution into a file for later diagnostics
+integer :: write_limit = 1000000   ! but only if rep count is not greater than this limit
+
+type t_inputs
+ integer  :: t_nreps
+end type
 
 
-call init_random_seq(r)
+! to add more tests or change the parameters, specify a test count
+! and update the sets of inputs here:
 
-! Have a background sequence underway
-do i = 1, n1
-   rb(i) = g05ddf_wrap(dble(0.0), dble(1.0))
-end do
+integer, parameter :: ntests = 11
+type(t_inputs) :: t(ntests) = (/ &
+    t_inputs(       100), &
+    t_inputs(       500), &
+    t_inputs(      1000), &
+    t_inputs(      5000), &
+    t_inputs(     10000), &
+    t_inputs(     50000), &
+    t_inputs(    100000), &
+    t_inputs(    500000), &
+    t_inputs(   1000000), &
+    t_inputs(   5000000), &
+    t_inputs(  10000000) /)
 
-! Also do a second repeatable sequence
-do i = 1, n2
-   r2(i) = random_gaussian(r, dble(0.0), dble(1.0))
-end do
 
-! Have a background sequence underway
-do i = n1 + 1, 10
-   rb(i) = g05ddf_wrap(dble(0.0), dble(1.0))
-end do
+call initialize_utilities('test_random')
+call register_module(source,revision,revdate)
 
-! Also do a second repeatable sequence
-do i = n2 + 1, 10
-   r2(i) = random_gaussian(r, dble(0.0), dble(1.0))
-end do
+write(*, *) ''
+write(*, *) 'sample size   computed mean     actual mean       diff mean     % diff mean'
+write(*, *) ''
 
-do i = 1, 10
-   write(*, *) i, rb(i), r2(i)
-end do
+do j=1, ntests
+
+   call init_random_seq(r, 5)
+
+   n = t(j)%t_nreps
+
+   ! save all values in a file for post-plotting?
+   write_this_one = (write_me .and. n <= write_limit)
+
+   if (write_this_one) then
+      write(temp, "(A,I10)"), "random_", n
+      call squeeze_out_blanks(temp, fname)
+      f = open_file(fname)
+   endif
+
+   ! analytical values:
+   mean = 0.5
+
+   sum = 0.0_r8
+
+   do i = 1, n
+      r1 = random_uniform(r)
+
+      if (write_this_one) write(f,*) r1
+
+      sum = sum + r1
+   end do
+
+   if (write_this_one) call close_file(f)
+
+   ! computed values:
+   compmean = sum / n
+
+   ! differences
+   compdiffm  = compmean - mean
+
+   write(*, formf) n, mean, compmean, compdiffm, &
+                   abs(compdiffm/mean) * 100._r8
+   
+
+enddo
+
+call finalize_utilities()
 
 end program test_random
 
