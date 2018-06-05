f5b09cfb19dbbd80590f90f8f959453e9c7e773e
Nancy Collins <nancy@ucar.edu>
2018-06-05 20:29:00 +0000

    update this on my changes branch.
    git-svn-id: https://svn-dares-dart.cgd.ucar.edu/DART/branches/rma_trunk@12633 dfa8782c-da17-4c45-ba5c-5625b50a00d6
    
    Former-commit-id: de82b721154c08509cef2eeae0c6ad4093b439cc

--- assimilation_code/modules/utilities/netcdf_utilities_mod.f90
+++ assimilation_code/modules/utilities/netcdf_utilities_mod.f90
@@ -6,21 +6,25 @@
 
 module netcdf_utilities_mod
 
-!>@todo FIXME: NEEDS MUCH MORE WORK.
+!> a module to streamline the code that calls netcdf routines. 
 !>
-!> start of a module to help streamline the code that
-!> calls netcdf routines.  usually adds error checking.
+!> calling code should NOT have to use the netcdf module, nor see
+!> any netcdf ID values, types, lengths, etc.  this code does error 
+!> checking and reports the offending call, filename, and user-supplied 
+!> context (often the 'routine' variable with the calling subroutine name).
 !> 
-!> also isolates as much access to the netcdf libs to routines
-!> inside this module.
+!> isolates access to the netcdf libs to routines inside this module.
 !>
-!> routines in this file are prefixed with nc_
+!> routines in this file are prefixed with nc_ and attempt to say
+!> in english what they do.  the intent is someone who does not know
+!> anything about netcdf can still use these.
 !>
-!> the intent is that these are simple routines that aren't trying to
-!> handle all possible options.  if you have complicated needs, write your
-!> own specialized routine, either here or in the calling code.
+!> these are simple routines that can't possibly handle all options.  
+!> if you have complicated needs, write your own specialized routine, 
+!> either here or in the calling code.
 !> for example, the 'put_var' routine here doesn't have start or count, 
 !> intentionally, for array entries.
+!>
 
 
 use types_mod, only : r4, r8, digits12, i2, i4, i8, PI, MISSING_R8, MISSING_I
@@ -37,6 +41,9 @@ public :: nc_check,                       &
           nc_add_attribute_to_variable,   &
           nc_get_attribute_from_variable, &
           nc_define_dimension,            &
+          nc_define_unlimited_dimension,  &
+          nc_get_dimension_size,          &
+          nc_define_character_variable,   &
           nc_define_integer_variable,     &
           nc_define_real_variable,        &
           nc_define_double_variable,      &
@@ -91,6 +98,11 @@ interface nc_get_attribute_from_variable
    module procedure nc_get_real_array_att_from_var
 end interface
 
+interface nc_define_character_variable
+   module procedure nc_define_var_char_1d
+   module procedure nc_define_var_char_Nd
+end interface
+
 interface nc_define_integer_variable
    module procedure nc_define_var_int_1d
    module procedure nc_define_var_int_Nd
@@ -107,10 +119,12 @@ interface nc_define_double_variable
 end interface
 
 interface nc_put_variable
+   module procedure nc_put_char_1d
    module procedure nc_put_single_int_1d
    module procedure nc_put_int_1d
    module procedure nc_put_single_real_1d
    module procedure nc_put_real_1d
+   module procedure nc_put_char_2d
    module procedure nc_put_int_2d
    module procedure nc_put_real_2d
    module procedure nc_put_int_3d
@@ -584,7 +598,7 @@ end subroutine nc_get_real_array_att_from_var
 
 !--------------------------------------------------------------------
 !--------------------------------------------------------------------
-! defining dimensions section
+! dimensions section
 
 subroutine nc_define_dimension(ncid, dimname, dimlen, context, filename)
 
@@ -603,6 +617,44 @@ call nc_check(ret, routine, 'define dimension '//trim(dimname), context, filenam
 end subroutine nc_define_dimension
 
 !--------------------------------------------------------------------
+
+subroutine nc_define_unlimited_dimension(ncid, dimname, context, filename)
+
+integer,          intent(in) :: ncid
+character(len=*), intent(in) :: dimname
+character(len=*), intent(in), optional :: context
+character(len=*), intent(in), optional :: filename
+
+character(len=*), parameter :: routine = 'nc_define_unlimited_dimension'
+integer :: ret, dimid
+
+ret = nf90_def_dim(ncid, dimname, NF90_UNLIMITED, dimid)
+call nc_check(ret, routine, 'define unlimited dimension '//trim(dimname), context, filename, ncid)
+
+end subroutine nc_define_unlimited_dimension
+
+!--------------------------------------------------------------------
+
+function nc_get_dimension_size(ncid, dimname, context, filename)
+
+integer,          intent(in) :: ncid
+character(len=*), intent(in) :: dimname
+character(len=*), intent(in), optional :: context
+character(len=*), intent(in), optional :: filename
+integer :: nc_get_dimension_size
+
+character(len=*), parameter :: routine = 'nc_get_dimension_size'
+integer :: ret, dimid
+
+ret = nf90_inq_dimid(ncid, dimname, dimid)
+call nc_check(ret, routine, 'inq dimid '//trim(dimname), context, filename, ncid)
+
+ret = nf90_inquire_dimension(ncid, dimid, len=nc_get_dimension_size)
+call nc_check(ret, routine, 'inquire dimension '//trim(dimname), context, filename, ncid)
+
+end function nc_get_dimension_size
+
+!--------------------------------------------------------------------
 !--------------------------------------------------------------------
 ! defining variables section
 
@@ -611,6 +663,74 @@ end subroutine nc_define_dimension
 !> the signatures (combinations of arguments) inseperable. 
 !> it's less common to define scalars in netcdf files, so those ones 
 !> get a separate entry point.
+
+!--------------------------------------------------------------------
+
+subroutine nc_define_var_char_1d(ncid, varname, dimname, context, filename)
+
+integer,          intent(in) :: ncid
+character(len=*), intent(in) :: varname
+character(len=*), intent(in) :: dimname
+character(len=*), intent(in), optional :: context
+character(len=*), intent(in), optional :: filename
+
+character(len=*), parameter :: routine = 'nc_define_var_char_1d'
+integer :: ret, dimid, varid
+
+ret = nf90_inq_dimid(ncid, dimname, dimid)
+call nc_check(ret, routine, 'inquire dimension id for dim '//trim(dimname), context, filename, ncid)
+
+ret = nf90_def_var(ncid, varname, nf90_char, dimid, varid)
+call nc_check(ret, routine, 'define character variable '//trim(varname), context, filename, ncid)
+
+end subroutine nc_define_var_char_1d
+
+!--------------------------------------------------------------------
+
+subroutine nc_define_var_char_Nd(ncid, varname, dimnames, context, filename)
+
+integer,          intent(in) :: ncid
+character(len=*), intent(in) :: varname
+character(len=*), intent(in) :: dimnames(:)
+character(len=*), intent(in), optional :: context
+character(len=*), intent(in), optional :: filename
+
+character(len=*), parameter :: routine = 'nc_define_var_char_Nd'
+integer :: ret, dimid1, dimid2, dimid3, varid
+
+if (size(dimnames) >= 1) then
+   ret = nf90_inq_dimid(ncid, dimnames(1), dimid1)
+   call nc_check(ret, routine, 'inquire dimension id for dim '//trim(dimnames(1)), context, filename, ncid)
+endif
+
+if (size(dimnames) >= 2) then
+   ret = nf90_inq_dimid(ncid, dimnames(2), dimid2)
+   call nc_check(ret, routine, 'inquire dimension id for dim '//trim(dimnames(2)), context, filename, ncid)
+endif
+
+if (size(dimnames) >= 3) then
+   ret = nf90_inq_dimid(ncid, dimnames(3), dimid3)
+   call nc_check(ret, routine, 'inquire dimension id for dim '//trim(dimnames(3)), context, filename, ncid)
+endif
+
+if (size(dimnames) >= 4) then
+   call error_handler(E_ERR, routine, 'only 1d, 2d and 3d character variables supported', &
+                      source, revision, revdate, text2='variable '//trim(varname))
+endif
+
+if (size(dimnames) == 1) then
+   ret = nf90_def_var(ncid, varname, nf90_char, dimid1, varid=varid)
+else if (size(dimnames) == 2) then
+   ret = nf90_def_var(ncid, varname, nf90_char, dimids=(/ dimid1, dimid2 /), varid=varid)
+else if (size(dimnames) == 3) then
+   ret = nf90_def_var(ncid, varname, nf90_char, dimids=(/ dimid1, dimid2, dimid3 /), varid=varid)
+endif
+
+call nc_check(ret, routine, 'define character variable '//trim(varname), context, filename, ncid)
+
+end subroutine nc_define_var_char_Nd
+
+!--------------------------------------------------------------------
 
 subroutine nc_define_integer_scalar(ncid, varname, context, filename)
 
@@ -935,6 +1055,28 @@ end function nc_variable_exists
 !--------------------------------------------------------------------
 ! put values into variables
 
+
+subroutine nc_put_char_1d(ncid, varname, varvals, context, filename)
+
+integer,          intent(in) :: ncid
+character(len=*), intent(in) :: varname
+character(len=*), intent(in) :: varvals
+character(len=*), intent(in), optional :: context
+character(len=*), intent(in), optional :: filename
+
+character(len=*), parameter :: routine = 'nc_put_char_1d'
+integer :: ret, varid
+
+ret = nf90_inq_varid(ncid, varname, varid)
+call nc_check(ret, routine, 'inquire variable id for '//trim(varname), context, filename, ncid)
+
+ret = nf90_put_var(ncid, varid, varvals)
+call nc_check(ret, routine, 'put values for '//trim(varname), context, filename, ncid)
+
+end subroutine nc_put_char_1d
+
+!--------------------------------------------------------------------
+
 subroutine nc_put_single_int_1d(ncid, varname, varindex, varval, context, filename)
 
 integer,          intent(in) :: ncid
@@ -1018,6 +1160,27 @@ ret = nf90_put_var(ncid, varid, varvals)
 call nc_check(ret, routine, 'put values for '//trim(varname), context, filename, ncid)
 
 end subroutine nc_put_real_1d
+
+!--------------------------------------------------------------------
+
+subroutine nc_put_char_2d(ncid, varname, varvals, context, filename)
+
+integer,          intent(in) :: ncid
+character(len=*), intent(in) :: varname
+character(len=*), intent(in) :: varvals(:)
+character(len=*), intent(in), optional :: context
+character(len=*), intent(in), optional :: filename
+
+character(len=*), parameter :: routine = 'nc_put_char_2d'
+integer :: ret, varid
+
+ret = nf90_inq_varid(ncid, varname, varid)
+call nc_check(ret, routine, 'inquire variable id for '//trim(varname), context, filename, ncid)
+
+ret = nf90_put_var(ncid, varid, varvals)
+call nc_check(ret, routine, 'put values for '//trim(varname), context, filename, ncid)
+
+end subroutine nc_put_char_2d
 
 !--------------------------------------------------------------------
 
