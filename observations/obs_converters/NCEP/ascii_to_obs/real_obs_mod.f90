271947343a7a7e7e5064feb01ce347323d294f5d
Jeff Steward <55454880+jlsucar@users.noreply.github.com>
2019-10-24 15:41:05 -0600

    Checking the length of the obsfile before setting

--- observations/obs_converters/NCEP/ascii_to_obs/real_obs_mod.f90
+++ observations/obs_converters/NCEP/ascii_to_obs/real_obs_mod.f90
@@ -147,6 +147,7 @@ character(len=32) :: skip_reasons(num_fail_kinds) = (/ &
 
 
 integer :: obs_unit
+integer :: obs_file_len
 integer :: obs_prof, obs_kind, obs_kind_gen, which_vert, iqc, obstype, pc
 real (r8) :: obs_err, lon, lat, lev, zob, time, rcount, zob2
 real (r8) :: vloc, obs_value, aqc
@@ -200,6 +201,14 @@ call error_handler(E_MSG,'real_obs_sequence',msgstring1)
 ! open NCEP observation data file
 
 write(obsdate, '(i4.4,i2.2,i2.2)') year, month, day
+
+obs_file_len = len(trim(adjustl(ObsBase))) + len(obsdate) + len(hourt)
+
+if (obs_file_len > len(obsfile)) then
+   write(msgstring1,'(A,I0)') 'ObsFile string length too long: ',obs_file_len
+   call error_handler(E_ERR,'real_obs_sequence',msgstring1)
+endif
+
 obsfile  = trim(adjustl(ObsBase))//obsdate//hourt
 obs_unit = open_file(obsfile, form='formatted', action='read')
 write(msgstring1,*) 'input file opened= "'//trim(obsfile)//'"'
