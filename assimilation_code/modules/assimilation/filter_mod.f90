1510513883708e2c485006b97e3e001680a288a7
Tim Hoar <thoar@ucar.edu>
2020-06-17 15:28:05 -0600

    confirmed that we are not allocating a 'model_size' array.
    The 'domain_length' variable is the size of all the state elements
    that have been distributed TO THAT TASK.

--- assimilation_code/modules/assimilation/filter_mod.f90
+++ assimilation_code/modules/assimilation/filter_mod.f90
@@ -2042,20 +2042,18 @@ type(ensemble_type), intent(inout) :: ens_handle
 integer               :: i
 logical               :: interf_provided ! model does the perturbing
 logical, allocatable  :: miss_me(:)
-integer               :: domain_length
-
+integer               :: domain_length ! the number of elements ON THIS TASK
 
 ! Copy from ensemble member 1 to the other copies
 do i = 1, ens_handle%my_num_vars
    ens_handle%copies(2:ens_size, i) = ens_handle%copies(1, i)  ! How slow is this?
 enddo
 
-domain_length = size(ens_handle%copies,2)
-
 ! If the state allows missing values, we have to record their locations
 ! and restore them in all the new perturbed copies.
 
 if (get_missing_ok_status()) then
+   domain_length = size(ens_handle%copies,2)
    allocate(miss_me(domain_length))
    miss_me = .false.
    where(ens_handle%copies(1, :) == missing_r8) miss_me = .true.
