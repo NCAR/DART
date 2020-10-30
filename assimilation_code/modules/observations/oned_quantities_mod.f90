e003bb2603734e7514d96ef669a9981d9aa51a4c
Nancy Collins <nancy@ucar.edu>
2020-10-30 11:54:28 -0600

    modify comment above QTY_STATE_VARIABLE line to explain more
    expand comment to say why it is listed in this file when it is
    already automatically entered by the preprocess program.
    also say it has to be the first line and not modified in any way.

--- assimilation_code/modules/observations/oned_quantities_mod.f90
+++ assimilation_code/modules/observations/oned_quantities_mod.f90
@@ -3,20 +3,24 @@
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
 !
 
-! ! in this section, define the quantity of interest.  it must
+! ! in this section, define the quantities of interest. the name must
 ! ! start with QTY_xxx and be less than 32 characters total.
 ! ! if there are units, min/max values, pdf, etc you can add one or
 ! ! more name=value pairs.  at this point there *cannot* be spaces
 ! ! around the equals sign.  the value can have spaces if it is
 ! ! surrounded by double quotes.  e.g. desc="assumes dry air density"
+! !
 ! ! comment lines (like these) can be added if they start with a second !
  
 ! BEGIN DART PREPROCESS QUANTITY DEFINITIONS
 !
-!  ! QTY_STATE_VARIABLE is predefined in the preprocess program.
-!  ! please do not change this first entry.
+!  ! QTY_STATE_VARIABLE is also predefined in the preprocess program.
+!  ! it is being repeated here because for some classes of models
+!  ! this is the only quantity that they will use.  it must be the
+!  ! the first entry in this file and must not be altered.
 !
 !  QTY_STATE_VARIABLE               desc="basic item in a state"
+! 
 !  QTY_1D_INTEGRAL                  desc="compute value with an integral"
 !  QTY_STATE_VAR_POWER     pdf=log  desc="raising a state value to a power"
 !  QTY_LARGE_SCALE_STATE            desc="state varies with large time/space scale"
