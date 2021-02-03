698ed2f4dc666973ed8bf639999b9db172be03e7
Tim Hoar <thoar@ucar.edu>
2021-02-03 14:38:16 -0700

    Removed unused quantities modules from COSMOS converters

--- observations/obs_converters/COSMOS/COSMOS_development.f90
+++ observations/obs_converters/COSMOS/COSMOS_development.f90
@@ -1,8 +1,6 @@
 ! DART software - Copyright UCAR. This open source software is provided
 ! by UCAR, "as is", without charge, subject to all terms of use at
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
-!
-! $Id$
 
 program COSMOS_development
 
@@ -60,10 +58,9 @@ use netcdf
 implicit none
 
 ! version controlled file description for error handling, do not edit
-character(len=256), parameter :: source   = &
-   "$URL$"
-character(len=32 ), parameter :: revision = "$Revision$"
-character(len=128), parameter :: revdate  = "$Date$"
+character(len=*), parameter :: source   = 'COSMOS_development.f90'
+character(len=*), parameter :: revision = ''
+character(len=*), parameter :: revdate  = ''
 
 !-----------------------------------------------------------------------
 ! Namelist with default values
@@ -598,8 +595,3 @@ end function find_site_index
 
 end program COSMOS_development
 
-! <next few lines under version control, do not edit>
-! $URL$
-! $Id$
-! $Revision$
-! $Date$
