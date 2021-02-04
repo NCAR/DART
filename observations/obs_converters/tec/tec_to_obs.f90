11878a52e01cb0b12a7c15c6d6bcc1455ff22f57
Tim Hoar <thoar@ucar.edu>
2021-02-03 19:37:50 -0700

    Removing unused quantity modules from tec converter
    This converter seems to be more of an example than anything.

--- observations/obs_converters/tec/tec_to_obs.f90
+++ observations/obs_converters/tec/tec_to_obs.f90
@@ -1,8 +1,6 @@
 ! DART software - Copyright UCAR. This open source software is provided
 ! by UCAR, "as is", without charge, subject to all terms of use at
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
-!
-! $Id$
 
 ! An example program to create DART format observations from a tec input file
 !
@@ -276,8 +274,3 @@ call finalize_utilities()
 
 end program tec_to_obs
 
-! <next few lines under version control, do not edit>
-! $URL$
-! $Id$
-! $Revision$
-! $Date$
