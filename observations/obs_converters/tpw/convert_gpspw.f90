fdbdea0e4b1963755f8ef979f0ff46212122018d
Tim Hoar <thoar@ucar.edu>
2021-02-03 19:58:12 -0700

    Removed unused quantity modules from TPW converters

--- observations/obs_converters/tpw/convert_gpspw.f90
+++ observations/obs_converters/tpw/convert_gpspw.f90
@@ -1,8 +1,6 @@
 ! DART software - Copyright UCAR. This open source software is provided
 ! by UCAR, "as is", without charge, subject to all terms of use at
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
-!
-! $Id$
 
 program convert_gpspw
 
@@ -63,10 +61,9 @@ use           netcdf
 implicit none
 
 ! version controlled file description for error handling, do not edit
-character(len=256), parameter :: source   = &
- "$URL$"
-character(len=32 ), parameter :: revision = "$Revision$"
-character(len=128), parameter :: revdate  = "$Date$"
+character(len=*), parameter :: source   = 'convert_gpspw.f90'
+character(len=*), parameter :: revision = ''
+character(len=*), parameter :: revdate  = ''
 
 !--------------------------------------
 
@@ -91,9 +88,10 @@ real(r8) :: lon, lat, hght, tpw
 
 logical  :: first_obs, lon_between
 
-character(len = 4)   :: stnid, locid
-character(len = 19)  :: obsdate, indate
-character(len = 256) :: msgstring, infile, outfile
+character(len=4)   :: stnid, locid
+character(len=19)  :: obsdate, indate
+character(len=256) :: infile, outfile
+character(len=512) :: msgstring
 
 ! items in namelist, along with default values
 integer  :: start_year  = 2008
@@ -287,8 +285,3 @@ call finalize_utilities()
 
 end program
 
-! <next few lines under version control, do not edit>
-! $URL$
-! $Id$
-! $Revision$
-! $Date$
