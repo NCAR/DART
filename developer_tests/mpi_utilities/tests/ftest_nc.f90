a7dc0fbe429fe8941001226e6b0d0c48d4674ee3
Nancy Collins <nancy@ucar.edu>
2021-08-06 09:32:31 -0600

    remove remaining SVN stuff

--- developer_tests/mpi_utilities/tests/ftest_nc.f90
+++ developer_tests/mpi_utilities/tests/ftest_nc.f90
@@ -1,8 +1,6 @@
 ! DART software - Copyright UCAR. This open source software is provided
 ! by UCAR, "as is", without charge, subject to all terms of use at
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
-!
-! $Id$
 
 program ftest_nc
 
@@ -127,8 +125,3 @@ end subroutine netcdf_error_exit
 
 end program ftest_nc
 
-! <next few lines under version control, do not edit>
-! $URL$
-! $Id$
-! $Revision$
-! $Date$
