f10654849604b7f5d72faa2da11408e5e94d856a
Helen Kershaw <hkershaw@ucar.edu>
2021-09-30 10:28:57 -0600

    changelog and conf.py bump for release

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ copyright = '2021, University Corporation for Atmospheric Research'
 author = 'Data Assimilation Research Section'
 
 # The full version, including alpha/beta/rc tags
-release = '9.11.10'
+release = '9.11.11'
 master_doc = 'README'
 
 # -- General configuration ---------------------------------------------------
