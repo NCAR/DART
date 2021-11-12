805dcf76746ffa05594a16ba9dc7cdd5fdcef422
Helen Kershaw <hkershaw@ucar.edu>
2021-11-12 09:05:11 -0700

    mkmf.template defined in mkmf (or supplied by -t)
    preprocess builds

--- build_templates/buildfunctions.sh
+++ build_templates/buildfunctions.sh
@@ -154,6 +154,8 @@ function modelbuild() {
 #-------------------------
 function buildpreprocess() {
 
+ local pp_dir=$DART/assimilation_code/programs/preprocess
+
  # run preprocess if it is in the current directory
  if [ -f preprocess ]; then
    echo "already there"
@@ -162,19 +164,19 @@ function buildpreprocess() {
  fi
 
 # link to preprocess if it is already built
-if [ -f $DART/src/programs/preprocess/preprocess ]; then
+if [ -f $pp_dir/preprocess ]; then
    echo "not there but built"
-   ln -s $DART/src/programs/preprocess/preprocess .
+   ln -s $pp_dir/preprocess .
    ./preprocess 
    return
 fi
 
  # build preproces
- cd $DART/src/programs/preprocess
- $DART/build_templates/mkmf -x -p $DART/src/programs/preprocess/preprocess \
-      -a $DART $DART/src/programs/preprocess/path_names_preprocess
+ cd $pp_dir
+ $DART/build_templates/mkmf -x -p $pp_dir/preprocess \
+      -a $DART $pp_dir/path_names_preprocess
  cd -
- ln -s $DART/src/programs/preprocess/preprocess .
+ ln -s $pp_dir/preprocess .
  ./preprocess
 }
 
