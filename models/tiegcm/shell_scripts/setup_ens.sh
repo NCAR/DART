40283dcbb4b9f2c905a02c66b1606b91c13a004d
Helen Kershaw <hkershaw@ucar.edu>
2022-02-28 14:27:45 -0700

    bug-fix for setup_ens. Start loop at correct value and use rsync vs copy so you can blast the member directories

--- models/tiegcm/shell_scripts/setup_ens.sh
+++ models/tiegcm/shell_scripts/setup_ens.sh
@@ -3,9 +3,8 @@
 
 ens_size=5
 
-for (( i==1; i<=$ens_size; i++))
+for (( i=1; i<=$ens_size; i++))
 do
   mem="mem"$(printf "%02d" $i)
-  cp -r mem.setup $mem
-
+  rsync -a --delete  mem.setup/ $mem
 done
