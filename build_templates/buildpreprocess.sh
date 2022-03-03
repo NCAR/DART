ace64fb3b3c741699533f439bc9145972f63ccb2
Helen Kershaw <hkershaw@ucar.edu>
2022-03-02 19:45:55 -0700

    remove print statements

--- build_templates/buildpreprocess.sh
+++ build_templates/buildpreprocess.sh
@@ -11,20 +11,18 @@ function buildpreprocess() {
 
  # run preprocess if it is in the current directory
  if [ -f preprocess ]; then
-   echo "already there"
    ./preprocess
    return
  fi
 
-# link to preprocess if it is already built
+# link to preprocess if it is already built, run
 if [ -f $pp_dir/preprocess ]; then
-   echo "not there but built"
    ln -s $pp_dir/preprocess .
    ./preprocess 
    return
 fi
 
- # build preproces
+ # build preprocess, link, run
  cd $pp_dir
  $DART/build_templates/mkmf -x -p $pp_dir/preprocess \
       -a $DART $pp_dir/path_names_preprocess
