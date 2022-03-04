daa915a92cfe7e1fc60929e6b6f4127dc9d7afa8
Helen Kershaw <hkershaw@ucar.edu>
2022-03-04 13:25:44 -0700

    build test utilities

---
+++ developer_tests/utilities/work/quickbuild.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+main() {
+
+
+[ -z "$DART" ] && echo "ERROR: Must set DART environment variable" && exit 9
+source "$DART"/build_templates/buildfunctions.sh
+
+MODEL="template"
+LOCATION="threed_sphere"
+dev_test=1
+TEST="utilities"
+
+serial_programs=( \
+PrecisionCheck \
+error_handler_test \
+file_utils_test \
+find_enclosing_indices_test \
+find_first_occurrence_test \
+nml_test \
+parse_args_test \
+sort_test \
+stacktest
+)
+
+
+# quickbuild arguments
+arguments "$@"
+
+# clean the directory
+\rm -f -- *.o *.mod Makefile .cppdefs
+
+# preprocess not needed for these tests
+
+# build 
+buildit
+
+# clean up
+\rm -f -- *.o *.mod
+
+}
+
+main "$@"
