6400bd904bcb929f0ed6c6bf7dcd65ae13e8c1d1
Helen Kershaw <hkershaw@ucar.edu>
2022-03-04 11:13:29 -0700

    quickbuild random_seq

---
+++ developer_tests/random_seq/work/quickbuild.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+main() {
+
+
+[ -z "$DART" ] && echo "ERROR: Must set DART environment variable" && exit 9
+source "$DART"/build_templates/buildfunctions.sh
+
+MODEL="template"
+LOCATION="threed_sphere"
+dev_test=1
+TEST="random_seq"
+
+serial_programs=( \
+test_corr \
+test_diff \
+test_exp \
+test_gamma \
+test_gaussian \
+test_hist \
+test_inv_gamma \
+test_random \
+test_reseed
+)
+
+
+# quickbuild arguments
+arguments "$@"
+
+# clean the directory
+\rm -f -- *.o *.mod Makefile .cppdefs
+
+# preprocess not needed for these tests
+
+# build 
+buildit
+
+# clean up
+\rm -f -- *.o *.mod
+
+}
+
+main "$@"
