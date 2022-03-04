dcca3f2483c7fe1ff0fc3ec49008a347313b2259
Helen Kershaw <hkershaw@ucar.edu>
2022-03-03 20:41:19 -0700

    compile everything in DART at once.
    May need to build preprocess once at the start of run_all_quickbuilds.sh

---
+++ developer_tests/build_everything/run_all_quickbuilds.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# options to set
+#   1. DART directory
+#   2. compiler
+
+export DART=/Users/hkershaw/DART/Refactor/Build_system/DART.build-all
+FC=intel
+
+
+if [[ ! -d $DART ]] ; then 
+  echo "No DART directory: " $DART
+  exit 1
+fi 
+
+cp mkmf.template.compile_all $DART/build_templates/mkmf.template
+cd $DART
+
+# run fixsystem 
+# running this once at the beginning otherwise all make commands will
+# try and alter the mpi_*_utilities_mod.f90 simultaneously
+cd assimilation_code/modules/utilities; ./fixsystem $FC
+cd -
+
+# local versions of obs_def_mod.f90 and obs_kind_mod.f90
+find . -name input.nml -exec sed -i '' -e "/^[[:space:]]*#/! s|.*output_obs_def_mod_file.*|output_obs_def_mod_file = './obs_def_mod.f90'|g" \
+      -e "/^[[:space:]]*#/! s|.*output_obs_qty_mod_file.*|output_obs_qty_mod_file = './obs_kind_mod.f90'|g" \
+      -e "/^[[:space:]]*#/! s|.*output_obs_kind_mod_file.*|output_obs_qty_mod_file = './obs_kind_mod.f90'|g" {} \;  
+
+my_dir=$(pwd)
+pids=()
+dirs=()
+status=()
+
+while read f; do
+
+cd $f; ./quickbuild.sh & 
+pids+=( "$!" )
+dirs+=( "$f" )
+cd $my_dir
+
+done < $DART/developer_tests/build_everything/all_quickbuilds
+
+for pid in ${pids[@]}; do
+  #echo "${pid}"
+  wait ${pid}
+  status+=( "$?" )
+done
+
+# looping through the status arr to check exit code for each
+i=0
+for st in ${status[@]}; do
+    if [[ ${st} -ne 0 ]]; then
+        echo "$i ${dirs[$i]} failed"
+        OVERALL_EXIT=1
+    else
+        echo "$i  ${dirs[$i]} finished"
+    fi
+    ((i+=1))
+done
+
