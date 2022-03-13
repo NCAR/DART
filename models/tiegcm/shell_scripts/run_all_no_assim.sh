9409dabbf0c0dce0316164f397e55fca22fa88b8
Helen Kershaw <hkershaw@ucar.edu>
2022-03-12 17:12:14 -0700

    scripts for cycling with assimilation on Cheyenne

--- models/tiegcm/shell_scripts/run_all_no_assim.sh
+++ models/tiegcm/shell_scripts/run_all_no_assim.sh
@@ -3,7 +3,6 @@
 # To check exit code for each arry job 
 # qhist -j 2667681 -l
 
-
 num_cycles=25
 MODEL_RUNS=$(qsub run5.0.pbs)
 
@@ -11,7 +10,8 @@ echo "Submitted " $MODEL_RUNS
 
 for (( i=1; i<$num_cycles; i++))
 do
-  FILTER_RUN=$(qsub -W depend=afterok:$MODEL_RUNS submit_filter.pbs)
-  MODEL_RUNS=$(qsub -W depend=afterok:$FILTER_RUN run5.0.pbs)
-  echo "Submitted cycle" $i
+  MODEL_RUNS=$(qsub -W depend=afterok:$MODEL_RUNS run5.0.pbs)
+  echo "Submitted " $MODEL_RUNS
 done
+
+
