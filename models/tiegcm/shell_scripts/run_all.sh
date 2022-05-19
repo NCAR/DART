f352f7316f1a641f72d32351c6fc087731615291
Helen Kershaw <hkershaw@ucar.edu>
2022-05-19 13:23:42 -0600

    run number of cycles (not n - 1)

--- models/tiegcm/shell_scripts/run_all.sh
+++ models/tiegcm/shell_scripts/run_all.sh
@@ -9,7 +9,7 @@ MODEL_RUNS=$(qsub run5.0.pbs)
 
 echo "Submitted " $MODEL_RUNS
 
-for (( i=1; i<$num_cycles; i++))
+for (( i=1; i<=$num_cycles; i++))
 do
   FILTER_RUN=$(qsub -W depend=afterok:$MODEL_RUNS submit_filter.pbs)
   MODEL_RUNS=$(qsub -W depend=afterok:$FILTER_RUN run5.0.pbs)
