825b5f437f4094cec37ff1300861b60d74d493ca
Helen Kershaw <hkershaw@ucar.edu>
2022-08-05 20:35:04 -0600

    bug-fix: remove obs_utilites_mod from model buildfunctions.sh
    add to wrf_hydro quickbuild.sh
    fixes #383

--- models/wrf_hydro/work/quickbuild.sh
+++ models/wrf_hydro/work/quickbuild.sh
@@ -11,7 +11,8 @@ source "$DART"/build_templates/buildfunctions.sh
 
 MODEL=wrf_hydro
 LOCATION=threed_sphere
-EXTRA="$DART/models/wrf/module_map_utils.f90"
+EXTRA="$DART/models/wrf/module_map_utils.f90 \
+       $DART/observations/obs_converters/utilities/obs_utilities_mod.f90"
 
 
 programs=(
