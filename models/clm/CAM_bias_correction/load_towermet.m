7fb4afd2220d6c77c36236deb168a9199dbab8f3
Helen Kershaw <hkershaw@ucar.edu>
2023-05-08 08:49:59 -0400

    style: auto indent with Matlab

--- models/clm/CAM_bias_correction/load_towermet.m
+++ models/clm/CAM_bias_correction/load_towermet.m
@@ -5,10 +5,10 @@ function [tower_main] = load_towermet(metvar,years,path)
 % Output: 'tower_main' is concatenated met data set
 tower_main=[];
 
-	for i = 1:length(years);
-	    tower_dummy=squeeze(ncread([path 'Forcing_Tower_US-NR1_' years{i} '_ver.2022.11.29.nc'],metvar));
-	    tower_main=[tower_main;tower_dummy];
-	    clear tower_dummy 
-	end
+for i = 1:length(years);
+    tower_dummy=squeeze(ncread([path 'Forcing_Tower_US-NR1_' years{i} '_ver.2022.11.29.nc'],metvar));
+    tower_main=[tower_main;tower_dummy];
+    clear tower_dummy
+end
 
 end
