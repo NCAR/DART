5c1189468cfa413c6cb8c78a34a16fb01e38ca58
Helen Kershaw <hkershaw@ucar.edu>
2023-05-08 10:16:25 -0400

    doc: fix matlab help

--- models/clm/CAM_bias_correction/load_towermet.m
+++ models/clm/CAM_bias_correction/load_towermet.m
@@ -1,6 +1,6 @@
 function [tower_main] = load_towermet(metvar,years,path)
 % function load_towermet: loads all years of tower met forcing (PLUMBER2 format)
-% Input:  'metvar' is PLUMBER2 met data name; 'yeartower' is array of tower years to load
+% Input:  'metvar' is PLUMBER2 met data name; 'years' is array of tower years to load
 %         'path' is path to the PLUMBER2 met data
 % Output: 'tower_main' is concatenated met data set
 tower_main=[];
