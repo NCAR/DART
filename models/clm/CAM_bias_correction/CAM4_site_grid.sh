70107b35cbc89ba146f6dfde936b84cdffe42dee
Helen Kershaw <hkershaw@ucar.edu>
2023-05-08 09:43:57 -0400

    style: bash indenting

--- models/clm/CAM_bias_correction/CAM4_site_grid.sh
+++ models/clm/CAM_bias_correction/CAM4_site_grid.sh
@@ -17,31 +17,31 @@ for YEAR in {1997..2010..1}
 
 do
 
-echo "  "
-echo "entering YEAR loop where value of YEAR is:"
-echo ${YEAR}
-echo "  "
+   echo "  "
+   echo "entering YEAR loop where value of YEAR is:"
+   echo ${YEAR}
+   echo "  "
    for NINST in {0001..0080..1}  # Will create 0001, 0002, ... 0080
    do
-echo "  "
-echo "entering NINST loop where value of YEAR is:"
-echo ${NINST}
-echo "  "  
+      echo "  "
+      echo "entering NINST loop where value of YEAR is:"
+      echo ${NINST}
+      echo "  "  
  
-   #Create output directory if necessary
-   OUTDIR=${CAM4_site_path}${NINST}
-   if [[ ! -d ${OUTDIR} ]] ; then
-        mkdir ${OUTDIR}
-   fi
-  
-   
-   ncks -d doma_nx,102,102 -d doma_ny,69,69 -d a2x6h_nx,102,102 -d a2x6h_ny,69,69 \
-         ${CAM4_grid_path}/CAM_DATM.cpl_${NINST}.ha2x1dx6h.${YEAR}.nc \
-         ${CAM4_site_path}${NINST}/CAM4_NR1.cpl_${NINST}.ha2x1dx6h.${YEAR}.nc
- 
-   echo "  "
-   echo "Completed CAM4 NR1 extract"
-   echo "  "
+      #Create output directory if necessary
+      OUTDIR=${CAM4_site_path}${NINST}
+      if [[ ! -d ${OUTDIR} ]] ; then
+           mkdir ${OUTDIR}
+      fi
+     
+      
+      ncks -d doma_nx,102,102 -d doma_ny,69,69 -d a2x6h_nx,102,102 -d a2x6h_ny,69,69 \
+            ${CAM4_grid_path}/CAM_DATM.cpl_${NINST}.ha2x1dx6h.${YEAR}.nc \
+            ${CAM4_site_path}${NINST}/CAM4_NR1.cpl_${NINST}.ha2x1dx6h.${YEAR}.nc
+    
+      echo "  "
+      echo "Completed CAM4 NR1 extract"
+      echo "  "
 
    done
 done
--- models/clm/CAM_bias_correction/CAM6_site_grid.sh
+++ models/clm/CAM_bias_correction/CAM6_site_grid.sh
@@ -16,47 +16,47 @@ CAM6_site_path="/glade/work/bmraczka/CAM6_NR1/"
 for YEAR in {2011..2020..1}
 do
 
-echo "  "
-echo "entering YEAR loop where value of YEAR is:"
-echo ${YEAR}
-echo "  "
+   echo "  "
+   echo "entering YEAR loop where value of YEAR is:"
+   echo ${YEAR}
+   echo "  "
    for NINST in {0001..0080..1}  # Will create 0001, 0002, ... 0080
    do
-echo "  "
-echo "entering NINST loop where value of YEAR is:"
-echo ${NINST}
-echo "  "  
- 
-   #Create output directory if necessary
-   OUTDIR=${CAM6_site_path}${NINST}
-   if [[ ! -d ${OUTDIR} ]] ; then
-        mkdir ${OUTDIR}
-   fi
-  
-   
-   # SOLAR
-   ncks -d doma_nx,204,204 -d doma_ny,138,138 -d a2x1hi_nx,204,204 -d a2x1hi_ny,138,138 \
-         ${CAM6_grid_path}${NINST}/f.e21.FHIST_BGC.f09_025.CAM6assim.011.cpl_${NINST}.ha2x1hi.${YEAR}.nc \
-         ${CAM6_site_path}${NINST}/CAM6_NR1.cpl_${NINST}.ha2x1hi.${YEAR}.nc
-   echo "  "
-   echo "Finished CAM6 SOLAR, 1/3 completed"
-   echo "  "
- 
-   # NON-SOLAR                                       
-   ncks -d doma_nx,204,204 -d doma_ny,138,138 -d a2x3h_nx,204,204 -d a2x3h_ny,138,138 \
-         ${CAM6_grid_path}${NINST}/f.e21.FHIST_BGC.f09_025.CAM6assim.011.cpl_${NINST}.ha2x3h.${YEAR}.nc \
-         ${CAM6_site_path}${NINST}/CAM6_NR1.cpl_${NINST}.ha2x3h.${YEAR}.nc
+      echo "  "
+      echo "entering NINST loop where value of YEAR is:"
+      echo ${NINST}
+      echo "  "  
 
-   echo "  "
-   echo "Finished CAM6 NON-SOLAR, 2/3 completed"
-   echo "  "
-   # 1 hr STATE
-   ncks -d doma_nx,204,204 -d doma_ny,138,138 -d a2x1h_nx,204,204 -d a2x1h_ny,138,138 \
-         ${CAM6_grid_path}${NINST}/f.e21.FHIST_BGC.f09_025.CAM6assim.011.cpl_${NINST}.ha2x1h.${YEAR}.nc \
-         ${CAM6_site_path}${NINST}/CAM6_NR1.cpl_${NINST}.ha2x1h.${YEAR}.nc
-   echo "  "
-   echo "Finished CAM6 1 hr STATE, 3/3 completed"
-   echo "  "
+      #Create output directory if necessary
+      OUTDIR=${CAM6_site_path}${NINST}
+      if [[ ! -d ${OUTDIR} ]] ; then
+           mkdir ${OUTDIR}
+      fi
+
+
+      # SOLAR
+      ncks -d doma_nx,204,204 -d doma_ny,138,138 -d a2x1hi_nx,204,204 -d a2x1hi_ny,138,138 \
+            ${CAM6_grid_path}${NINST}/f.e21.FHIST_BGC.f09_025.CAM6assim.011.cpl_${NINST}.ha2x1hi.${YEAR}.nc \
+            ${CAM6_site_path}${NINST}/CAM6_NR1.cpl_${NINST}.ha2x1hi.${YEAR}.nc
+      echo "  "
+      echo "Finished CAM6 SOLAR, 1/3 completed"
+      echo "  "
+
+      # NON-SOLAR                                       
+      ncks -d doma_nx,204,204 -d doma_ny,138,138 -d a2x3h_nx,204,204 -d a2x3h_ny,138,138 \
+            ${CAM6_grid_path}${NINST}/f.e21.FHIST_BGC.f09_025.CAM6assim.011.cpl_${NINST}.ha2x3h.${YEAR}.nc \
+            ${CAM6_site_path}${NINST}/CAM6_NR1.cpl_${NINST}.ha2x3h.${YEAR}.nc
+
+      echo "  "
+      echo "Finished CAM6 NON-SOLAR, 2/3 completed"
+      echo "  "
+      # 1 hr STATE
+      ncks -d doma_nx,204,204 -d doma_ny,138,138 -d a2x1h_nx,204,204 -d a2x1h_ny,138,138 \
+            ${CAM6_grid_path}${NINST}/f.e21.FHIST_BGC.f09_025.CAM6assim.011.cpl_${NINST}.ha2x1h.${YEAR}.nc \
+            ${CAM6_site_path}${NINST}/CAM6_NR1.cpl_${NINST}.ha2x1h.${YEAR}.nc
+      echo "  "
+      echo "Finished CAM6 1 hr STATE, 3/3 completed"
+      echo "  "
 
    done
 done
