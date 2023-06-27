a47b523365bcd4b102998b6f8f801d54a4dfa566
Nancy Collins <nancy@ucar.edu>
2023-06-27 16:40:57 -0600

    make the error messages more helpful
    suggest why an error is happening, both for the model name
    and for the location choices.

--- models/new_model.sh
+++ models/new_model.sh
@@ -4,19 +4,20 @@
 # by UCAR, "as is", without charge, subject to all terms of use at
 # http://www.image.ucar.edu/DAReS/DART/DART_download
 
-function usage { echo "Usage: $0 [model_name] [location_mod]"; }
+function usage { echo "Usage: $0 [model_name] [location_choice]"; }
 
 model_name=$1
-location_mod=$2
+location_choice=$2
 
 if [[ ($# -le 1) ]]; then
   usage
   exit 0
 elif [[ -d "$model_name" ]]; then
-  echo "$model_name already exists! Please try a different model name."
+  echo "A directory called $model_name already exists! Please try a different model name."
   exit 1
-elif [[ ! -d "../assimilation_code/location/$location_mod" ]]; then
-  echo "$location_mod is not a proper location module! Please try a different location name."
+elif [[ ! -d "../assimilation_code/location/$location_choice" ]]; then
+  echo "$location_choice is not a valid location name! Please try a different location name."
+  echo "Available location names are located in ../assimilation_code/location"
   exit 1
 fi
 
@@ -26,7 +27,7 @@ sed -i '' -e "s;template_model;$model_name;" "$model_name/readme.rst" 2>/dev/nul
 cp "template/work/quickbuild.sh" "$model_name/work/quickbuild.sh"
 sed -i '' -e  "s;template_model;$model_name;" -e "s;template_location;$location_mod;" "$model_name/work/quickbuild.sh" 2>/dev/null
 
-case $location_mod in
+case $location_choice in
   threed | threed_cartesian | threed_sphere) # threed does not compile, but is a good framework
     cp "template/threed_model_mod.f90" "$model_name/model_mod.f90"
     cp "template/work/threed_input.nml" "$model_name/work/input.nml"
@@ -34,6 +35,10 @@ case $location_mod in
   oned)
     cp "template/oned_model_mod.f90" "$model_name/model_mod.f90"
     cp "template/work/oned_input.nml" "$model_name/work/input.nml"
+    ;;
+  twod)
+    cp "template/twod_model_mod.f90" "$model_name/model_mod.f90"
+    cp "template/work/twod_input.nml" "$model_name/work/input.nml"
     ;;
   *) # default is a threed_sphere template. As other location templates added, they can be added to case
     cp "template/threed_model_mod.f90" "$model_name/model_mod.f90"
