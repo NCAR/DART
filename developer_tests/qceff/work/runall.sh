d4de78956216394c2c5575bff1a6a5ea89edacd4
Helen Kershaw <hkershaw@ucar.edu>
2023-09-28 16:38:55 -0400

    remove stray /dev/null left in accidentally

--- developer_tests/qceff/work/runall.sh
+++ developer_tests/qceff/work/runall.sh
@@ -28,13 +28,13 @@ fi
 
 ./test_table_read ; should_pass "no table"
 
-./test_table_read qcf_table.txt /dev/null ; should_pass "correct v1 table"
+./test_table_read qcf_table.txt ; should_pass "correct v1 table"
 
-./test_table_read qcf_table_v2.txt  /dev/null  ; should_fail "detect wrong version"
+./test_table_read qcf_table_v2.txt ; should_fail "detect wrong version"
 
-./test_table_read qcf_table_extra_columns.txt /dev/null  ; should_pass "extra colums"
+./test_table_read qcf_table_extra_columns.txt ; should_pass "extra colums"
 
-./test_table_read qcf_table_bad_qty.txt  /dev/null  ; should_fail "bad qty"
+./test_table_read qcf_table_bad_qty.txt ; should_fail "bad qty"
 
-./test_table_read qcf_table_broke.txt  /dev/null  ; should_fail "bad value"
+./test_table_read qcf_table_broke.txt ; should_fail "bad value"
 
