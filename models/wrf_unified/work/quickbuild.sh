e5d0c092fe0c4b719b274f874e292451af2638b2
Helen Kershaw <hkershaw@ucar.edu>
2024-09-26 13:56:40 -0400

    removed duplicate WRF_BC, WRF_DART_untiliies
    will add back as needed

--- models/wrf_unified/work/quickbuild.sh
+++ models/wrf_unified/work/quickbuild.sh
@@ -19,38 +19,17 @@ closest_member_tool
 filter
 model_mod_check
 perfect_model_obs
-perturb_single_instance
-wakeup_filter
 )
 
 serial_programs=(
 #radiance_obs_seq_to_netcdf  # needs rttov
-advance_time
-create_fixed_network_seq
-create_obs_sequence
 fill_inflation_restart
-obs_common_subset
 obs_diag
-obs_selection
-obs_seq_coverage
-obs_seq_to_netcdf
-obs_seq_verify
 obs_sequence_tool
 )
 
 
 model_serial_programs=(
-WRF_DART_utilities/add_pert_where_high_refl
-WRF_DART_utilities/advance_cymdh
-WRF_DART_utilities/convertdate
-WRF_DART_utilities/ensemble_init
-WRF_BC/pert_wrf_bc
-WRF_DART_utilities/replace_wrf_fields
-select
-WRF_BC/update_wrf_bc
-WRF_DART_utilities/wrf_dart_obs_preprocess
-WRF_DART_utilities/extract
-WRF_DART_utilities/grid_refl_obs
 )
 
 arguments "$@"
