c9a2893617a467c9e7b2b5de2cf66d05a0fd054c
Helen Kershaw <hkershaw@ucar.edu>
2024-09-27 16:28:58 -0400

    chore: PANDA new build system

---
+++ observations/obs_converters/PANDA/work/quickbuild.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# DART software - Copyright UCAR. This open source software is provided
+# by UCAR, "as is", without charge, subject to all terms of use at
+# http://www.image.ucar.edu/DAReS/DART/DART_download
+
+main() {
+
+export DART=$(git rev-parse --show-toplevel)
+source "$DART"/build_templates/buildconvfunctions.sh
+
+CONVERTER=PANDA
+LOCATION=threed_sphere
+
+
+programs=(
+panda_co_ascii_to_obs
+panda_o3_ascii_to_obs
+panda_pm25_ascii_to_obs
+obs_sequence_tool
+advance_time
+)
+
+# build arguments
+arguments "$@"
+
+# clean the directory
+\rm -f -- *.o *.mod Makefile .cppdefs
+
+# build and run preprocess before making any other DART executables
+buildpreprocess
+
+# build 
+buildconv
+
+
+# clean up
+\rm -f -- *.o *.mod
+
+}
+
+main "$@"
