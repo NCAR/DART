3a99b71801dee32dda92abbae1a3698223b92b27
Moha El Gharamti <gharamti@ucar.edu>
2025-03-12 11:15:00 -0600

    Managing domains
    Added logic to manage active domains

--- models/pywatershed/model_mod.f90
+++ models/pywatershed/model_mod.f90
@@ -10,15 +10,15 @@ module model_mod
 use types_mod,             only : r8, i8, i4, MISSING_R8, vtablenamelength, &
                                   earth_radius
 
-use time_manager_mod,      only : time_type, set_time
+use time_manager_mod,      only : time_type, set_time, set_calendar_type
 
 use location_mod,          only : location_type, set_location, get_location,  &
                                   get_close_obs, get_close_state,             &
                                   convert_vertical_obs, convert_vertical_state
 
-use utilities_mod,         only : do_nml_file, do_nml_term,    &
-                                  nmlfileunit, find_namelist_in_file,           &
-                                  check_namelist_read
+use utilities_mod,         only : do_nml_file, do_nml_term, E_ERR, E_MSG,        &
+                                  nmlfileunit, find_namelist_in_file, to_upper,  &
+                                  check_namelist_read, file_exist, error_handler
 
 use location_io_mod,      only :  nc_write_location_atts, nc_write_location
 
@@ -26,18 +26,29 @@ use netcdf_utilities_mod, only : nc_add_global_attribute, nc_synchronize_file, &
                                  nc_add_global_creation_time, nc_begin_define_mode, &
                                  nc_end_define_mode
 
-use         obs_kind_mod,  only : QTY_STATE_VARIABLE
+use         obs_kind_mod,  only : get_index_for_quantity, &
+                                  get_name_for_quantity, &
+                                  get_quantity_for_type_of_obs, &
+                                  QTY_BUCKET_MULTIPLIER, &
+                                  QTY_RUNOFF_MULTIPLIER, &
+                                  QTY_STREAM_FLOW
 
 use ensemble_manager_mod,  only : ensemble_type
 
 use distributed_state_mod, only : get_state
 
-use state_structure_mod,   only : add_domain
+use state_structure_mod,   only : add_domain, get_domain_size, get_index_start,      &
+                                  get_index_end, get_num_domains, get_num_variables, &
+                                  get_num_dims, get_dim_name, get_dim_length,        &
+                                  get_variable_name, get_model_variable_indices,     &
+                                  get_varid_from_kind, get_dart_vector_index,        &
+                                  get_variable_size, state_structure_info
 
 use default_model_mod,     only : adv_1step, end_model, pert_model_copies,              & 
                                   nc_write_model_vars, MAX_STATE_VARIABLE_FIELDS_CLAMP, &
                                   init_time => fail_init_time,                          & 
-                                  init_conditions => fail_init_conditions 
+                                  init_conditions => fail_init_conditions,              &
+                                  parse_variables_clamp 
 
 use dart_time_io_mod,      only : read_model_time, write_model_time
 
@@ -45,8 +56,7 @@ implicit none
 private
 
 ! required by DART code - will be called from filter and other
-! DART executables.  interfaces to these routines are fixed and
-! cannot be changed in any way.
+! DART executables. 
 public :: get_model_size,         &
           get_state_meta_data,    &
           model_interpolate,      &
@@ -66,13 +76,18 @@ public :: get_model_size,         &
           write_model_time,       &
           shortest_time_between_assimilations
 
-character(len=256), parameter :: source   = "pywatershed/model_mod.f90"
+character(len=256), parameter :: source = "pywatershed/model_mod.f90"
 
 type(location_type), allocatable :: state_loc(:)  ! state locations, compute once and store for speed
 
-type(time_type) :: time_step
-integer(i8)     :: model_size
-logical, save   :: module_initialized = .false. 
+type(time_type)    :: time_step
+integer(i8)        :: model_size
+logical, save      :: module_initialized = .false. 
+character(len=256) :: msg1, msg2, msg3              ! Strings for error and warning messages
+integer            :: idom                          ! domain counter
+integer            :: idom_chn = -1                 ! channel domain number placeholder
+integer            :: idom_hru = -1                 ! hru domain number placeholder
+integer            :: idom_prm = -1                 ! parameters domain number placeholder
 
 ! Model namelist declarations with defaults
 integer            :: assimilation_period_days     = -1           ! model instantaneous streamflow at the hour 23 of the day
@@ -132,9 +147,102 @@ if (do_nml_term()) write(     *     , nml=model_nml)
 call set_calendar_type('gregorian')
 time_step  = set_time(assimilation_period_seconds, assimilation_period_days)
 
+! Add, configure and manage domains
+call manage_domains()
+
 model_size = 0
 
 end subroutine static_init_model
+
+
+!----------------------------------------------------
+! Add hydro + other domains and configure the network 
+! Make sure we have consistent templates and domains
+
+subroutine manage_domains()
+
+character(len=*), parameter :: routine = 'manage_domains'
+
+integer            :: adom, ndom 
+character(len=256) :: domain_name
+
+! Make sure there are enough domains 
+! If no domains are entered, then error out
+if (len_trim(domain_order(1)) == 0) then 
+   write(msg1, *) 'The domain order is incorrectly specified.'
+   write(msg2, *) 'The domain order cannot be empty, it should be something like:'
+   write(msg3, *) 'domain_order = "channel", "hru", "parameters"'
+   call error_handler(E_ERR, routine, msg1, source, text2=msg2, text3=msg3)
+endif
+
+! With a correct domain order, now look
+! for the corresponding template files and 
+! start processing each domain
+ndom = size(domain_order)
+adom = 0 ! We should have at least 1 active domain
+
+do idom = 1, ndom
+   ! Figure how many active domains are available 
+   if (len_trim(domain_order(idom)) == 0) exit 
+   adom = adom + 1
+enddo
+if (debug > 99) write(*, '(A, i3)') 'The number of active domains is:', adom
+
+! Check to see if the template files
+! for these active domains are correctly specified
+do idom = 1, adom
+   if (len_trim(domain_shapefiles(idom)) == 0) then 
+      write(msg1, '(A, i3, 1x, A)') 'The template file for domain', & 
+                  idom, 'is missing. Exiting ..' 
+      call error_handler(E_ERR, routine, msg1, source)
+   endif
+   if (debug > 99) then 
+      write(*, '(3A, i3)') 'Found shapefile "', &
+               trim(domain_shapefiles(idom)), &
+               '" for domain', idom
+   endif
+enddo
+
+! Read and add variables in the active domains
+DOMAINS: do idom = 1, adom
+   ! Domain name and its template
+   domain_name = domain_order(idom)
+   call to_upper(domain_name)
+
+   if (index(domain_name, 'CHANNEL') > 0) then 
+      ! Read the stream network and channel attributes   
+      call read_stream_network(channel_config_file)
+      call read_channel_atts(domain_shapefiles(idom))
+
+      ! Add channel variables: streamflow
+      idom_chn = add_domain(domain_shapefiles(idom), &
+                 parse_variables_clamp(channel_variables))
+   endif
+
+    
+enddo DOMAINS 
+
+end subroutine manage_domains
+
+
+!-------------------------------------------------------------
+! Read stream network variables and construct the connectivity 
+
+subroutine read_stream_network(filename)
+
+character(len=*), intent(in) :: filename
+
+end subroutine read_stream_network
+
+
+!-------------------------------------------------------------
+! Read channel global attributes from streamflow restart file
+
+subroutine read_channel_atts(filename)
+
+character(len=*), intent(in) :: filename
+
+end subroutine read_channel_atts
 
 
 !------------------------------------------------------------------
@@ -218,7 +326,6 @@ integer,             intent(out), optional :: qty_type
 
 ! these should be set to the actual location and state quantity
 location = state_loc(index_in)
-if (present(qty_type)) qty_type = QTY_STATE_VARIABLE 
 
 end subroutine get_state_meta_data
 
