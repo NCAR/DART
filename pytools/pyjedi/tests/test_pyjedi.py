97aa0b6ec705587d60f3fd6f6555e6699feb0380
Stephen Herbener <stephen.herbener@gmail.com>
2025-08-14 17:06:57 -0600

    Added amsua radiance test.

--- pytools/pyjedi/tests/test_pyjedi.py
+++ pytools/pyjedi/tests/test_pyjedi.py
@@ -31,9 +31,33 @@ def test_ioda2obsq_radiosonde():
     assert "RADIOSONDE_V_WIND_COMPONENT" in result.stdout, "Did not find 'RADIOSONDE_V_WIND_COMPONENT' in ioda2obsq output"
     assert "Converted 169 observations" in result.stdout, "Did not find 'Converted 169 observations' in ioda2obsq output"
 
+def test_ioda2obsq_amsua():
+    # Set up paths to all the necessary files
+    testDir = os.path.dirname(__file__)
+    command = 'ioda2obsq'
+    config_path = os.path.join(testDir, "obsq.amsua.yaml")
+    ioda_path = os.path.join(testDir, "amsua_n19_obs_2018041500.10.nc4")
+    obsq_path = os.path.join(testDir, "obs_seq.amsua.out")
+
+    # Run the converter, ioda2obsq
+    result = subprocess.run(
+        [command, config_path, ioda_path, obsq_path],
+        capture_output=True,
+        text=True
+    )
+
+    # Check results
+    print(result.stdout)
+    if (result.returncode != 0):
+        print("Error messages from ioda2obsq: \n", result.stderr)
+
+    assert result.returncode == 0, "Non-zero return code detected"
+
+    assert "NOAA_19_AMSUA_TB" in result.stdout, "Did not find 'NOAA_19_AMSUA_TB' in ioda2obsq output"
+    assert "Converted 70 observations" in result.stdout, "Did not find 'Converted 169 observations' in ioda2obsq output"
 
 if __name__ == "__main__":
-    for func in [test_ioda2obsq_radiosonde]:
+    for func in [test_ioda2obsq_radiosonde, test_ioda2obsq_amsua]:
         try:
             print(f"****** Testing {func.__name__} ******")
             func()
