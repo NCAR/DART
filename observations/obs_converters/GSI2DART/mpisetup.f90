bbf972dce163b214917f82719b7f4b539b235fdb
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-10-27 22:50:36 +0000

    replaced certain mpi functions with DART abstracted versions

--- observations/obs_converters/GSI2DART/mpisetup.f90
+++ observations/obs_converters/GSI2DART/mpisetup.f90
@@ -32,32 +32,38 @@ module mpisetup
 !$$$
 
 use kinds, only: r_kind, r_single, r_double
+use mpi_utilities_mod, only : task_count, my_task_id, initialize_mpi_utilities, finalize_mpi_utilities
 implicit none
 ! mpi definitions.
-include 'mpif.h'
+! include 'mpif.h'
 integer, public :: numproc, nproc
-integer, public :: mpi_status(mpi_status_size)
-integer, public :: mpi_realkind
+! integer, public :: mpi_status(mpi_status_size)
+! integer, public :: mpi_realkind
 
 contains
 
 subroutine mpi_initialize()
 integer :: ierr
-call mpi_init(ierr)
+! call mpi_init(ierr)
+
+call initialize_mpi_utilities('gsi_to_dart')
 ! nproc is process number, numproc is total number of processes.
-call mpi_comm_rank(mpi_comm_world,nproc,ierr)
-call mpi_comm_size(mpi_comm_world,numproc,ierr)
+! call mpi_comm_rank(mpi_comm_world,nproc,ierr)
+! call mpi_comm_size(mpi_comm_world,numproc,ierr)
+numproc = task_count()
+nproc = my_task_id()
 
 if (nproc == 0) print *,'running on ',numproc,' processors ...'
 if (r_kind == r_single) then
-   mpi_realkind = mpi_real4
-   if (nproc == 0) print *,'mpi_realkind set to mpi_real4',mpi_real4
+   ! mpi_realkind = mpi_real4
+   ! if (nproc == 0) print *,'mpi_realkind set to mpi_real4',mpi_real4
 else if (r_kind == r_double) then
-   mpi_realkind = mpi_real8
-   if (nproc == 0) print *,'mpi_realkind set to mpi_real8',mpi_real8
+   ! mpi_realkind = mpi_real8
+   ! if (nproc == 0) print *,'mpi_realkind set to mpi_real8',mpi_real8
 else
    print *,'ERROR: illegal r_kind (must be single or double)'
    call mpi_cleanup()
+   ! call finalize_mpi_utilities()
 endif
 
 end subroutine mpi_initialize
@@ -67,12 +73,13 @@ integer :: ierr
 flush(6,err=10)
 flush(0,err=10)
 10 continue
-call mpi_barrier(mpi_comm_world,ierr)
+!call mpi_barrier(mpi_comm_world,ierr)
 if (nproc == 0) write(6,*) 'all done!'
-call mpi_finalize(ierr)
-if (ierr /= 0) then
- print *, 'MPI_Finalize error status = ',ierr
-end if
+!call mpi_finalize(ierr)
+call finalize_mpi_utilities()
+! if (ierr /= 0) then
+!  print *, 'MPI_Finalize error status = ',ierr
+! end if
 end subroutine mpi_cleanup
 
 end module mpisetup
