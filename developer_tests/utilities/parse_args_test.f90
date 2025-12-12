7da4676b63023dcd0d831dccdf711cd71292984e
nancy <nancy@ucar.edu>
2025-12-12 16:17:13 -0700

    added more tests for strings with stranger characters in them.
    added tests for strings that look like floating point numbers,
    and strings with backslash and quoted strings.

--- developer_tests/utilities/parse_args_test.f90
+++ developer_tests/utilities/parse_args_test.f90
@@ -19,6 +19,11 @@ character(len=64) :: names(MAXVALS)
 character(len=64) :: vals(MAXVALS)
 logical :: continue_line
 
+character(len=20) :: testnum
+
+! the ascii code for the backslash character is 92.
+character(len=1), parameter :: BACKSLASH = ACHAR(92)
+
 !----------------------------------------------------------------------
 
 ! main code here
@@ -26,52 +31,145 @@ logical :: continue_line
 ! initialize the dart libs
 call initialize_utilities('parse_args_test')
 
+
+! parse tests - divide the blank-separated words on a line
+! into individual words.  note no interpretation of the
+! contents is expected - the returns are always strings.
+
+testnum = 'test 1'
 call resetvals()
 nextline = 'one two three'
 call get_args_from_string(nextline, nargs, args)
 
-call ok((nargs == 3),         'testline 1, nargs')
-call ok((args(1) == 'one'),   'testline 1, arg 1')
-call ok((args(2) == 'two'),   'testline 1, arg 2')
-call ok((args(3) == 'three'), 'testline 1, arg 3')
-call ok((args(4) == ''),      'testline 1, arg 4')
+call ok((nargs == 3),         trim(testnum)//', nargs')
+call ok((args(1) == 'one'),   trim(testnum)//', arg 1')
+call ok((args(2) == 'two'),   trim(testnum)//', arg 2')
+call ok((args(3) == 'three'), trim(testnum)//', arg 3')
+call ok((args(4) == ''),      trim(testnum)//', arg 4')
 
 
+testnum = 'test 2'
+call resetvals()
+nextline = ' one      two three  '
+call get_args_from_string(nextline, nargs, args)
+
+call ok((nargs == 3),         trim(testnum)//', nargs')
+call ok((args(1) == 'one'),   trim(testnum)//', arg 1')
+call ok((args(2) == 'two'),   trim(testnum)//', arg 2')
+call ok((args(3) == 'three'), trim(testnum)//', arg 3')
+call ok((args(4) == ''),      trim(testnum)//', arg 4')
+
+
+testnum = 'test 3'
+call resetvals()
+nextline = '     one        two        three'
+call get_args_from_string(nextline, nargs, args)
+
+call ok((nargs == 3),         trim(testnum)//', nargs')
+call ok((args(1) == 'one'),   trim(testnum)//', arg 1')
+call ok((args(2) == 'two'),   trim(testnum)//', arg 2')
+call ok((args(3) == 'three'), trim(testnum)//', arg 3')
+call ok((args(4) == ''),      trim(testnum)//', arg 4')
+
+
+testnum = 'test 4'
+call resetvals()
+nextline = 'one       two th/ree'
+call get_args_from_string(nextline, nargs, args)
+
+call ok((nargs == 3),          trim(testnum)//', nargs')
+call ok((args(1) == 'one'),    trim(testnum)//', arg 1')
+call ok((args(2) == 'two'),    trim(testnum)//', arg 2')
+call ok((args(3) == 'th/ree'), trim(testnum)//', arg 3')
+call ok((args(4) == ''),       trim(testnum)//', arg 4')
+
+
+testnum = 'test 5'
 call resetvals()
 nextline = '"quoted string"'
 call get_args_from_string(nextline, nargs, args)
 
-call ok((nargs == 1),                 'testline 2, nargs')
-call ok((args(1) == 'quoted string'), 'testline 2, arg 1')
-call ok((args(2) == ''),              'testline 2, arg 2')
+call ok((nargs == 1),                 trim(testnum)//', nargs')
+call ok((args(1) == 'quoted string'), trim(testnum)//', arg 1')
+call ok((args(2) == ''),              trim(testnum)//', arg 2')
 
 
+testnum = 'test 6'
 call resetvals()
-nextline = '1 20 300 4000'
+nextline = '  "quoted string"  "second string  with blanks"  '
 call get_args_from_string(nextline, nargs, args)
 
-call ok((nargs == 4),        'testline 3, nargs')
-call ok((args(1) == '1'),    'testline 3, arg 1')
-call ok((args(2) == '20'),   'testline 3, arg 2')
-call ok((args(3) == '300'),  'testline 3, arg 3')
-call ok((args(4) == '4000'), 'testline 3, arg 4')
-call ok((args(5) == ''),     'testline 3, arg 5')
+call ok((nargs == 2),                              trim(testnum)//', nargs')
+call ok((args(1) == 'quoted string'),              trim(testnum)//', arg 1')
+call ok((args(2) == 'second string  with blanks'), trim(testnum)//', arg 2')
+call ok((args(3) == ''),                           trim(testnum)//', arg 3')
 
 
+testnum = 'test 7'
+call resetvals()
+nextline = '"quoted" "string"'
+call get_args_from_string(nextline, nargs, args)
+
+call ok((nargs == 2),           trim(testnum)//', nargs')
+call ok((args(1) == 'quoted'),  trim(testnum)//', arg 1')
+call ok((args(2) == 'string'),  trim(testnum)//', arg 1')
+call ok((args(3) == ''),        trim(testnum)//', arg 2')
+
+
+testnum = 'test 8'
+call resetvals()
+nextline = 'string'//BACKSLASH//'abc'
+call get_args_from_string(nextline, nargs, args)
+ 
+call ok((nargs == 1),             trim(testnum)//', nargs')
+call ok((args(1) == 'stringabc'), trim(testnum)//', arg 1')
+call ok((args(2) == ''),          trim(testnum)//', arg 2')
+
+
+testnum = 'test 9'
+call resetvals()
+nextline = 'directory'//BACKSLASH//BACKSLASH//'abc'
+call get_args_from_string(nextline, nargs, args)
+
+call ok((nargs == 1),                               trim(testnum)//', nargs')
+call ok((args(1) == 'directory'//BACKSLASH//'abc'), trim(testnum)//', arg 1')
+call ok((args(2) == ''),                            trim(testnum)//', arg 2')
+
+
+! start of name/value pair tests
+
+testnum = 'test 10'
 call resetvals()
 nextline = 'a=1 b=2 c=3'
 call get_name_val_pairs_from_string(nextline, nargs, names, vals, continue_line)
 
-call ok((nargs == 3),           'testline 4, nargs')
-call ok((names(1) == 'a'),      'testline 4, name 1')
-call ok((vals(1)  == '1'),      'testline 4, val 1')
-call ok((names(2) == 'b'),      'testline 4, name 2')
-call ok((vals(2)  == '2'),      'testline 4, val 2')
-call ok((names(3) == 'c'),      'testline 4, name 3')
-call ok((vals(3)  == '3'),      'testline 4, val 3')
-call ok((names(4) == ''),       'testline 4, name 4')
-call ok((vals(4)  == ''),       'testline 4, val 4')
-call ok((.not. continue_line),  'testline 4, continue_line')
+call ok((nargs == 3),           trim(testnum)//', nargs')
+call ok((names(1) == 'a'),      trim(testnum)//', name 1')
+call ok((vals(1)  == '1'),      trim(testnum)//', val 1')
+call ok((names(2) == 'b'),      trim(testnum)//', name 2')
+call ok((vals(2)  == '2'),      trim(testnum)//', val 2')
+call ok((names(3) == 'c'),      trim(testnum)//', name 3')
+call ok((vals(3)  == '3'),      trim(testnum)//', val 3')
+call ok((names(4) == ''),       trim(testnum)//', name 4')
+call ok((vals(4)  == ''),       trim(testnum)//', val 4')
+call ok((.not. continue_line),  trim(testnum)//', continue_line')
+
+
+testnum = 'test 10'
+call resetvals()
+nextline = 'a = 1.0 b   =  2.0  c=3.0'
+call get_name_val_pairs_from_string(nextline, nargs, names, vals, continue_line)
+
+call ok((nargs == 3),           trim(testnum)//', nargs')
+call ok((names(1) == 'a'),      trim(testnum)//', name 1')
+call ok((vals(1)  == '1.0'),    trim(testnum)//', val 1')
+call ok((names(2) == 'b'),      trim(testnum)//', name 2')
+call ok((vals(2)  == '2.0'),    trim(testnum)//', val 2')
+call ok((names(3) == 'c'),      trim(testnum)//', name 3')
+call ok((vals(3)  == '3.0'),    trim(testnum)//', val 3')
+call ok((names(4) == ''),       trim(testnum)//', name 4')
+call ok((vals(4)  == ''),       trim(testnum)//', val 4')
+call ok((.not. continue_line),  trim(testnum)//', continue_line')
 
 
 ! finalize parse_args_test
