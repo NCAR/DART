64b2fb97b8033686448a5ecd7e61aeb9f27c06bc
nancy <nancy@ucar.edu>
2025-12-27 09:39:48 -0700

    make csv parse test self-contained and use testeverything
    tests for the csv parsing routine

--- developer_tests/utilities/parse_csv_test.f90
+++ developer_tests/utilities/parse_csv_test.f90
@@ -3,81 +3,68 @@
 ! http://www.image.ucar.edu/DAReS/DART/DART_download
 !
 
-! this test reads stdin.  to automate it, run it with:
-!  cat bob | ./parse_words_test
-! or
-!  ./parse_words_test < bob
-!
-! where the file bob contains one line per test with words to be parsed.
+program parse_csv_test
 
-program parse_words_test
+use utilities_mod,        only : initialize_utilities, finalize_utilities, &
+                                 open_file, close_file
+use parse_args_mod,       only : get_csv_words_from_string
 
-use utilities_mod,     only : initialize_utilities, finalize_utilities, &
-                              register_module, error_handler, E_ERR, E_MSG
-use parse_args_mod,    only : get_csv_words_from_string
+use test   ! fortran-testanything
 
 implicit none
 
 ! version controlled file description for error handling, do not edit
-character(len=*), parameter :: source   = "parse_words_test.f90"
-character(len=*), parameter :: revision = ""
-character(len=*), parameter :: revdate  = ""
+character(len=*), parameter :: source   = "parse_csv_test.f90"
 
-integer :: iunit = 5
-integer :: ierr, nwords, i
+integer :: nwords, i
 character(len=512) :: nextline
 character(len=64) :: words(100)
-character(len=64) :: names(100)
-character(len=64) :: vals(100)
-logical :: continue_line
 
 !----------------------------------------------------------------------
 
 ! main code here
  
 ! initialize the dart libs
-call initialize_utilities('parse_words_test')
+call initialize_utilities('parse_csv_test', standalone_program=.true.)
+call plan(19)
 
-print *, 'enter lines with words separated by commas.'
-print *, '(enter control-D to end)'
+nextline = "these,are,comma,separated,words,on,a,line"
+call get_csv_words_from_string(nextline, ',', nwords, words)
+call ok((nwords == 8), "line 1, word count")
+call ok((words(1) == "these"), "line 1, word 1")
+call ok((words(8) == "line"), "line 1, word 8")
 
+nextline = "one,two,three"
+call get_csv_words_from_string(nextline, ',', nwords, words)
+call ok((nwords == 3), "line 2, word count")
+call ok((words(1) == "one"), "line 2, word 1")
+call ok((words(3) == "three"), "line 2, word 3")
 
-GETMORE : do
-   read(iunit, '(A)', iostat=ierr) nextline
-   if (ierr /= 0) exit GETMORE
-   if (nextline == 'NEXT') exit GETMORE
+nextline = '"quoted string1"'//",nonquote,'quote two','embedded_"//'"'//"_quote'"
+call get_csv_words_from_string(nextline, ',', nwords, words)
+call ok((nwords == 4), "line 3, word count")
+call ok((words(1) == "quoted string1"), "line 3, word 1")
+call ok((words(2) == "nonquote"), "line 3, word 2")
+call ok((words(3) == "quote two"), "line 3, word 3")
+call ok((words(4) == 'embedded_"_quote'), "line 3, word 4")
 
-   call get_csv_words_from_string(nextline, ',', nwords, words)
+nextline = "1;2;3"
+call get_csv_words_from_string(nextline, ';', nwords, words)
+call ok((nwords == 3), "line 4, word count")
+call ok((words(1) == "1"), "line 4, word 1")
+call ok((words(3) == "3"), "line 4, word 3")
 
-   print *, 'input line: "'//trim(nextline)//'"'
-   print *, 'got ', nwords, ' words from the input line'
-   do i=1, nwords
-      print *, 'word ', i, 'is: ', '"'//trim(words(i))//'"'
-   enddo
+nextline = "single_string"
+call get_csv_words_from_string(nextline, ',', nwords, words)
+call ok((nwords == 1), "line 5, word count")
+call ok((words(1) == "single_string"), "line 5, word 1")
 
-enddo GETMORE
+nextline = "comma separated strings,with embedded blanks"
+call get_csv_words_from_string(nextline, ',', nwords, words)
+call ok((nwords == 2), "line 6, word count")
+call ok((words(1) == "comma separated strings"), "line 6, word 1")
+call ok((words(2) == "with embedded blanks"), "line 6, word 2")
 
-print *, 'enter lines with words separated by semicolons.'
-print *, '(enter control-D to end)'
-
-
-GETMORE2 : do
-   read(iunit, '(A)', iostat=ierr) nextline
-   if (ierr /= 0) exit GETMORE2
-   if (nextline == 'NEXT') exit GETMORE2
-
-   call get_csv_words_from_string(nextline, ';', nwords, words)
-
-   print *, 'input line: "'//trim(nextline)//'"'
-   print *, 'got ', nwords, ' words from the input line'
-   do i=1, nwords
-      print *, 'word ', i, 'is: ', '"'//trim(words(i))//'"'
-   enddo
-
-enddo GETMORE2
-
-! finalize parse_words_test
-call error_handler(E_MSG,'parse_words_test','Finished successfully.',source,revision,revdate)
 call finalize_utilities()
 
 ! end of main code
