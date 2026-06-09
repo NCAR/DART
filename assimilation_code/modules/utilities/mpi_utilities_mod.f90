a8aa708f6d0a5b286c051af8ade726966813df13
Marlee Smith <masmith@ucar.edu>
2026-06-09 12:12:08 -0600

    Replacing error stop with stop in non-null mpi utilities mods

--- assimilation_code/modules/utilities/mpi_utilities_mod.f90
+++ assimilation_code/modules/utilities/mpi_utilities_mod.f90
@@ -183,13 +183,13 @@ errcode = -999
 call MPI_Initialized(already, errcode)
 if (errcode /= MPI_SUCCESS) then
    write(*, *) 'MPI_Initialized returned error code ', errcode
-   error stop errcode
+   stop errcode
 endif
 if (.not.already) then
    call MPI_Init(errcode)
    if (errcode /= MPI_SUCCESS) then
       write(*, *) 'MPI_Init returned error code ', errcode
-      error stop errcode
+      stop errcode
    endif
 endif
 
@@ -205,7 +205,7 @@ endif
 call MPI_Comm_rank(my_local_comm, myrank, errcode)
 if (errcode /= MPI_SUCCESS) then
    write(*, *) 'MPI_Comm_rank returned error code ', errcode
-   error stop errcode
+   stop errcode
 endif
 
 ! pass the arguments through so the utilities can log the program name
